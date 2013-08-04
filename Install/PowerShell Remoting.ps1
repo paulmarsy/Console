16e8e981ed1a291f255c97eed89f62efc35c4d59
Paul Marston <vai@vai-net.co.uk>
2013-08-04 03:40:46 +0100

    Adding .gitattributes, normalising line endings
    Former-commit-id: 38f4767fd6907e00a8d3f60f6689e7daf72956b6 [formerly 37705c57df9d28590e9299ba5f211f7194f29789] [formerly 7d4553289a22af380e09a05b7d864bdbdc4e0abb] [formerly 0cb836fb7f9e5735d18ebc1f0f51253942e300fc] [formerly 815313dc884aaff626f171181a75b6434c2a35a7] [formerly 190ada2346c5b2a19eaf9a76c10de02b4746dee8] [formerly 0655acf3c4a07a2c468be092a00b5d480d81166d] [formerly c64f8e104a0e9a0ff666cd68b3e3dc0038456fec] [formerly 8ca60116d33d838fd56066e982ff109d3147b36d]
    Former-commit-id: f804961052ddcbe36587675cfb37bbb69caed878
    Former-commit-id: 19cce3a902b33796cde191d09ea07e250abdead5
    Former-commit-id: cf731a533fa5fb88c88c51ab0bdf857ce119214c
    Former-commit-id: 9a182c8b1582c593952a89574ac26e46fadf710c
    Former-commit-id: ad28873dff96499e95dcd84d36cfbde41d0c8dd2
    Former-commit-id: 39a697e03225abd016335bd90eb36f4c2f9629b7
    Former-commit-id: 48fb07f768c1d1d49bfc6a2079a56f8f2304b2de
    Former-commit-id: 96147e628504e94488fc754a50cef51ac21712c9

--- Install/PowerShell Remoting.ps1
+++ Install/PowerShell Remoting.ps1
@@ -1,4 +1,4 @@
 "Enabling PowerShell Remoting..."
 Enable-PSRemoting -Force
-Enable-WSManCredSSP -Role Client –DelegateComputer * -Force | Out-Null
-Enable-WSManCredSSP -Role Server –Force | Out-Null
+Enable-WSManCredSSP -Role Client -DelegateComputer * -Force | Out-Null
+Enable-WSManCredSSP -Role Server -Force | Out-Null
