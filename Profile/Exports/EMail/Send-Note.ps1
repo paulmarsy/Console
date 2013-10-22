6bc02575fea84f037be5945121a8ba0fb4b23cd8
Paul Marston <vai@vai-net.co.uk>
2013-10-22 22:31:33 +0100

    Adding email functionality
    Former-commit-id: ee09dfb09fca8e75a5d037412107d6c4602bc9ec [formerly fe875f8b1d937ec0543cec509c77337eb2b73d63] [formerly fc10f841e965335151cd5d8c1b11addd630a56ef] [formerly 1dbc1d7916ddc78360094e800668cfa1ad083fc0] [formerly 5e956ccb8dc056627b52abbc71aa62db5e892393] [formerly 229e112d0a5b608b94783aaf739f981a89145858] [formerly a847c8984e5bb5a5d0d10a3bb9f4be672572441b] [formerly dc5772fa79fed5f8348edf7664e59b9b9f1ee2e8] [formerly 856be7c1290844c0a9440597561726956759d0d1]
    Former-commit-id: 81f4814b168c416598a73504999af8660f805cc3
    Former-commit-id: 1837ca2f34fda0f5b487b84a53f814c1175e5499
    Former-commit-id: c9def3434a09acf837cd444edb71d513c0dc68fe
    Former-commit-id: 73ef0c46897fcc2a032998d4ecc43941b9330ab6
    Former-commit-id: 2dd5b48738aed00b23bce3c54db01df624c35fc2
    Former-commit-id: 93585f565f50ae39229e244bcc26b6e56359efb7
    Former-commit-id: c58e3d8a333614b1c074cd60e66100e85573527c
    Former-commit-id: acfd277bee4e334508011394afb2775157c3fcdf

---
+++ Profile/Exports/EMail/Send-Note.ps1
@@ -0,0 +1,18 @@
+function Send-Note {
+	[CmdletBinding()]
+    param(
+    	[Parameter(Mandatory=$true)][ValidateSet("Work", "Home")]$To,
+        [Parameter(Mandatory=$true,ValueFromRemainingArguments=$true)][string]$Body
+    )
+
+    $password = ConvertTo-SecureString $ProfileConfig.EMail.Password
+    $credential = New-Object System.Management.Automation.PSCredential ($ProfileConfig.EMail.Username, $password)
+
+    $emailTo = switch ($To) {
+    	"Work" { "paulm@asos.com" }
+    	"Home" { "vai@vai-net.co.uk" }
+    }
+
+	Send-MailMessage -To $emailTo -Subject "Note" -Body $Body -UseSsl -Port 587 -From $ProfileConfig.EMail.From -Credential $credential
+}
+@{Function = "Send-Note"}
