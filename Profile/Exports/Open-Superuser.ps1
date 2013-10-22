d47a44fa7008f2b9886f4c36ab333f6d2ecd3b5c
Paul Marston <paulm@asos.com>
2013-10-22 16:54:05 +0100

    Updating
    Former-commit-id: c5cc10500601ee5a04d95929a9dd3a67037d92de [formerly 04bfbd3a2009fbd2f854663c3d89574f1a195115] [formerly 060fe20a4ab5f2f235ef9151f8d34bf1b0fcb160] [formerly a17b6d746359c05d4f836e8f6acdce881c713d7b] [formerly 4bee0b03a1afa7a17d671dd2740e943409ef19ec] [formerly 786a7e401f4aea8e29beaff31daf58da3cf8d63e] [formerly 205d4e880a8ac679aae756e3d6c19275c9d7883f] [formerly 1b72f19d33fe0cbe56cfaca26f9c2df1ff9f4966] [formerly c37ed4babeaa647f7e27ac2aafb5704272446b00]
    Former-commit-id: abd7b30bffa173b02a45d8d71ebdf9cd973389a6
    Former-commit-id: b31d7886d9906d27b6e3648687302d74c45f94ed
    Former-commit-id: 67c49fa69c408b1b2bf379c7d9f6ade42e593245
    Former-commit-id: e27d73e34adfd6e4d668511b2f632f2f3895d791
    Former-commit-id: 37904e1a3b5838685843f7bd76cd2f703009d25f
    Former-commit-id: 03350ef26b910a1f77b5bfbac55688172163f1bc
    Former-commit-id: d2e3b9c7e11549d615d31f717eea62ca411cfd67
    Former-commit-id: e25f0257008034691e5d58335a8ba1c30e2b86e7

--- Profile/Exports/Open-Superuser.ps1
+++ Profile/Exports/Open-Superuser.ps1
@@ -1,7 +1,7 @@
 Set-Alias su Open-Superuser
 function Open-Superuser
 {
-    Start-Process -FilePath "$InstallPath\Console\ConEmu64.exe" -ArgumentList "/cmd powershell.exe" -Verb RunAs -WorkingDirectory $pwd
+    Start-Process -FilePath "$InstallPath\Console\ConEmu64.exe" -ArgumentList "/cmd powershell.exe -cur_console:na" -WorkingDirectory $pwd
     Exit
 }
 @{Function = "Open-Superuser"; Alias = "su"}
--- Profile/Exports/TFS/Get-VersionControlServer.ps1
+++ Profile/Exports/TFS/Get-VersionControlServer.ps1
@@ -4,6 +4,10 @@ function Get-VersionControlServer {
 	(
 		$tfsServerUrl = $ProfileConfig.TFS.Server
 	)
+
+	if ($tfsServerUrl -eq $null) {
+		Write-Error "No TFS Server URL Given"
+	}
 	
 	[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Client") | Out-Null
 	[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Build.Client") | Out-Null
