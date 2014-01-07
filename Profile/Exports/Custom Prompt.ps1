024f5ce1865f234ec5553c2fb30c3f127d8d82c0
Paul Marston <paulm@asos.com>
2014-01-07 09:47:48 +0000

    Fixing custom prompt so it doesnt crash when a folder you are in is deleted
    Former-commit-id: f5b45fe5b070ce82bff4f1693fe2546e5a6de8c1 [formerly cb36fcb6b97cfa5308da926d104ec7212117cf36] [formerly e554afde14b049807d2942c43fc6986826b9a8cb] [formerly 973809ef7dcbe3f1556b49221fd5fc7e4afbf9e0] [formerly 112cfac5e9f852a0a4ca0bfba624a73256307490] [formerly 8050374ef02675ebdd7d136ac43d1160ad8b2608] [formerly f0f9e2f753b0e38923c664082075df937396912b] [formerly c5234939228847e3bef3f663fd88dc544f46a58a] [formerly 8839dafc6f8b36018bacce9364b0ccb15512c6c3]
    Former-commit-id: 07d3f7680ef57eab797cad2a98f8bfd858400ce1
    Former-commit-id: 9c1f39e50365d7e006692d3f3055682c11e1431a
    Former-commit-id: c4f56e829565bf4eb405fdb045756ef160575539
    Former-commit-id: 87ef0c381bae585cc1afe876ef9bbf82b0d1902a
    Former-commit-id: 68bb2c59e1d6a7625aa699cc76b429201bb973c5
    Former-commit-id: ccfdc9b165352569e37d1dc957ee8c32974508ca
    Former-commit-id: 270143c14264fab909bcc2f07b40374b4822b9f8
    Former-commit-id: 1ec274b8208dd37993faac7c421debee3bba680a

--- Profile/Exports/Custom Prompt.ps1
+++ Profile/Exports/Custom Prompt.ps1
@@ -8,10 +8,14 @@ function Write-CustomPrompt {
     else { $securityColour = "Green" }
 
     if ((Split-Path $pwd -NoQualifier) -eq "\") { $path = Split-Path $pwd -Qualifier }
-	else { $path = (Split-Path $pwd -Parent | Get-ChildItem -Filter (Split-Path $pwd -Leaf) -Force).Name }
+	else { $path = Split-Path $pwd -Parent | Get-ChildItem -Filter (Split-Path $pwd -Leaf) -Force | Select-Object -ExpandProperty Name }
 
-	Write-Host -ForegroundColor $securityColour -NoNewLine "$path"
-    Write-VcsStatus
+    if ($path) {
+        Write-Host -ForegroundColor $securityColour -NoNewLine "$path"
+        Write-VcsStatus
+    } else {
+        Write-Host -ForegroundColor $securityColour -NoNewLine "<Invalid Path>"
+    }
     if ($NestedPromptLevel -ne 0) {
         Write-Host -ForegroundColor $securityColour -NoNewLine "($NestedPromptLevel)"
     }
