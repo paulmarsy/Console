dacc842e2b55e5ef12c8c8c2b3b33852781545e9
Paul Marston <paulm@asos.com>
2014-02-14 11:36:24 +0000

    Adding find-string cmdlet
    Former-commit-id: 23c26ca54d946d566c6eb54239ebaa86333bfbd5 [formerly 8a60f7754e9b4186e7f62f55436c868969a8888f] [formerly a2446e2c927729f3ed6d4d95082b8ad025cf06b8] [formerly 3288a242fda3ddf5921dbfe07dc5cf1876c51eaa] [formerly a6f6186f69618914d783a472ba9035b86c07391d] [formerly 8e4a12f691ec1ebed7b97ff7ddcd58bd0e045206] [formerly fd86f0b2d0154dc58a09bf0cc1c86ff14f754eaf] [formerly c7b6025fb6406ddbf3f6a7117a60cb9a1f4c4226] [formerly 01dba5aed431c065e59f3d37ff7eec97817371b1]
    Former-commit-id: 9efb9628e03810765601586b0003ed82101df6c8
    Former-commit-id: 8dd30c3325c67c8d8af83953af5119e2e0bbd43c
    Former-commit-id: fbfdf1f91c4fd22167ae2bfe3b67876aba966ea3
    Former-commit-id: 8711e707b7f82fe52490703c966073fcf5bfc329
    Former-commit-id: d9cb330cf6fb80491006ae144dc5e6ea3aae0ebf
    Former-commit-id: f2435eebeacb5381fddd6596e2be6584d50a41ba
    Former-commit-id: b2b404fd1f7e1c46fdc6dd7837c10d9a037c2d4c
    Former-commit-id: 6e65cc508dd7c3d71e3e0fa85cca60f55db6e5a9

--- Profile/Exports/Find-String.ps1
+++ Profile/Exports/Find-String.ps1
@@ -1,57 +1,55 @@
+
 function Find-String {
     [CmdletBinding()]
 	param(
 		[Parameter(Position=0,Mandatory=$true)]$pattern,
 		$path = $pwd,
 		[switch]$showContext,
-		[switch]$includeLargeFiles	
+		[switch]$includeLargeFiles
     )
 
 	$maxFileSizeToSearchInBytes = 1024*1024 # 1mb
-
-	$priorPath = ""
-
+	
 	Write-Host "Finding '$pattern' in $path...`n" -ForegroundColor White
 	Get-ChildItem -Path $path -Recurse | 
 		? { $_.PSIsContainer -eq $false -and ($includeLargeFiles -or $_.Length -le $maxFileSizeToSearchInBytes) } | 
 		Select-String -Pattern ([Regex]::Escape($pattern)) -AllMatches -Context 2 |
+		Group-Object -Property Path |
 		% {
-			# Display file name header
-			if (!$showContext -or $priorPath -ne $_.Path) {
-				Write-Host ($_.RelativePath($path)) -ForegroundColor Cyan -NoNewLine; Write-Host ":" -NoNewLine
-			}
+			Write-Host (Resolve-Path -Relative $_.Name) -ForegroundColor Cyan -NoNewLine
+			Write-Host ":"
 
-			if ($priorPath -eq $_.Path -and $showContext) { Write-Host "--" -NoNewLine }
-			elseif ($priorPath -ne $_.Path -and $showContext) { Write-Host }
-			else 
-			if ($showContext) { Write-Host }
-			$priorPath = $_.Path
+			$_.Group | % {
+		     	# Display pre-context
+	            if ($showContext -and $_.Context.DisplayPreContext) {
+	            	$lines = ($_.LineNumber - $_.Context.DisplayPreContext.Length)..($_.LineNumber - 1)
+	        		for ($i = 0; $i -lt $_.Context.DisplayPreContext.length; $i++) {
+	        			Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPreContext[$i]) -ForegroundColor DarkGray
+	        		}
+	            }
 
-	     	# Display pre-context
-            if ($showContext -and $_.Context.DisplayPreContext) {
-            	$lines = ($_.LineNumber - $_.Context.DisplayPreContext.Length)..($_.LineNumber - 1)
-        		for ($i = 0; $i -lt $_.Context.DisplayPreContext.length; $i++) {
-        			Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPreContext[$i]) -ForegroundColor DarkGray
-        		}
-            }
+	            # Display main result
+	            Write-Host ("{0}: " -f $_.LineNumber) -NoNewLine
+	            $index = 0
+	            foreach ($match in $_.Matches) {
+	                Write-Host $_.Line.SubString($index, $match.Index - $index) -NoNewLine
+	                Write-Host $match.Value -ForegroundColor Black -BackgroundColor Yellow -NoNewLine
+	                $index = $match.Index + $match.Length
+	            }
+	            Write-Host $_.Line.SubString($index)
 
-            # Display main result
-            Write-Host ("{0}: " -f $_.LineNumber) -NoNewLine
-            $index = 0
-            foreach ($match in $_.Matches) {
-                Write-Host $_.Line.SubString($index, $match.Index - $index) -NoNewLine
-                Write-Host $match.Value -ForegroundColor Black -BackgroundColor Yellow -NoNewLine
-                $index = $match.Index + $match.Length
-            }
-            Write-Host $_.Line.SubString($index)
+	            # Display post-context
+				if ($showContext -and $_.Context.DisplayPostContext) {
+					$lines = ($_.LineNumber + 1)..($_.LineNumber + $_.Context.DisplayPostContext.Length)
+		        	for ($i = 0; $i -lt $_.Context.DisplayPostContext.length; $i++) {
+		        		Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPostContext[$i]) -ForegroundColor DarkGray
+		        	}
+		  		}
 
-            # Display post-context
-			if ($showContext -and $_.Context.DisplayPostContext) {
-				$lines = ($_.LineNumber + 1)..($_.LineNumber + $_.Context.DisplayPostContext.Length)
-	        	for ($i = 0; $i -lt $_.Context.DisplayPostContext.length; $i++) {
-	        		Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPostContext[$i]) -ForegroundColor DarkGray
-	        	}
-	  		}
+		  		if ($showContext) {
+		  			Write-Host "--"
+		  		}
+		  	}
 	  	}
 }
 @{Function = "Find-String"}
