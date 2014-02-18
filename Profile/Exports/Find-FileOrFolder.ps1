046b23c7818d78146a752ecb72de50f461d5db7c
Paul Marston <paulm@asos.com>
2014-02-18 10:23:35 +0000

    Finishing find-fileorfolder cmdlet and tweaking find-string cmdlet
    Former-commit-id: e1f88142a8b59daea1e959bfdc9eb32d8956de9f [formerly a901394dfdc6d5bebcd1939d9b988478c5583c91] [formerly d2f422f7ce99533fa566624d4ea2f7f456834d9b] [formerly 834acf8b6f38dbc36ec47b36e6271eea3e661d12] [formerly de975ab2051b2e8f6644c7f844351710fa3d7dc9] [formerly e1ab38a15ae3a1cb33b6779fceb43f79d47b43fa] [formerly 30c7b6f40ee65db1a046c4f676191ecd3a5d9fcd] [formerly 552b0bbef670288bece4027fe3b8362546247f6e] [formerly 423f418a8138702ab4a39dd932bef416adf5bbeb]
    Former-commit-id: 1e4621715cc2eec99d4a046b6b617945d6589d7b
    Former-commit-id: 0db7e5927d360ec564bc6520fcd7c25f0c248d0a
    Former-commit-id: 3ea0bc8671b7c79cd85722ac1d54e459e28dbbf6
    Former-commit-id: 0abd74218ed4379667de65dc48da43d54a634591
    Former-commit-id: 1f4490eee19ffc6eecdbde22d23565144905a6df
    Former-commit-id: 79f92a816c29ba435062552ec87f1c355fdf5f0a
    Former-commit-id: 1b940addb594a9f3959dedf2127b7e1f1324c6bd
    Former-commit-id: 1734fb445c1150c2d0518fb9a8c2f16f6688177c

--- Profile/Exports/Find-FileOrFolder.ps1
+++ Profile/Exports/Find-FileOrFolder.ps1
@@ -1,15 +1,12 @@
-
 function Find-FileOrFolder {
     [CmdletBinding()]
 	param(
 		[Parameter(Position=0,Mandatory=$true)]$name,
-		$path = $pwd
+		$path = $pwd,
+		[switch]$partialName
     )
-
-	$maxFileSizeToSearchInBytes = 1024*1024 # 1mb
 	
-	Write-Host "Finding '$pattern' in $path...`n" -ForegroundColor White
-	Get-ChildItem -Path $path -Recurse | 
-		? { $_.PSIsContainer -eq $false -and ($includeLargeFiles -or $_.Length -le $maxFileSizeToSearchInBytes) } | 
+	if ($partialName) { $name = "*$($name)*" }
+	Get-ChildItem -Path $path -Recurse -Filter $name | % { Resolve-Path -Relative $_.FullName }
 }
-@{Function = "Find-F"}
+@{Function = "Find-FileOrFolder"}
