9e2e4ec22328deb5edb86d5f2f75e0f782166893
Paul Marston <paulm@asos.com>
2014-03-28 09:43:48 +0000

    test
    Former-commit-id: 3f3ffc6466617dae2baf7f312709a1a1d69c2d59 [formerly bfc524f36acdea31c30a5a14eb6035c8b5a5f447] [formerly 93f46b317d6e2f20adb137588c8502923c1b59c4] [formerly c1df6c5c6fbfaf63b60e2c5bd8e581992f0508c2] [formerly 111852066d08ac410d1084328e0679e45ec6e35a] [formerly 033176ef093ee165babc71a2eab38b4d20f02305] [formerly 5cb0772fd366ae6b8276631a908f904adbf13041] [formerly cf7cd3fb43f24061d5a0f957022eba2667b3189e] [formerly c155605875e64a38604097b7857fd52defcb4e63]
    Former-commit-id: a3b6757aecff0e3d6a496afad299eb9eb6b393fa
    Former-commit-id: e8d5241453edc12f4bc573fc383cc284f363ee8a
    Former-commit-id: 92f57f4cba2fe01ec14c1fd90c733d35934af8bd
    Former-commit-id: 5190a5f88b41b12800e524086886e935c058fc43
    Former-commit-id: 4b011ac390906fc064169e54fce8265415ec3ebb
    Former-commit-id: 015952d968e6be46a8e811ad6a77a41d5b2da8d9
    Former-commit-id: a291c9546577ddcd4573c0eb0a3780740f10cfb9
    Former-commit-id: cc751f197b1ff4ed6765e90da68d594d32bb1e9a

---
+++ Profile/Exports/Update-ConsoleGit.ps1
@@ -0,0 +1,21 @@
+function Update-ConsoleGit {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]$commitMessage,
+		[switch]$pushToGitHub
+    )
+
+    Push-Location $ProfileConfig.General.InstallPath
+    try {
+    	& git add -A
+    	& git commit -a -m $commitMessage
+    	if ($pushToGitHub) {
+    		& git pull origin
+    		& git push origin
+    	}
+    }
+	finally {
+		Pop-Location
+	}
+}
+@{Function = "Update-ConsoleGit"}
