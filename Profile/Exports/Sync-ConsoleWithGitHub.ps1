70c9ebe40f4a7a193f90ab51f33432194bd7a774
Paul Marston <paul@marston.me>
2014-03-30 17:07:22 +0100

    Adding discardchanges option to sync-consolewithgithub cmdlet
    Former-commit-id: b3e313b91f3ca20dd412e52470a0e7f4a707ce53 [formerly ae86bb3355ce6bb6f322235caf646ed963786a06] [formerly b2b198d217e4e77eecc8ac49b01c929d6f2068ef] [formerly a0653bb681e7f18bf263465b2043d1878293eebc] [formerly fad916a7c708ef538281b4dcb6e514cb89507fde] [formerly 8adaea2a9ead54cef6670dba45be036c8110aa36] [formerly 6f2b0ed55027c00718408e38b4f35a7a0d8bf8a1] [formerly 4c6ae3420a07a387b4e735eb37fac1136f81f0af] [formerly ee27e91193a5bb9d9e21523f40576bb8907cb643]
    Former-commit-id: 83d296b7863e38f2ae26fe0bfffd738818607105
    Former-commit-id: 79e0aa30efcfd5e482d25f63754b47427e5a54a1
    Former-commit-id: fac74a6132c60d870cfedaaaf7c12caa49086428
    Former-commit-id: 252b05a1a1eb2e88a1765c3dd0c8cdc062006c51
    Former-commit-id: dbd79a87c3327729ddca6d6e1ed8da9ed1d50efe
    Former-commit-id: 4b0d603441878fe7e6bd72180eb211cbf9c9b9fe
    Former-commit-id: f89864f6927ddaa0b1a9c35c307830f2e9bd88a2
    Former-commit-id: 768a5cd946a29ddd1829370dc4e50790849fb0d3

--- Profile/Exports/Sync-ConsoleWithGitHub.ps1
+++ Profile/Exports/Sync-ConsoleWithGitHub.ps1
@@ -1,17 +1,20 @@
 function Sync-ConsoleWithGitHub {
 	[CmdletBinding(DefaultParameterSetName = "Commit")]
 	param(
+		[Parameter(ParameterSetName = "Status")][switch]$status,
+		[Parameter(ParameterSetName = "Discard")][switch]$discardChanges,
 		[Parameter(ParameterSetName = "Commit", Position = 1)]$commitMessage,
-		[Parameter(ParameterSetName = "Commit")][switch]$dontSyncWithGitHub,
-		[Parameter(ParameterSetName = "Status")][switch]$status
+		[Parameter(ParameterSetName = "Commit")][switch]$dontSyncWithGitHub
     )
 
     Push-Location $ProfileConfig.General.InstallPath
     try {
     	if ($status) {
     		& git status
-    	}
-    	else {
+    	} else if ($discardChanges) {
+			& git clean -df
+			& git checkout .
+    	} else {
     		if (-not [string]::IsNullOrWhiteSpace($commitMessage)) {
 		    	Write-Host -ForegroundColor Cyan "Commiting change to local git..."
 		    	& git add -A
