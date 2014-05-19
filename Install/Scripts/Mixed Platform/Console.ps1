d161ecd617aa8b8c7c7424a307f59e2fbb5adeb3
Your Name <email@example.com>
2014-05-19 19:57:07 +0100

    Adding sublime text shortcut to start
    Former-commit-id: e3449c86b6e01de510a7b81d76640ff01471afbd [formerly fe9b447553e07061dcf579316dcadebee5835e17] [formerly 0f8cff9298f733ec24be2ab1bdab7ac67b453b74] [formerly 5ce503ad866e8334eeace6fdb63c69090f305f08] [formerly 5f4151849d366c212c113d8433cc9bc3dc4f72a9] [formerly 034e718d8f6a6ac4bd0dd35fcdf14b290b7a5a1b] [formerly db0597c64bbafd8a1e3384367bf828699c6f774d] [formerly 6fee796b5544779415cba9b480c91d126d851e0f] [formerly c2786b8b07972d3df3a57e9424a1feb9d7a184fd]
    Former-commit-id: 0fc6d5f27bd1b898cb6fd13feb2c55c98ee5f3fb
    Former-commit-id: 13f46e61ea82cd4c13498127a9ea40137b6aa688
    Former-commit-id: 56a963208e5bb428e9602d3ae98ec8da1f69f1a9
    Former-commit-id: 6aafeb521ea596989d040f79d48c8d36793abe1a
    Former-commit-id: 959af83d827510c25f919ef19f1e8057cee83ada
    Former-commit-id: c250c227c5e98f890c2e8bedb607ff5876edb869
    Former-commit-id: 299e0c208cdaaa9282c08d0fc36923d88f97544d
    Former-commit-id: b40947c7b1431a7484d124fdf0bba41ad67ed510

--- Install/Scripts/Mixed Platform/Console.ps1
+++ Install/Scripts/Mixed Platform/Console.ps1
@@ -4,6 +4,8 @@ $conEmuExecutable = Join-Path $InstallPath "Third Party\Console\ConEmu64.exe"
 $conEmuCommandIcon = Join-Path $InstallPath "Support Files\Icons\CommandPrompt.ico"
 $conEmuPowerShellIcon = Join-Path $InstallPath "Support Files\Icons\PowerShellPrompt.ico"
 
+$sublimeTextExecutable = Join-Path $InstallPath "Third Party\Sublime Text\sublime_text.exe"
+
 Get-ChildItem ".\Console" -Filter *.ps1 | Sort-Object Name | % { & $_.FullName }
 
 Exit-Scope
--- Install/Scripts/Mixed Platform/Console/Pin To Start And Taskbar.ps1
+++ Install/Scripts/Mixed Platform/Console/Pin To Start And Taskbar.ps1
@@ -8,6 +8,14 @@ Invoke-InstallStep "Setting up Console Start Shortcut" {
 	[System.Runtime.Interopservices.Marshal]::ReleaseComObject($WshShell) | Out-Null
 }
 
+Invoke-InstallStep "Setting up Sublime Text Start Shortcut" {
+	$WshShell = New-Object -ComObject WScript.Shell
+	$consoleShortcut = $WshShell.CreateShortcut((Join-Path ([Environment]::GetFolderPath("StartMenu")) "Sublime Text.lnk"))
+	$consoleShortcut.TargetPath = $sublimeTextExecutable
+	$consoleShortcut.IconLocation = $sublimeTextExecutable
+	$consoleShortcut.Save()
+	[System.Runtime.Interopservices.Marshal]::ReleaseComObject($WshShell) | Out-Null
+}
 
 Invoke-InstallStep "Setting up Console Jump List Tasks" {
 	& $conEmuExecutable /UpdateJumpList /Exit
