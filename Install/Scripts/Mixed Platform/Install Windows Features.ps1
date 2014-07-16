dcb0cafaa255d92ce0e428d66aef4ab5bca7a96d
Paul Marston <paulm@asos.com>
2014-07-16 12:31:05 +0100

    Adding IIS, FTP, Hyper-V PowerShell cmdlets to installed windows features
    Former-commit-id: 9039bd4258536c3b20fbb69d275fa0128d362633 [formerly ee92b38ab827d05681b3f67a99eecfb604fce55c] [formerly 5829f2283190328c35406876b8e8a5c79ecb0e1e] [formerly 549226b871a9db20d5027a4d193e2c2b3fea7294] [formerly af2ebf96b7aaaa640136d7faa24a1e1ad7bbfc88] [formerly dbdf62eca887193ffab3cb2665c8078441d19239] [formerly d2662cab8ec14dc1c618334d1ff55780be724688] [formerly 8a7d62e957cab5e67a0a2c71df036d0bd34e25da] [formerly 001b20910150ed70210ae1ffff3cc037a36a7e77]
    Former-commit-id: 9de45f53a88f807894a27ed67213a464e619c577
    Former-commit-id: 3cc37dc65ff0e554084f98b20c9b62379d64a233
    Former-commit-id: 8d8ec6fe70171d00bd2bf8c99ac2dfe44377ebe4
    Former-commit-id: 611c69a724243e3b4748c133d2b7a9bc8a7362a6
    Former-commit-id: 7e5a9e38c418f6af66c4af6f9c7504182b1f0cf3
    Former-commit-id: 58b270295f77e605b82438433dc7b2b840dfb253
    Former-commit-id: ff4e50311c2134c68a1571dc7a3ef7f295507b13
    Former-commit-id: 774e664fa6d3ab45be241a421a489f34a3e92ece

--- Install/Scripts/Mixed Platform/Install Windows Features.ps1
+++ Install/Scripts/Mixed Platform/Install Windows Features.ps1
@@ -3,13 +3,89 @@ Write-InstallMessage -EnterNewScope "Installing Windows Features...."
 function Dism-Wrapper {
 	param($FeatureName)
 	Invoke-InstallStep "Installing $FeatureName...." {
-		$output = Dism /Online /Enable-Feature /FeatureName:$FeatureName
+		$output = & dism /Online /Enable-Feature /All /FeatureName:$FeatureName
 		if ($LASTEXITCODE -ne 0) {
 			 Write-InstallMessage "DISM Error: $output"
 		}	
 	}
 }
 
+# Telnet Client
 Dism-Wrapper TelnetClient
+
+# Hyper-V Module for Windows PowerShell
+Dism-Wrapper Microsoft-Hyper-V-Management-PowerShell
+
+# .NET Framework 3.5 (includes .NET 2.0 and 3.0)
+Dism-Wrapper NetFx3
+Dism-Wrapper WCF-HTTP-Activation
+Dism-Wrapper WCF-NonHTTP-Activation
+
+# .NET Framework 4.5 Advanced Services
+Dism-Wrapper NetFx4-AdvSrvs
+Dism-Wrapper NetFx4Extended-ASPNET45
+
+# WCF Services
+Dism-Wrapper WCF-Services45
+Dism-Wrapper WCF-HTTP-Activation45
+Dism-Wrapper WCF-MSMQ-Activation45
+Dism-Wrapper WCF-Pipe-Activation45
+Dism-Wrapper WCF-TCP-Activation45
+Dism-Wrapper WCF-TCP-PortSharing45
+
+# FTP Server
+Dism-Wrapper IIS-FTPServer
+Dism-Wrapper IIS-FTPSvc
+
+# Web Management Tools
+Dism-Wrapper IIS-ManagementConsole
+Dism-Wrapper IIS-ManagementScriptingTools
+Dism-Wrapper IIS-ManagementService
+
+# World Wide Web Services
+Dism-Wrapper IIS-WebServer
+Dism-Wrapper IIS-WebServerManagementTools
+Dism-Wrapper IIS-WebServerRole
+
+# Application Development Features
+Dism-Wrapper IIS-ApplicationDevelopment
+Dism-Wrapper IIS-NetFxExtensibility
+Dism-Wrapper IIS-NetFxExtensibility45
+Dism-Wrapper IIS-ApplicationInit
+Dism-Wrapper IIS-ASPNET
+Dism-Wrapper IIS-ASPNET45
+Dism-Wrapper IIS-ISAPIExtensions
+Dism-Wrapper IIS-ISAPIFilter
+Dism-Wrapper IIS-WebSockets
+
+# Common HTTP Features
+Dism-Wrapper IIS-CommonHttpFeatures
+Dism-Wrapper IIS-DefaultDocument
+Dism-Wrapper IIS-DirectoryBrowsing
+Dism-Wrapper IIS-HttpErrors
+Dism-Wrapper IIS-HttpRedirect
+Dism-Wrapper IIS-StaticContent
+
+# Health and Diagnostics
+Dism-Wrapper IIS-HealthAndDiagnostics
+Dism-Wrapper IIS-HttpLogging
+Dism-Wrapper IIS-RequestMonitor
+Dism-Wrapper IIS-HttpTracing
+
+# Performance Features
+Dism-Wrapper IIS-Performance
+Dism-Wrapper IIS-HttpCompressionDynamic
+Dism-Wrapper IIS-HttpCompressionStatic
+
+# Security
+Dism-Wrapper IIS-Security
+Dism-Wrapper IIS-BasicAuthentication
+Dism-Wrapper IIS-DigestAuthentication
+Dism-Wrapper IIS-IPSecurity
+Dism-Wrapper IIS-RequestFiltering
+Dism-Wrapper IIS-URLAuthorization
+Dism-Wrapper IIS-WindowsAuthentication
+
+
 
 Exit-Scope
