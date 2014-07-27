e87196e2f2e55f1191fdb66c4117150ee0a23150
Paul Marston <paul@marston.me>
2014-07-27 18:55:26 +0100

    Fixing auto eula registration of some sysinternal tools. setting up PuTTY default settings to match ConEmu
    Former-commit-id: 86b73bc2e55980061e2719d2ec9593465179de28 [formerly 0f03fc517144a9104fde36e23b740ba81e8d19fc] [formerly 4257e528ea7e8f19bfac2106f2a74ff11e4ed8c8] [formerly 54a1424d1b844aca0de056a9e0fcbee91a65b878] [formerly eaa1fb50e4c2b6166ef254307e50679eb2fc45d3] [formerly 80e3cb9a25c3f5f5b9bf3a8f47aa92e8edab94e4] [formerly 2ff58ca5d83c5397354583d947715d67015d5090] [formerly ba73f7db2dbcdc822bd50c78c907272de204d078] [formerly 8023d6c5f5168fc00ebb1de1dd48725eb329e44a]
    Former-commit-id: d6901c320bd445509d08949f3018736a53b62366
    Former-commit-id: 9069d9c962ed47a5a5a3b30a1ea25ccf01144c38
    Former-commit-id: 48374dd13e135400be4160fb425a34a056c6e1b7
    Former-commit-id: c92391646a710bf3e8b6fe7e09885de4c704c302
    Former-commit-id: 42c2288515d92402f26dec4f54b52063e0aa376e
    Former-commit-id: 130c69821ad61ae7e156cc2994b9633f7143b51f
    Former-commit-id: e6e56ef194b5ae2e3d7683c0b8650333dd0f9df9
    Former-commit-id: bbb26125a3175f028b5e9dd581531fbfb7da69c8

--- Install/Scripts/Mixed Platform/Accept Sysinternals EULA.ps1
+++ Install/Scripts/Mixed Platform/Accept Sysinternals EULA.ps1
@@ -3,7 +3,11 @@ Invoke-InstallStep "Accepting Sysinternals EULA" {
 
 	New-Item "HKCU:\Software\Sysinternals" -Force | Out-Null
 
-	Get-ChildItem $sysinternalsDir -Filter *.exe | % { $_.BaseName } | % {
+	$specialEulaKeys = @(	'procexp',			'procmon')
+	$additionalEulaKeys = @('Process Explorer',	'Process Monitor')
+
+	(Get-ChildItem $sysinternalsDir -Filter *.exe | ? { $specialEulaKeys -inotcontains $_.BaseName } | % { $_.BaseName }) `
+	+ $additionalEulaKeys | % {
 		New-Item "HKCU:\Software\Sysinternals\$_" -Force | Out-Null
 		New-ItemProperty "HKCU:\Software\Sysinternals\$_" "EulaAccepted" -Value "1" -Type DWord -Force | Out-Null
 	}
