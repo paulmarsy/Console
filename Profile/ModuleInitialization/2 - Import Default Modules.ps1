489e5ed978610ca0b8d6c37f901052234d523598
Paul Marston <paul@marston.me>
2014-07-31 21:12:17 +0100

    Tweaking psreadline options
    Former-commit-id: 2a32db673e2fbf1c72f710c01e13ac012ed03203 [formerly f25707c9cc0fd9763bf39ae9b7cbde4af09c377d] [formerly 595c3700ae985db8fe1b70ff3d690f6673ad2936] [formerly 88f785d1cc2ff8190c3290f5f51beb4246054ed7] [formerly 68f41c79276f89c3a1259f171e5434d2d3ba4ff6] [formerly 8f882bae9d9285709fbdfb2d6b9fcf920c530dcd] [formerly 9fe4e7d1dd15e08c00a238e6d04237c94b42a987] [formerly 9ba4453b4d16c1e04ae209bf52a19fbd005c186b] [formerly dffbfe989388e916e92ccd07292b517d9e00b694]
    Former-commit-id: f903f242b95eb3bc189578df092fc63c3818bd11
    Former-commit-id: f93862081dc26dff73b846030fa60db0d6922ed9
    Former-commit-id: 501e107e27211f82146d9d64c28baf15589ac1ef
    Former-commit-id: 3d3d5dd8848793dbfb9ad5b65d16247204efd9b5
    Former-commit-id: f9f8934bc899332029def01a8fb8c7efefdd17a0
    Former-commit-id: 001676ea59509e2e33f9161a446a4266c1f568b1
    Former-commit-id: 0e46f21e17d9311183d6bb7746cd6b068fc8baa0
    Former-commit-id: 920febc016896502bdadacd0073c91db4fa3d1a8

--- Profile/ModuleInitialization/2 - Import Default Modules.ps1
+++ Profile/ModuleInitialization/2 - Import Default Modules.ps1
@@ -7,8 +7,10 @@ Import-Module Pscx -Global -ArgumentList @{
 # PSReadline - https://github.com/lzybkr/PSReadLine
 if ($host.Name -eq 'ConsoleHost') {
 	Import-Module PSReadline -Global
-	Set-PSReadlineKeyHandler -Key Ctrl+C -ScriptBlock {}
-	Set-PSReadlineKeyHandler -Key Ctrl+V -ScriptBlock {}
+	Set-PSReadlineOption -HistoryNoDuplicates
+	Set-PSReadlineKeyHandler -Key Ctrl+C -Function CopyOrCancelLine
+	Set-PSReadlineKeyHandler -Key Ctrl+Shift+Home -Function ScrollDisplayTop
+	Set-PSReadlineKeyHandler -Key Ctrl+Shift+End -Function ScrollDisplayToCursor
 }
 
 # GitHub Windows - http://windows.github.com/
