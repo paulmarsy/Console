3d13d2f9b771ffce49193f821e41b2ee07ca3840
Your Name <email@example.com>
2014-09-21 22:09:42 +0100

    Allowing duplicate history items
    Former-commit-id: 0343f335a3e224ee08ba7857b88cafc286642738 [formerly 3b12b8758d597db8b61ffce290836e8e4792b2c1] [formerly 0d13b466ecb86b287e35bb62b726881d4a991004] [formerly d9305bcd02cdf3bab0561030e20ef10f117bc616] [formerly 547a028585946de34df9f521fe00e4590fc7364b] [formerly 4f024c61da61d3c43d2cb66cba4abd73da5ffd03] [formerly c306b403433d085141f29ef3523b7aba6e71ab85] [formerly 3533c23589fe4d411e396fd89159e349f6f36042] [formerly 2227bdd5cd00d79691adfb960075c76c6eb046e2]
    Former-commit-id: 9ccb3ddda2ba1deaee0028051296a3fc581e2581
    Former-commit-id: 21e494a2a3d4c8d59f0a9b423df6f02b71c3cc9c
    Former-commit-id: 2c13c071a1129530f502bd9e079f6dd7a2f84a0c
    Former-commit-id: 0830027fa44f9bc354196140dd088d69a38d54a1
    Former-commit-id: cb72122aea00ce5e5dac46c07931128c8d23d024
    Former-commit-id: 6dfe014c2073b18209ced8d52be2a31b6e34642f
    Former-commit-id: d70a86aabb72dd5b54ec9526f8c2afff87d3597c
    Former-commit-id: 9894b32146cd32cf2fe19f05118b8bfe49a400ac

--- AdvancedPowerShellConsole/ModuleInitialization/2 - Default Modules/PSReadline.ps1
+++ AdvancedPowerShellConsole/ModuleInitialization/2 - Default Modules/PSReadline.ps1
@@ -1,7 +1,6 @@
 # PSReadline - https://github.com/lzybkr/PSReadLine
 if ($host.Name -eq 'ConsoleHost') {
 	Import-Module PSReadline -Global
-	Set-PSReadlineOption -HistoryNoDuplicates
 	Set-PSReadlineKeyHandler -Key Ctrl+C -Function CopyOrCancelLine
 	Set-PSReadlineKeyHandler -Key Ctrl+Shift+Home -Function ScrollDisplayTop
 	Set-PSReadlineKeyHandler -Key Ctrl+Shift+End -Function ScrollDisplayToCursor
