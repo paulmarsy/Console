e3f99da6798ae542de22bd93d45268777282336b
Paul Marston <paul@marston.me>
2014-09-28 21:46:15 +0100

    Adding messaging after the console has been restarted
    Former-commit-id: 7f5f9966b39a5e763299ac09a5d778810804c947 [formerly 4e431ccf737d06175a04a7ac802562325f7495ae] [formerly ba208e40837696fb92f62de6fff9c3da2e5ef8c6] [formerly 70ad95b67a9cf12dcb655b8a835dcdeb9aebcf7f] [formerly 523042dd4b50c1e871a4d0d126ca578649cebf5d] [formerly 8beb0a9cba0e2dbb39f4535d7a3553c702994a3c] [formerly d190ff77cec1b093e196cda039e38b7deeb2adbd] [formerly 70350e83b8a27e738c7e213eeedecf211bc83830] [formerly aced80d00eed8bf779607c4cc781ab3dec7d4c0e]
    Former-commit-id: 5be901a8fefccdfa30068a807c3901e253adcc4b
    Former-commit-id: 0b60d62266be628d5f4dcd96aab716bda0cf76c1
    Former-commit-id: b5460dd6285e30fda0f9d9adaa77c82124e723cd
    Former-commit-id: 355bd889cfe5052aa81a61801a1d31e506b434ed
    Former-commit-id: d40c2723902086efaba3e87992ccf15b334321c9
    Former-commit-id: 4f3d79f7cee100722656c95ca6c1cb3bc07421fb
    Former-commit-id: 9839ad798f798a508c72c53e331fca9a0b998331
    Former-commit-id: 983fcd2578a6ab47cc130f751c28471c55b3cfe0

--- AdvancedPowerShellConsole/Exports/Functions/Restart-Console.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/Restart-Console.ps1
@@ -1,4 +1,12 @@
 function Restart-Console {
-    Start-Process -FilePath "$InstallPath\Third Party\Console\ConEmu64.exe" -ArgumentList "/cmd powershell.exe" -WorkingDirectory $pwd
+	[CmdletBinding()]
+	param(
+		[Parameter(Position = 0)]$Message = "Advanced PowerShell Console has been restarted"
+	)
+
+	$command = "& { Write-Host -ForegroundColor Red '$Message' }"
+	$encodedCommand = [Convert]::ToBase64String(([System.Text.Encoding]::Unicode.GetBytes($command)))
+
+    Start-Process -FilePath "$InstallPath\Third Party\Console\ConEmu64.exe" -ArgumentList "/cmd powershell.exe -NoExit -EncodedCommand $encodedCommand" -WorkingDirectory $pwd
     Exit
 }
