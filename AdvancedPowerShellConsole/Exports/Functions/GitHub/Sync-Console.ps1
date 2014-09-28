4ce627b214208c0008282b3a8f2f75b3db27ea33
Paul Marston <paul@marston.me>
2014-09-28 14:35:03 +0100

    test
    Former-commit-id: 688f44961fbf715d68e504023c28f20b42e4419c [formerly 69c81097540205715ef8a647759dbb8b076de1fe] [formerly 0a05973f8f7bae04b49cca4ed58411c2aea1100b] [formerly 1e3625ad8e451a7e8b46e95c5ecdca796bf7cba7] [formerly bec53e6916ab392859e51bdb6690f1d73b467b83] [formerly a09d6d1518e2c76398699b2afd1e91201f3a4af3] [formerly 9cbc2630f298d07a9bbd26e28d4fca45c4b0ed26] [formerly 502c2017d982d616c1952ce2745c124dfc3dadbd] [formerly b657002c6bbb20d4094b816d9d4bcb8946173b8c]
    Former-commit-id: 6a748374b422bc7e5144152d5f4f553edfedad63
    Former-commit-id: b6cfa9f5aea51194dbedf5b54049a9e898de5a4b
    Former-commit-id: bdbacd2e78b37e06de6bd3b4d28240acc71b7794
    Former-commit-id: 15e7cc48e0d3b6f2061d3cc91c6411a6e4c4b38b
    Former-commit-id: 6d259d356e597bd408b154675f4f06ad025a0d11
    Former-commit-id: dabe93586b03d03ea05e327e14a7946f1b429767
    Former-commit-id: b99ea94e9bd34b511260233d58feaacb63c1a8a3
    Former-commit-id: 1cd4c930805348c277916f9073fdff39de3aa05c

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Sync-Console.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Sync-Console.ps1
@@ -3,26 +3,41 @@ function Sync-Console {
 	param(
 		[Parameter(ParameterSetName = "Commit", Position = 1)]$CommitMessage,
 		[Parameter(ParameterSetName = "Commit")][switch]$DontSyncWithGitHub,
-		[Parameter(ParameterSetName = "Discard")][switch]$DiscardChanges
+		[Parameter(ParameterSetName = "Discard")][switch]$DiscardChanges,
+		[switch]$Quiet
     )
 
     Push-Location $ProfileConfig.General.InstallPath
     try {
+    	$argumentsVerbose = @()
+    	$argumentsQuiet = @()
+
+    	if ($PSBoundParameters['Verbose']) { $argumentsVerbose += @("--verbose") }
+    	if ($Quiet) { $argumentsQuiet += @("--quiet") }
+    	$argumentsBoth = $argumentsVerbose + $argumentsQuiet
+
     	if ($DiscardChanges) {
 			& git clean -df
 			& git checkout .
     	} else {
     		if (-not [string]::IsNullOrWhiteSpace($CommitMessage)) {
-		    	Write-Host -ForegroundColor Cyan "Commiting change to local git..."
+		    	if (-not $Quiet) { Write-Host -ForegroundColor Cyan "Commiting change to local git..." }
 		    	& git add -A
 		    	& git commit -a -m $commitMessage
 	    	}
 
 	    	if (-not $DontSyncWithGitHub) {
-	    		Write-Host -ForegroundColor Cyan "Pulling changes from GitHub..."
+	    		if (-not $Quiet) { Write-Host -ForegroundColor Cyan "Pulling changes from GitHub..." }
+	    		& git remote update
+
+	    		$local = & git rev-parse `@
+	    		$remote = & git rev-parse `@`{u`}
+	    		$base = & git merge-base `@ `@`{u`}
+
+	    		if (-not $Quiet) { Write-Host -ForegroundColor Cyan "Pulling changes from GitHub..." }
 	    		& git pull --rebase origin
 	    		if ($LASTEXITCODE -ne 0) { Write-Host -ForegroundColor Red "Error!"; return; }
-	    		Write-Host -ForegroundColor Cyan "Pushing changes to GitHub..."
+	    		if (-not $Quiet) { Write-Host -ForegroundColor Cyan "Pushing changes to GitHub..." }
 	    		& git push origin
 	    		if ($LASTEXITCODE -ne 0) { Write-Host -ForegroundColor Red "Error!"; return; }
 	    	}
