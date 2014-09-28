6d97f97ef810e2e4833f72de207e45eecf78ef15
Paul Marston <paul@marston.me>
2014-09-28 15:22:37 +0100

    Fixing showing of updating github status
    Former-commit-id: 5706533e7503428e56eac4c9dc1de5e7b8ef4113 [formerly 40a3b07cbc6664c685da3d1598be9b6aa70764f3] [formerly c657b3462b66aab51f857b64d64d5da5e84964ee] [formerly 0eff80a533e74a34d675db4fc5948d9eba5bb796] [formerly 4c7f3810610b8471d5636b7bccdb6add23d1b827] [formerly 9ed3c783bac0562759881133c6cab9e89851051e] [formerly 856c204c9db705e9139335ca6b91481ce0ca94be] [formerly b1f22695017721a2be8a73ed3e9878813b4fa2d9] [formerly 0103a9391ef046bc65590eba9fed066dc5b34e4e]
    Former-commit-id: 3f295bb8e536d17400fb99b2cfea304f86251edb
    Former-commit-id: 95efaefcfbf9a61ddaf7fa9157a2d98e66dd0093
    Former-commit-id: 5768b20a5a04e8952abc4ad53d2bdd58b69432d9
    Former-commit-id: 8cbedd51e6aae00d25dd92add00a43d776b50950
    Former-commit-id: 545d09dc41f3990dcf58fd38a288edccd2d26b42
    Former-commit-id: 7344b2d8c3a5ab25e094134352194b746e1007b7
    Former-commit-id: 8e1a783ff2669c3d1c05358f56eea4ec33d1c27c
    Former-commit-id: b50ef8c75d8093d5b503ac15f8d062c013b28dc8

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
@@ -1,7 +1,7 @@
 function Show-ConsoleStatus {
     Push-Location $ProfileConfig.General.InstallPath
     try {
-    	Start-Process -FilePath "git.exe" -ArgumentList "remote update" -WindowStyle Hidden
+    	Start-Process -FilePath "git.exe" -ArgumentList "remote update" -WindowStyle Hidden -Wait
 		& git status
     }
 	finally {
