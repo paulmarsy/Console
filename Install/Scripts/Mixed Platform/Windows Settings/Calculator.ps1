5f49a509d2844001ff08c3860d6a790adcd1d2a0
Paul Marston <paulm@asos.com>
2014-09-30 15:18:48 +0100

    Fixing calc install when reg key doesnt exist
    Former-commit-id: 7fa5f2d3896bb065a58cd27fa163e831172ec032 [formerly 679595014f03789d665429bf05622d53aca52f07] [formerly 91a0704be42f24ae0d7314cc934a8e212f632834] [formerly d6e5df0d3b987ed44db8423f56cadc72f0bd5554] [formerly 4bef22c5350b8dff611f18280baaa14de5ccce59] [formerly a5f7c4713dfbd6bf04bc2d52e04671fc289d1adc] [formerly b851a801945aa34ae2646539e856eb3cc092fa0d] [formerly 1674f292fc6fcf4918a060853ea49cb1470370dc] [formerly a10b5975d1471f1d407112150aea094820d5bbab]
    Former-commit-id: 01a6a54ef6965fe7089d9f36bea425877db7bb6f
    Former-commit-id: 28740f532a86cc6b5d37a1e7722138e908c09757
    Former-commit-id: 75c10e7f50069e68aa30d113b5052c008b3d6834
    Former-commit-id: d9fbbb04e23b6a5f1a98b391f441fed0cb03f1db
    Former-commit-id: 9d6200bdc638e7b9f79305413e94c10dc396f40e
    Former-commit-id: 52b0d86c532398a69b83744617211bad6792ac66
    Former-commit-id: c08f968c5c685c266515ad57974819631c5cd610
    Former-commit-id: 62526793f8c3d90ce68efbba28713ac2ea963ff4

--- Install/Scripts/Mixed Platform/Windows Settings/Calculator.ps1
+++ Install/Scripts/Mixed Platform/Windows Settings/Calculator.ps1
@@ -1,4 +1,5 @@
 Invoke-InstallStep "Calculator - Enabling digit grouping, programmer mode and converting units" {
+	New-Item "HKCU:\Software\Microsoft\Calc" -Force | Out-Null
 	New-ItemProperty "HKCU:\Software\Microsoft\Calc" "UseSep" -Value 1 -Type DWord -Force | Out-Null
 	New-ItemProperty "HKCU:\Software\Microsoft\Calc" "layout" -Value 2 -Type DWord -Force | Out-Null
 	New-ItemProperty "HKCU:\Software\Microsoft\Calc" "UnitConv" -Value 1 -Type DWord -Force | Out-Null
