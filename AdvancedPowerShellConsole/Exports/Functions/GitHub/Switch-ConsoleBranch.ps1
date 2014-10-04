8ba7ad9b030296f8f7ac34de59599f8a006d36dc
Your Name <email@example.com>
2014-10-04 20:30:33 +0100

    wip
    Former-commit-id: 46ec3efa353ba7742a9445c44c722ce33c39d791 [formerly 8100dc13926ca6e007ee49838822b328d7c9175e] [formerly 10cf355ae4655af59a4c2d2e36c15aa8bfdf10c3] [formerly 820df0e1ef332c7f56013f9f4e34aa20c6442664] [formerly 7d5ea4e70d819172a30a854becfefe573c43239f] [formerly 1453c80dfd6d190869b30befc656eb64cce907ad] [formerly 209d2a4e62d75af7acb3bf88da26c902463b0380] [formerly 5bb238a16f953618a90b175196e143482c94fda5] [formerly 0b3290d7d9f12bf89333050593cacd69be05ab24]
    Former-commit-id: c16790fac8efc92e1f5568f206a3d59f13df6892
    Former-commit-id: 35939c4c644ecf1faa69e5c2563b6ed9d2667f16
    Former-commit-id: 02e9ce866b7a21a3efa2d435a104595bdb609577
    Former-commit-id: 39471dfda823e99cdc3762779900d2c365724a06
    Former-commit-id: 0089f261083fda06c160624bc383f61878e0e883
    Former-commit-id: d904be7aac3fc84bcfeb2fb697d057bdfb2667ea
    Former-commit-id: 6535036a5a54ff4e36d866760a2eae7dc53ccd3e
    Former-commit-id: 4e5bb8c309f75b2e985c6bfb21b9f421d4aec594

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Switch-ConsoleBranch.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Switch-ConsoleBranch.ps1
@@ -2,26 +2,28 @@ function Switch-ConsoleBranch {
 	[CmdletBinding(DefaultParameterSetName="ExistingBranch")]
 	param(
 		[Parameter(ParameterSetName="ExistingBranch", Position = 0)][ValidateSet("master")]$BranchName = "master",
-		[Parameter(ParameterSetName="NewBranch", Position = 0)]$BranchName,
+		[Parameter(ParameterSetName="NewBranch", Position = 0)]$NewBranchName,
 		[Parameter(ParameterSetName="NewBranch", Mandatory = $true)][switch]$CreateNewBranch,
 		[Parameter(ParameterSetName="NewBranch")][switch]$Force
     )
 
 	_enterConsoleWorkingDirectory {
-		param($BranchName, $Force, $PsCmdlet)
+		param($BranchName, $NewBranchName, $Force, $PsCmdlet)
 	
 		_checkBranchForUncommitedFiles
 
 		if ($PsCmdlet.ParameterSetName -eq "NewBranch") {
-			Write-Host -ForegroundColor Cyan "Creating new branch $BranchName..."
-			& git branch $BranchName (?: { $Force.IsPresent } { "--force" })
+			Write-Host -ForegroundColor Cyan "Creating new branch $NewBranchName..."
+			& git branch $NewBranchName (?: { $Force.IsPresent } { "--force" })
 			
 			Write-Host -ForegroundColor Cyan "Publishing branch to GitHub..."
-			& git push -u origin $BranchName
+			& git push -u origin $NewBranchName
 			Sync-Console
+
+			$BranchName = $NewBranchName
 		}
 
-		Write-Host -ForegroundColor Cyan "Switching to $(?: { $Force.IsPresent } { "new " })branch..."
+		Write-Host -ForegroundColor Cyan "Switching to $(?: { $CreateNewBranch.IsPresent } { "new " })branch $BranchName..."
 		& git checkout $BranchName
-    } @($BranchName, $Force, $PsCmdlet)
+    } @($BranchName, $NewBranchName, $Force, $PsCmdlet)
 }
