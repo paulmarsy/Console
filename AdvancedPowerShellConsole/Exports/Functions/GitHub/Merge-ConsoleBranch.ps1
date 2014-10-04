0d98361b673a171e91c2982b785a86e9c0803c76
Your Name <email@example.com>
2014-10-04 21:06:36 +0100

    wip
    Former-commit-id: b9fd0c311edb4ebb6a53cf8d4a557e10cece06fd [formerly c6fb05969d44ac9ffaf7a6929d88d5ffeec8658e] [formerly e3db0bb16b9837d9b2c439ec6eace003120a8fe5] [formerly 2960714b14a26fae793404b784e7ba7e9a1320ec] [formerly 3e9577d58574bfa828d9108774aa7ce347e35bcb] [formerly f7c22632513f3300701394c13605bf3f40e190a2] [formerly 7644c8270b45fc53166c236d68b0ec922409fbad] [formerly f1df301a5028b1beac4d895a3b3f3e93366cc15f] [formerly 2f193ed3b9fe7e279645b79f1ee3c81f25ff6ab0]
    Former-commit-id: 07fd1088ae751fca5c68a70b5630d38554cbd122
    Former-commit-id: 40ed0c73ad99c6a2f472ff7eea51e05228aa7ca2
    Former-commit-id: 1019d4518dc7fc2056cef77cb3bde5a4217433fb
    Former-commit-id: 1093ed8334f2761f23f77ad86557fc9128b6feac
    Former-commit-id: 13528ae079ddc3f648ed0f7369f02685e6dd09af
    Former-commit-id: 5264ace6fe0843367248df84ba4c2f53b44515c7
    Former-commit-id: 93c26f3388da42bcfc38b380253ce09b31a91fa0
    Former-commit-id: cfdfbb0408bfaa99d7a3f527740e4378822b7b00

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Merge-ConsoleBranch.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Merge-ConsoleBranch.ps1
@@ -1,5 +1,4 @@
-function Publish-ConsoleBranch {
-	[CmdletBinding(DefaultParameterSetName="ExistingBranch")]
+function Merge-ConsoleBranch {
 	param(
 		[Parameter(Mandatory=$true,Position=0)][ValidateSet("master")]$ChildBranchName,
 		[Parameter(Position=1)][ValidateSet("master")]$ParentBranchName = "master"
