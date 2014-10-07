35890788eaa0b60f33a88e03dc398707c952c935
Your Name <email@example.com>
2014-10-08 00:16:00 +0100

    Making invkoe-ternary available to profileconfig module
    Former-commit-id: 3490c477e72594f58aa1c1b48812661beb8803ba [formerly 49c0933186d610b01ff95eb1f02e637ba76fcf6c] [formerly 0c634ec035db1db75c98064e6ed49d26309d1498] [formerly ee57967075a75ee8700fa23bc96eff7c8eecc080] [formerly b398240f3afd5334fbe5e12b81c15fc32ee3e543] [formerly 5d2bdcdd39ce7f5a6681d8a6921d761e1bd1da68] [formerly 46b9494e5a2f79d5265c81d20fe054b12d03e410] [formerly d4c6a2c2985d7621d7c25c45edaff5a9ce9b08fe] [formerly c1dd1bb6167a2bae9064af55bc525e8390b51de9]
    Former-commit-id: 59ac1e39b9c70227de0cb943ba2660279263857e
    Former-commit-id: 49b525969783789d2bf4f2359021c2bc01ec85e7
    Former-commit-id: 4f44943d07b4615fec1c2c522c329e5af2a87b4c
    Former-commit-id: 061163bc7532dc4c0ac87c5056086edafb46beb5
    Former-commit-id: b6ac242654a142c6a33d588c314ad265b9a3bb50
    Former-commit-id: 598ff4bc6a5e0e29be545307056ab0eb6882f611
    Former-commit-id: 314f793d786a4ea837f90f47419f69f2a8183ae2
    Former-commit-id: 49b00ee5a389bc1ab2c20ede684c0beb809d4096

--- AdvancedPowerShellConsole/ProfileConfig/ProfileConfig.psm1
+++ AdvancedPowerShellConsole/ProfileConfig/ProfileConfig.psm1
@@ -1,5 +1,8 @@
 param($InstallPath)
 
+. (Join-Path $InstallPath "AdvancedPowerShellConsole\Exports\Functions\Invoke-Ternary.ps1")
+. (Join-Path $InstallPath "AdvancedPowerShellConsole\Exports\Aliases\U+003F`&U+003A.ps1")
+
 $profileConfigFile = Join-Path (Split-Path $PROFILE.CurrentUserAllHosts) "ProfileConfig.xml"
 
 Get-ChildItem $PSScriptRoot -Filter *.ps1 | % { . $_.FullName }
