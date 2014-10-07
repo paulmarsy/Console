aaea93e37bac684512422c596da343c535ee8445
Your Name <email@example.com>
2014-10-07 01:09:49 +0100

    Tweaking install for different windows versions
    Former-commit-id: ec36ae704bed6b68e630485651352376c9e85c7a [formerly 1ce45cad368602eaac7fa7875e69778995905733] [formerly 4968c6796a85256ac8d5ba3f59ece17271ade844] [formerly 3da3473331d62c9a13767f32b1b323ecfb94717d] [formerly 0a9facd98514e5a6acd4817340a3aa7efe6a15d4] [formerly 14c96803c47c44213b78a1eed31da8f2a9bd1b46] [formerly daea47a68a8cf2111aa74260327f02c6380ee0ff] [formerly 8dd9e905726cac294cc985336880e23b46c5297b] [formerly e014fa6563d00381abb4198fd7acb76459679e88]
    Former-commit-id: 665c750ea07aa344ee419b61bf04a7d60a2d2fdf
    Former-commit-id: 167389e939b0a6ffa325dd69773d38608c992bc6
    Former-commit-id: 2241c8d95d2de6f8e85271471f1fc3f06de5da5e
    Former-commit-id: 5da20ab794915e62b04cdd4531e0d82f5e0c8e08
    Former-commit-id: 0bf870f0945508af9951c170dfed5c4e5f3780aa
    Former-commit-id: e51ecfc998af5fb4728cc958d47ced88a8dd4735
    Former-commit-id: fbf05205f9a560562cfefe495c7ad228e7f01fee
    Former-commit-id: f4fc7327f1fbeb875de84fa90106a425f7a09636

---
+++ Install/Scripts/Mixed Platform/Windows Settings/Windows Version Specific Changes.ps1
@@ -0,0 +1,22 @@
+Write-InstallMessage -EnterNewScope "Installing Windows version specific changes"
+
+$windowsVersion = [System.Environment]::OSVersion.Version
+
+$windowsChangeSet = $null
+if ($windowsVersion.Major -eq 6) {
+	if ($windowsVersion.Minor -eq 4) {
+		$windowsChangeSet = "Windows 10"
+	} elseif ($windowsVersion.Minor -in @(1, 2, 3)) {
+		$windowsChangeSet = "WIndows 7, 8 & 8.1"
+	}
+}
+
+if ($null -eq $windowsChangeSet) {
+	Write-InstallMessage -Type Info "No version specific for your operating system ($([System.Environment]::OSVersion.VersionString))"
+} else {
+	Invoke-InstallStep "Installing Windows changes for $windowsChangeSet" { 
+		Get-ChildItem -Path "$(Join-Path ".\Version Specific Changes" $windowsChangeSet)" -Filter *.ps1 | Sort-Object Name | % { & $_.FullName }
+	}
+}
+
+Exit-Scope
