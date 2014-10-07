38d95daee4e31f53a250c40dabd29ff4b86a764c
Your Name <email@example.com>
2014-10-08 00:14:41 +0100

    Moving data protection cmdlets to their own folder
    Former-commit-id: d33a0c3bf82c6eb42428176521f7ad1028afa7f2 [formerly 29625708fd98a1cf7d3e2c715c9a6a20c79856e6] [formerly 8a5018ab858346a757b3c23fc1edbfdce039ea99] [formerly 5213019ebc4a953cdae25ac7b215ba21146d5b12] [formerly e20eb2e26cb607a9277ebc33e8ebb455797dc98f] [formerly dce5111762f2bc5e21fe41fd6c3f60fd64cb8ada] [formerly 0c840ae850ddf3a2a378af81cee023453b010c1b] [formerly d574ecebf948e34d7358227800cd3ea6318cce4c] [formerly 14c4356aa889335b7d70e17a424e70b832c83e7d]
    Former-commit-id: 5c5481ee4c1a17212c7e25faad6b7067aceeb0c3
    Former-commit-id: 76d0b33c5ba707fd75ad3564027665afdfc18519
    Former-commit-id: 012dac1ad6be5fc14266feb9e0241bc406c9a76d
    Former-commit-id: 7652b4ccf55fd27766e00a5f9ccf494f6f095b1b
    Former-commit-id: 160a94318ff70c73afdf3decad4caf236850f980
    Former-commit-id: fe793681023fc9d8c860ca53eaf48450edff739f
    Former-commit-id: d2ca842905bad90e6c18bc800a6667038a0bd0dd
    Former-commit-id: a6e46dd0123aad243f106884e857b686ecdb4b31

--- AdvancedPowerShellConsole/Exports/Functions/DataProtection/Protect-Object.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/DataProtection/Protect-Object.ps1
@@ -1,11 +1,11 @@
 function Protect-Object {
 	[CmdletBinding()]
 	param(
-		[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]$Object,
+		[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]$InputObject,
         [System.Security.Cryptography.DataProtectionScope]$Scope = [System.Security.Cryptography.DataProtectionScope]::CurrentUser
 	)
 
-	$jsonRepresentation = ConvertTo-Json -InputObject $Object -Compress -Depth ([Int32]::MaxValue)
+	$jsonRepresentation = ConvertTo-Json -InputObject $InputObject -Compress -Depth ([Int32]::MaxValue)
 
 	return (Protect-String -InputObject $jsonRepresentation -Scope $Scope)
 }
--- AdvancedPowerShellConsole/Exports/Functions/DataProtection/UnProtect-File.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/DataProtection/UnProtect-File.ps1
@@ -1,27 +1,27 @@
 function UnProtect-File {
 	[CmdletBinding()]
 	param(
-		[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][ValidateScript({Test-Path $_})] $path,
+		[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][ValidateScript({Test-Path $_})]$Path,
         [System.Security.Cryptography.DataProtectionScope] $scope = [System.Security.Cryptography.DataProtectionScope]::CurrentUser,
         [Parameter(ParameterSetName = "Backup")][switch]$SaveProtectedFile,
-        [Parameter(ParameterSetName = "Backup")]$backupExtension = "protected",
+        [Parameter(ParameterSetName = "Backup")]$BackupExtension = "protected",
         [Parameter(ParameterSetName = "Backup")][switch]$Force
 	)
 
-	$path = Resolve-Path $path | select -ExpandProperty Path
+	$Path = Resolve-Path $Path | select -ExpandProperty Path
 
 	if ($SaveProtectedFile) {
-		$backupFile = [IO.Path]::ChangeExtension($path, $backupExtension)
+		$backupFile = [IO.Path]::ChangeExtension($Path, $BackupExtension)
 		if (-not $force -and (Test-Path $backupFile)) {
 			throw "Backup file ($backupFile) already exists, specify -Force parameter to overwrite"
 		}
 
-		Copy-Item -Path $path -Destination $backupFile -Force
+		Copy-Item -Path $Path -Destination $backupFile -Force
 	}
 
- 	$encryptedBytes = [System.IO.File]::ReadAllBytes($path)
+ 	$encryptedBytes = [System.IO.File]::ReadAllBytes($Path)
 
 	$unEncryptedBytes = [System.Security.Cryptography.ProtectedData]::UnProtect($encryptedBytes, $null, $scope)
 	
-	[System.IO.File]::WriteAllBytes($path, $unEncryptedBytes)
+	[System.IO.File]::WriteAllBytes($Path, $unEncryptedBytes)
 }
