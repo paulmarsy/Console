6e6e86aac66f8c85d3277f854fa71f9b5fa581fb
Paul Marston <paul@marston.me>
2014-10-08 21:04:17 +0100

    Refactoring how user scripts are imported
    Former-commit-id: 919ff4b85ced597710df73a8eef59a780b5652fd [formerly 910a9bc3a4b03776d1f2c617cf6fdeef66fb9466] [formerly a598fa6c7cd3613030711e10f05d24251af0cfd7] [formerly add00126c1b9385a795998c0182ca8f97734ed3a] [formerly d3acef48716aa7734f208141706864eee8e17ff7] [formerly b34c7f50bf823a444be9861f940e34d248f2524d] [formerly e7f5c76fd4650335e2c91955f8896e9618a28724] [formerly b933dba078ee78098a0dae79944b9e62d816cc1b] [formerly 5ac5410d6c2be7b4a5912dbadc025b0d0f0c4273]
    Former-commit-id: 9af2fafd816b83faff104ce62c109f5feda45189
    Former-commit-id: b84c0af356c3a8d5f38d18ce694c9a5245e16891
    Former-commit-id: 070386213a33a926246005f7888b5ade7d779beb
    Former-commit-id: 2c9c9cb4a1b6750822e76fac35978b5e1a15774e
    Former-commit-id: d7a431f334e3fdcc6ed8a987f02ea69eeaa51b6e
    Former-commit-id: 946e95fea066f20f943d22672b9778a387cd56ba
    Former-commit-id: 048da33f5ab7712e8daeef22e66bf97bcef4c643
    Former-commit-id: df875c0c4f48897b2e8c8b4d5ef6b736d1e5a3f6

---
+++ AdvancedPowerShellConsole/AdvancedPowerShellConsole.psm1
@@ -0,0 +1,35 @@
+param($InstallPath)
+
+Set-StrictMode -Version Latest
+
+$machineIpProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()
+Write-Host -NoNewline "`t"
+Write-Host -NoNewline -ForegroundColor Cyan ([Environment]::UserDomainName)
+Write-Host -NoNewline -ForegroundColor Gray "\"
+Write-Host -NoNewline -ForegroundColor Green ([Environment]::UserName)
+Write-Host -NoNewline -ForegroundColor Gray " on "
+Write-Host -NoNewline -ForegroundColor Red $machineIpProperties.HostName
+if (-not [string]::IsNullOrWhiteSpace($machineIpProperties.DomainName)) {
+    Write-Host -NoNewline -ForegroundColor Red ".$($machineIpProperties.DomainName)"
+}
+Write-Host ([Environment]::NewLine)
+
+Import-Module (Join-Path $PSScriptRoot "ProfileConfig") -ArgumentList $InstallPath -Global
+
+Get-ChildItem (Join-Path $PSScriptRoot "ModuleInitialization") -Filter *.ps1 -Recurse | Sort-Object FullName | % { & $_.FullName }
+
+$functions = Get-ChildItem (Join-Path $PSScriptRoot "Exports\Functions") -Filter *.ps1 -Recurse
+$aliases = Get-ChildItem (Join-Path $PSScriptRoot "Exports\Aliases") -Filter *.ps1 -Recurse
+
+$functions + $aliases | % { . $_.FullName }
+
+Sync-ConsoleWithGitHub -DontPushToGitHub -UpdateConsole Auto
+
+$exportExclusionPattern = "_*.ps1"
+
+Export-ModuleMember -Function ($functions | ? { $_ -notlike $exportExclusionPattern } | % { $_.BaseName })
+Export-ModuleMember -Alias  ($aliases | ? { $_ -notlike $exportExclusionPattern } | % { $_.BaseName })
+
+& (Join-Path $PSScriptRoot "Import Custom PowerShell Scripts.ps1") -ExportExclusionPattern $exportExclusionPattern
+
+Write-Host -ForegroundColor Green "Advanced PowerShell Console Module successfully loaded"
---
+++ AdvancedPowerShellConsole/Import Custom PowerShell Scripts.ps1
@@ -0,0 +1,23 @@
+param($ExportExclusionPattern)
+
+$powerShellScriptsFolder = Join-Path ([System.Environment]::GetFolderPath("MyDocuments")) "PowerShell Scripts"
+$includeFile = Join-Path $powerShellScriptsFolder "include.ps1"
+if ((Test-Path $includeFile) -and -not ([String]::IsNullOrWhiteSpace([IO.File]::ReadAllText($includeFile)))) {
+    Write-Host "Loading include file $includeFile..."
+    $referenceFunctions =  Get-ChildItem function: | Select-Object -ExpandProperty Name
+    $referenceAliases = Get-ChildItem alias: | Select-Object -ExpandProperty Name
+    try {
+        Push-Location $powerShellScriptsFolder
+        . $includeFile
+    }
+    finally {
+        Pop-Location
+    }
+    $differenceFunctions =  Get-ChildItem function: | Select-Object -ExpandProperty Name
+    $differenceAliases = Get-ChildItem alias: | Select-Object -ExpandProperty Name
+
+    $includedFunctions = Compare-Object -ReferenceObject $referenceFunctions -DifferenceObject $differenceFunctions | Select-Object -ExpandProperty InputObject
+    $includedFunctions | ? { $_ -notlike $ExportExclusionPattern -and (Get-Command $_).ModuleName -eq "AdvancedPowerShellConsole" } | % { Write-Host "Importing function $_..."; Export-ModuleMember -Function $_ } 
+    $includedAliases = Compare-Object -ReferenceObject $referenceAliases -DifferenceObject $differenceAliases | Select-Object -ExpandProperty InputObject
+    $includedAliases | ? { $_ -notlike $ExportExclusionPattern -and (Get-Command $_).ModuleName -eq "AdvancedPowerShellConsole" } |  % { Write-Host "Importing alias $_...";  Export-ModuleMember -Alias $_ }
+}
