7f7bbe1e13eea926eb900243e37e1248a397f911
Paul Marston <paulm@asos.com>
2014-10-09 16:27:24 +0100

    bug fixing
    Former-commit-id: 2cb285ebc02e306fb0eaa9b362003f11306943fd
    Former-commit-id: 54936ee6b01f34efaa7cb0b4c1dc3d331d5cef58
    Former-commit-id: 6a8c7679d2b78b1b22184b0d6da9f88210109401
    Former-commit-id: 809d8580287b882a86126c72498f8ab3c58ed16c
    Former-commit-id: 886dc9b50de3c26d04b3561522d8df747bce8c55
    Former-commit-id: abdbdeffe660fb392952df3c4291ddf365612c09
    Former-commit-id: 6059aca9c13fd6465f22d7747f5ffe13713d0293
    Former-commit-id: 4cf3b0078e57d83606a6e7b5861adb850051afee
    Former-commit-id: a91f192461c42eeb09e625011d168c729e63f974

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Switch-ConsoleBranch.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Switch-ConsoleBranch.ps1
@@ -8,11 +8,11 @@ function Switch-ConsoleBranch {
     )
 
 	_workOnConsoleWorkingDirectory {
-		param($BranchName, $CreateNewBranch, $ParentBranchName, $NewBranchName, $PsCmdlet)
+		param($BranchName, $CreateNewBranch, $ParentBranchName, $NewBranchName)
 	
 		if ((Assert-ConsoleIsInSync -Quiet -AssertIsFatal) -eq $false) { return }
 
-		if ($PsCmdlet.ParameterSetName -eq "NewBranch") {
+		if ($CreateNewBranch) {
 			_invokeGitCommand "checkout $ParentBranchName"
 
 			Write-Host -ForegroundColor Cyan "Creating remote branch $NewBranchName on GitHub..."
@@ -26,7 +26,7 @@ function Switch-ConsoleBranch {
 			$BranchName = $NewBranchName
 		}
 
-		Write-Host -ForegroundColor Cyan "Switching to $(?: { $CreateNewBranch.IsPresent } { "new " })branch $BranchName..."
+		Write-Host -ForegroundColor Cyan "Switching to $(?: { $CreateNewBranch } { "new " })branch $BranchName..."
 		_invokeGitCommand "checkout $BranchName"
-    } @($BranchName, $CreateNewBranch, $ParentBranchName, $NewBranchName, $PsCmdlet)
+    } @($BranchName, $CreateNewBranch, $ParentBranchName, $NewBranchName)
 }
