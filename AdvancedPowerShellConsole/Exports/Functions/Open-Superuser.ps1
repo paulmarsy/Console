e600dfd2cea0f8f4bee10dbc307a83f634d77465
Paul Marston <paul@marston.me>
2014-10-09 21:18:46 +0100

    Refactoring a lot of stuff
    Former-commit-id: 3e0e69a08c48b0e9d40dbb2ddec0d962ad88ddae
    Former-commit-id: e0743777df11e6ef7d6058b2a4471891afe00e71
    Former-commit-id: fffaa17c9701d3954284e66c27d1011e94f038e8
    Former-commit-id: 4702ab2d2e321cf5d813a6778c24b94041c3bac4
    Former-commit-id: 24fef07d475d3627e9da2ddf2b5919a50cb15ef4
    Former-commit-id: 84a58053a73ea5615145b229dfc67adf134dd25f
    Former-commit-id: 290832282a1ff6dec8db57a2736e7c2f36e724dd
    Former-commit-id: c25683c254e9e67b68b2d0051d21fe7f0f8892b6
    Former-commit-id: 989a6967ead7065921107decf98c1df9c0ff0ee6

--- AdvancedPowerShellConsole/Exports/Functions/Open-Superuser.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/Open-Superuser.ps1
@@ -1,5 +1,4 @@
 function Open-Superuser
 {
-    Start-Process -FilePath "$InstallPath\Third Party\Console\ConEmu64.exe" -ArgumentList "/cmd powershell.exe -cur_console:na" -WorkingDirectory $pwd
-    Exit
+	Restart-Console -Su
 }
