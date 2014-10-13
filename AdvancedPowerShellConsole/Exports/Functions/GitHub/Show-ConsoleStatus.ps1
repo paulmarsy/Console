b1366923a8071ec081351855a5cce048b9b8ef18
Paul Marston <paul@marston.me>
2014-10-13 23:35:42 +0100

    adding viewing branch structure
    Former-commit-id: f21c3ebc5a1883c0daa70864ddb570e76a7c6c2f
    Former-commit-id: cf9a0c37d2d4b41cd5b41f9f83130f0a9d268c24
    Former-commit-id: 89d9e0f22677e5b4ca3c95ac099a010f51d38e51
    Former-commit-id: 3c7a4d9df9199e5ae0b6d1a1832186300a166573
    Former-commit-id: d7533b79ac95ae1e6de831dce3c3121a99f966ec
    Former-commit-id: 3d70c300f0c379ebfc20c416760006b62256cbe6
    Former-commit-id: ce634e5755daf64468f638c162e190bd8b98bc85
    Former-commit-id: 9df0d2ecf232014bc51a475a833777b475210bbe
    Former-commit-id: f517d58bf45e212c2a8ab2a826816484940052d4

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
@@ -1,6 +1,7 @@
 function Show-ConsoleStatus {
 	[CmdletBinding()]
 	param(
+		[switch]$Detailed.
 		[switch]$IncludeIgnored
     )
 
@@ -18,6 +19,11 @@ function Show-ConsoleStatus {
 		_invokeGitCommand "branch --list"
 
 		Write-Host -ForegroundColor Cyan "`nBranch structure..."
+		if ($Detailed) {
+			_invokeGitCommand "log --color --date-order --graph --oneline --decorate --all"
+		} else {
+			_invokeGitCommand "log --color --date-order --graph --oneline --decorate --simplify-by-decoration --all"
+		}
 
 		Write-Host -ForegroundColor Cyan "`nShow uncommited changes..."
 		_invokeGitCommand "status --short --branch --untracked-files=all $(?: { $IncludeIgnored } { "--ignored" })"
