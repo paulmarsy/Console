b513cc53d52bc9730bcc373a3784c21b172fae13
Paul Marston <paul@marston.me>
2014-10-14 00:34:57 +0100

    adding and refactoring showing console git branch structure
    Former-commit-id: eaa8f7679af48a82cf0658762d115f5670015a02
    Former-commit-id: 485f5221de250b5f9cade30fdcce8af85d2964e1
    Former-commit-id: d38ab655a28708dc053d33fca923347e91c83e51
    Former-commit-id: ba6c6158893a6c5789a38b0ce2b2f681a77cfaae
    Former-commit-id: 9d3d6bc6c9b677f0cec1a197384c8704bc0d916f
    Former-commit-id: 5fdbfe469ef720c6ac33190dc1814bbad654e822
    Former-commit-id: b736c8394118358265e6f409cd8261b78621f64b
    Former-commit-id: b1cd9eb58bd238421818de44ea59449f7f19f05a
    Former-commit-id: 44a36be0920ee3f9a9c0185fcd26dadbec1d5a24

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
@@ -2,7 +2,8 @@ function Show-ConsoleStatus {
 	[CmdletBinding()]
 	param(
 		[switch]$Detailed,
-		[switch]$IncludeIgnored
+		[switch]$IncludeIgnored,
+		[switch]$OnlyThisBranch
     )
 
 	_workOnConsoleWorkingDirectory {
@@ -12,18 +13,23 @@ function Show-ConsoleStatus {
 		Write-Host -ForegroundColor Cyan "Currently on branch:"
 		Write-Host -ForegroundColor Red "`t$(_getCurrentBranch)"
 
+		if (-not $OnlyThisBranch) {
 		Write-Host -ForegroundColor Cyan "`nRemote tracking branches..."
 		_invokeGitCommand "branch --remotes"
 
 		Write-Host -ForegroundColor Cyan "`nLocal branches..."
 		_invokeGitCommand "branch --list"
+		}
 
 		Write-Host -ForegroundColor Cyan "`nBranch structure..."
-		if ($Detailed) {
-			_invokeGitCommand "log --color --date-order --graph --oneline --decorate --all"
-		} else {
-			_invokeGitCommand "log --color --date-order --graph --oneline --decorate --simplify-by-decoration --all"
+		$branchStructureCommand = "log --color --date-order --graph --oneline --decorate --no-walk"
+		if (-not $OnlyThisBranch) {
+			$branchStructureCommand += " --all"
 		}
+		if (-not $Detailed) {
+			$branchStructureCommand += " --simplify-by-decoration"
+		}
+		_invokeGitCommand $branchStructureCommand
 
 		Write-Host -ForegroundColor Cyan "`nShow uncommited changes..."
 		_invokeGitCommand "status --short --branch --untracked-files=all $(?: { $IncludeIgnored } { "--ignored" })"
--- AdvancedPowerShellConsole/ModuleInitialization/3 - General/Configure Git.ps1
+++ AdvancedPowerShellConsole/ModuleInitialization/3 - General/Configure Git.ps1
@@ -4,12 +4,13 @@ function UpdateGit {
     )
     & git config $config user.name $ProfileConfig.Git.Name
     & git config $config user.email $ProfileConfig.Git.Email
-    & git config $config core.ignorecase $True
-    & git config $config core.autocrlf $True 
+    & git config $config core.ignorecase true
+    & git config $config core.autocrlf true 
     & git config $config core.editor "'$(Join-Path $InstallPath "Third Party\Sublime Text\sublime_text.exe")' -w"
-    & git config $config diff.renames $True
+    & git config $config diff.renames true
     & git config $config diff.tool bc4
     & git config $config merge.tool bc3
+    & git config $config --add pager.log false
 }
 
 <#
