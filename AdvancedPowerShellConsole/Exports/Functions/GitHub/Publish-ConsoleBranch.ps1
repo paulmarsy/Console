2d288b0f955696dd40947097d828c315e19d4b07
Paul Marston <paul@marston.me>
2014-10-14 01:15:06 +0100

    refactorign
    Former-commit-id: d3dbdc05f4a52ac47ea85ea648d04fd5cc1ebe63
    Former-commit-id: ff7d68f05b3fc01752e7629223f77ce036b5d0f2
    Former-commit-id: 807d13e34af33adea44e517d02e38e035ceb720b
    Former-commit-id: 9c0b0240bebe9c8ca0410af4783bd092ae0ab17f
    Former-commit-id: 82751de1ed6c490699cbbdca101b17880468fdc2
    Former-commit-id: b9c3e1d6da71d01425a2c3577f5def8e2dabeaf6
    Former-commit-id: 608f545bc0465eba7d32228a60d34855dca40be9
    Former-commit-id: 99381b335330d8b855b5216c4ccd06c5439f1871
    Former-commit-id: 0e3a8869b407b6d970ba6a661accbdb27e75395a

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Publish-ConsoleBranch.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Publish-ConsoleBranch.ps1
@@ -7,9 +7,17 @@ function Publish-ConsoleBranch {
 	_workOnConsoleWorkingDirectory {
 		if ((Assert-ConsoleIsInSync -Quiet -AssertIsFatal) -eq $false) { return }
 
-		Merge-ConsoleBranch -SourceBranchName $ChildBranchName -DestinationBranchName $ParentBranchName
+		Sync-ConsoleWithGitHub
 
-		Switch-ConsoleBranch -BranchName $ParentBranchName
+		Merge-ConsoleBranch -SourceBranchName "origin/$ParentBranchName" -DestinationBranchName $ParentBranchName -DontSyncWithGitHub
+		Merge-ConsoleBranch -SourceBranchName $ParentBranchName -DestinationBranchName $ChildBranchName -DontSyncWithGitHub
+		Merge-ConsoleBranch -SourceBranchName "origin/$ChildBranchName" -DestinationBranchName $ChildBranchName -DontSyncWithGitHub
+		Merge-ConsoleBranch -SourceBranchName $ChildBranchName -DestinationBranchName "origin/$ChildBranchName" -DontSyncWithGitHub
+		Merge-ConsoleBranch -SourceBranchName $ChildBranchName -DestinationBranchName $ParentBranchName -DontSyncWithGitHub
+		Merge-ConsoleBranch -SourceBranchName $ParentBranchName -DestinationBranchName "origin/$ParentBranchName" -DontSyncWithGitHub
+		
+		Switch-ConsoleBranch -BranchName $ChildBranchName; Sync-ConsoleWithGitHub
+		Switch-ConsoleBranch -BranchName $ParentBranchName; Sync-ConsoleWithGitHub
 
 		Write-Host -ForegroundColor Cyan "Deleting branch $ChildBranchName..."
 		_invokeGitCommand "branch -d $ChildBranchName"
