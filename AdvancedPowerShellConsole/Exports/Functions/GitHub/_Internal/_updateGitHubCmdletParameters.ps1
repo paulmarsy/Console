8bf9fba5cf59831c7ce4c260e550dd0fc823d289
Paul Marston <paul@marston.me>
2014-10-14 01:19:59 +0100

    refactorign
    Former-commit-id: df4579640b91734848c1b8be29b8f98e1d5ee66f
    Former-commit-id: 7ebef2d915f8f0c92e2507be82e56564f9ac2dad
    Former-commit-id: f486168bfea765eaab29b723054684fe786d9b25
    Former-commit-id: 46f7933d0e139ebe5c2704ac79699e54baf146d5
    Former-commit-id: 6a1dbdfec1952fef68f98029e07674ded45c2783
    Former-commit-id: 902c7a8f44cc2fbeb20890f558e0436e990fa0ad
    Former-commit-id: a52237bc5fcdf8324fb53cb0ba248590270a4769
    Former-commit-id: 2f9bc6b8618f569f0990e2f0b2aaede22fe07cb9
    Former-commit-id: ba5abd5baba1ad994af8ffb61b301d301207fcb1

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitHubCmdletParameters.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitHubCmdletParameters.ps1
@@ -1,7 +1,9 @@
 function _updateGitHubCmdletParameters {
     _workOnConsoleWorkingDirectory {
         # Update the cmdlet's ValidateSet's with local branches
-        $localBranchNames =  & git branch | % { $_.Remove(0, 2) }
+        $localBranchNames =  & git branch -l | % { $_.Remove(0, 2) }
+        $remoteBranchNames =  & git branch -l | % { $_.Remove(0, 2) } | ? { -not $_.StartsWith("origin/HEAD") -and $_ -notlike "origin/pr/*"}
+        $branchNames = $localBranchNames + $remoteBranchNames
         if ($LASTEXITCODE -ne 0) { throw "Git command returned exit code: $LASTEXITCODE" }
         
         function updateGitHubCmdletValidateSetParameters {
@@ -10,7 +12,7 @@ function _updateGitHubCmdletParameters {
             $ValidValuesField = [System.Management.Automation.ValidateSetAttribute].GetField("validValues", [System.Reflection.BindingFlags]::NonPublic -bor [System.Reflection.BindingFlags]::Instance)
 
             Get-Command -Name $CmdletName | % { $_.Parameters.Values } | ? { $Parameters -contains $_.Name } | % { $_.Attributes } | ? { $_ -is [System.Management.Automation.ValidateSetAttribute] } | % {
-                $ValidValuesField.SetValue($_, ([string[]]$localBranchNames))
+                $ValidValuesField.SetValue($_, ([string[]]$branchNames))
             }
         }
 
