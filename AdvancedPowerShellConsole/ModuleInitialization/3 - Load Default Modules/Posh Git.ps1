0f651406a4cde40c4fbe2defbb636a5e326e5a8e
Paul Marston <paul@marston.me>
2014-10-16 21:47:38 +0100

    Changing module initialisation
    Former-commit-id: f97a18b67866b9982bf44694f44ed3d861b91920
    Former-commit-id: d3525e42ddd4289691c0fe2f3fd5fde6c7c185a3
    Former-commit-id: 6a7df3e7b56f5d02b617dddd03e362974c877980
    Former-commit-id: 5bc5c4c561dc678632cf8e75397c50d5ead97877
    Former-commit-id: baba55bbad242c78185921f78659baba17adb55f
    Former-commit-id: 9545b768369eb7032b4692ececf5b1cea33ef896
    Former-commit-id: bbecbae2f53f0f9ddb316247bd855c948a724b27
    Former-commit-id: 825da5083b05aa91a0ef44b7d4de786a948aed02
    Former-commit-id: b58f9eeeecd0c2224c13f23eadb632cdf3e5829c

---
+++ AdvancedPowerShellConsole/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
@@ -0,0 +1,33 @@
+# GitHub Windows - http://windows.github.com/
+& (Join-Path $env:LOCALAPPDATA "GitHub\shell.ps1") -SkipSSHSetup
+Import-Module posh-git -Global
+$GitPromptSettings.EnableWindowTitle = "Windows PowerShell - Git - "
+Enable-GitColors
+Start-SshAgent -Quiet
+
+$eventJob = Register-EngineEvent -SourceIdentifier ([System.Management.Automation.PsEngineEvent]::Exiting) -Action {
+    Stop-SshAgent
+}
+$ExecutionContext.SessionState.Module.OnRemove = {
+	$eventJob | Stop-Job -PassThru | Remove-Job
+}.GetNewClosure()
+
+$gitConfigFileLocation = Join-Path $ProfileConfig.General.PowerShellAppSettingsFolder "Gitconfig.config"
+function SetGitConfig {
+	param(
+		$Name,
+        $Value
+    )
+
+    & git.exe config --file "$gitConfigFileLocation" $Name $Value
+}
+
+SetGitConfig "user.name" $ProfileConfig.Git.Name
+SetGitConfig "user.email" $ProfileConfig.Git.Email
+SetGitConfig "core.ignorecase" "true"
+SetGitConfig "core.autocrlf" "true" 
+SetGitConfig "core.editor" "'$(Join-Path $InstallPath "Third Party\Sublime Text\sublime_text.exe")' -w"
+SetGitConfig "diff.renames" "true"
+SetGitConfig "diff.tool" "bc4"
+SetGitConfig "merge.tool" "bc3"
+SetGitConfig "pager.log" "false"
