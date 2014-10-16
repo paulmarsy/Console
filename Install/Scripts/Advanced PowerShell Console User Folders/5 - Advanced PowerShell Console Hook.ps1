ef41c0c218bc43a080384ed6bff45c500ad85cc2
Paul Marston <paul@marston.me>
2014-10-16 21:51:35 +0100

    Refactoring hook name
    Former-commit-id: c7b308c1352ce1a2488fea1003fd6a2e769b6617
    Former-commit-id: 12ae0b5d97d96bb6205938b00fa6738cbfd0fded
    Former-commit-id: face0251b79fd6750ed340e18017003ba35894db
    Former-commit-id: b68bada3772c7187323d952fa1a02684222cdbd5
    Former-commit-id: 6846dbe50f67eb941a53eca3755f3de3f71cd6ab
    Former-commit-id: ca583d9e0ef56b1cee3c303f6dd143278a699457
    Former-commit-id: ae84d964cfd280cd030699693c87cff4c305ec95
    Former-commit-id: e57637ab2486b20ad76d2cacf17e458686d06565
    Former-commit-id: 2ef990a6b02d9425592b3347a0f225671df47f92

--- Install/Scripts/Advanced PowerShell Console User Folders/5 - Advanced PowerShell Console Hook.ps1
+++ Install/Scripts/Advanced PowerShell Console User Folders/5 - Advanced PowerShell Console Hook.ps1
@@ -1,6 +1,6 @@
 Write-InstallMessage -EnterNewScope "Configuring Advanced PowerShell Console Hook"
 
-$advancedPowerShellConsoleIdToken = "<# AdvancedPowerShellConsole Token #>"
+$advancedPowerShellConsoleHookToken = "<# AdvancedPowerShellConsole Hook #>"
 $profileToHookInto = $PROFILE.CurrentUserAllHosts
 $profileFolder = Split-Path $profileToHookInto -Parent
 
@@ -14,7 +14,7 @@ Invoke-InstallStep "Setting up PowerShell Profile Directory" {
 Invoke-InstallStep "Adding hook to PowerShell Profile" {
 	if (Test-Path $profileToHookInto) {
 		$token = Get-Content $profileToHookInto | Select-Object -First 1
-	 	if ($token -ne $advancedPowerShellConsoleIdToken) {
+	 	if ($token -ne $advancedPowerShellConsoleHookToken) {
 	 		$profileBackupPath = Join-Path $AdvancedPowerShellConsoleUserScriptsFolder ("$([IO.Path]::GetFileName($profileToHookInto)).bak")
 	    	Write-InstallMessage -Type Warning "Existing $profileToHookInto file exists, backing up to $profileBackupPath"
 	    	Copy-Item $profileToHookInto $profileBackupPath
@@ -25,7 +25,7 @@ Invoke-InstallStep "Adding hook to PowerShell Profile" {
 	New-Item $profileToHookInto -Type File -Force | Out-Null
 	Set-Content -Path $profileToHookInto -Value `
 @"
-$($advancedPowerShellConsoleIdToken)
+$($advancedPowerShellConsoleHookToken)
 
 . "$(Join-Path $AdvancedPowerShellConsoleAppSettingsFolder 'Init.ps1')"
 "@
