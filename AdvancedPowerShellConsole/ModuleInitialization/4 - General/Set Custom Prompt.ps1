2db63fe51437df45e84eafeccb6dcce3e2c84d3a
Paul Marston <paul@marston.me>
2014-10-16 21:49:35 +0100

    Changing how the custom prompt is set
    Former-commit-id: 87bc977a0be187ae2ec6472ea7cde55ba72afa3b
    Former-commit-id: 1b8f6b29afe7e8c09e0609963cc7ae2d2bd53f88
    Former-commit-id: 81c4661a34426f3469579d2802bbb000e1b6fea1
    Former-commit-id: a01792c0bb3b5aae7721aeb8495cdd6a1a4fe6c6
    Former-commit-id: d2ee4451cc8375ee9ea0221614f2dbe3ca55aeb6
    Former-commit-id: 042f290a6550d40ddcf0302fbc08d490277f1473
    Former-commit-id: dbda9ee0dd57006a1848d5d8a081647d6a216541
    Former-commit-id: 8d2efb276ec24ec103f7a19bd11770c8aedf84c3
    Former-commit-id: 05beb8f3e1aee27f728f9b5f370dca5b867ac6da

--- AdvancedPowerShellConsole/ModuleInitialization/4 - General/Set Custom Prompt.ps1
+++ AdvancedPowerShellConsole/ModuleInitialization/4 - General/Set Custom Prompt.ps1
@@ -1,13 +1,13 @@
-function Write-CustomPrompt {
+Set-Item -Path Function:\prompt -Value {
     $realLASTEXITCODE = $LASTEXITCODE
 
-	$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal $windowsIdentity
     if ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { $securityColour = "Red" }
     else { $securityColour = "Green" }
 
     if ((Split-Path $pwd -NoQualifier) -eq "\") { $path = Split-Path $pwd -Qualifier }
-	else { $path = Split-Path $pwd -Parent | Get-ChildItem -Filter (Split-Path $pwd -Leaf) -Force | Select-Object -ExpandProperty Name }
+    else { $path = Split-Path $pwd -Parent | Get-ChildItem -Filter (Split-Path $pwd -Leaf) -Force | Select-Object -ExpandProperty Name }
 
     if ($path) {
         Write-Host -ForegroundColor $securityColour -NoNewLine "$path"
@@ -21,5 +21,5 @@ function Write-CustomPrompt {
     Write-Host -ForegroundColor $securityColour -NoNewLine "$"
 
     $global:LASTEXITCODE = $realLASTEXITCODE
-	return " "
+    return " "
 }
