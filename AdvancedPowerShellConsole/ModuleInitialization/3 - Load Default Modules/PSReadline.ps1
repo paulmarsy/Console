f59deb6235b5b579146f3d5dc19461dc7ef23e8a
Paul Marston <paul@marston.me>
2014-10-18 00:07:37 +0100

    Tweaking psreadline settings
    Former-commit-id: 677af3ab80457cbe968227ade305ccd34dab895a
    Former-commit-id: e3cf96f5ba6114cbe4b30d9ee67504d2083bd876
    Former-commit-id: b7ba37129ddecc0ece084aeb4d0e7909a7fd18d6
    Former-commit-id: 6349028e58cbdac419806e1e99dd8ad6acde0281
    Former-commit-id: 980146c2da0d837a7b98806b1921f68a3fa564da
    Former-commit-id: a315be2e394d1c59dcc90abd989c845745223808
    Former-commit-id: abb8177fa09a1ee96cc26796041de0d6b24880a3
    Former-commit-id: 153a4c3b143931c281736870231d4c371dc198c3
    Former-commit-id: dbf927d9f2bcae266d132e3235ce968992ffecbe

--- AdvancedPowerShellConsole/ModuleInitialization/3 - Load Default Modules/PSReadline.ps1
+++ AdvancedPowerShellConsole/ModuleInitialization/3 - Load Default Modules/PSReadline.ps1
@@ -1,6 +1,9 @@
 # PSReadline - https://github.com/lzybkr/PSReadLine
 if ($Host.Name -eq 'ConsoleHost') {
 	Import-Module PSReadline -Global
+	Set-PSReadlineOption -ShowToolTips
+	Set-PSReadlineOption -HistorySavePath (Join-Path $ProfileConfig.General.PowerShellAppSettingsFolder "PSReadline-history.txt")
+	Set-PSReadlineOption -MaximumHistoryCount 100
 	Set-PSReadlineKeyHandler -Key Ctrl+C -Function CopyOrCancelLine
 	Set-PSReadlineKeyHandler -Key Ctrl+Shift+Home -Function ScrollDisplayTop
 	Set-PSReadlineKeyHandler -Key Ctrl+Shift+End -Function ScrollDisplayToCursor
