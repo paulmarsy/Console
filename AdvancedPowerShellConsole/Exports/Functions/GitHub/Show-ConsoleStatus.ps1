2323c7ff4ae7910043ae7161ce59ca17f459becf
Paul Marston <paul@marston.me>
2014-10-19 15:57:35 +0100

    improving showing of console status
    Former-commit-id: 4d7d2983641cf317a3d7a93bacbdc13a3cbf15bf
    Former-commit-id: 79abc670538e0f8312c5db61fc79d9629e8417a3
    Former-commit-id: 90ad4b6244c4786b3f04ca966d06925425910daf
    Former-commit-id: a3af5aeaa63ae59946f8085e2cae56b0e758bbef
    Former-commit-id: 3f8fdb505e4a8b3824048f5a2a49c603643057f1
    Former-commit-id: 28e729a997b2c39ec2afa1930562329407bbaac0
    Former-commit-id: 20eb0b03999eb3723beea10d597379c79f7f98b0
    Former-commit-id: 6ba335279d91458273eed06698f0dfff5bbf1b40
    Former-commit-id: 4b05e4eb8c40c50ea52bec49d4b0b836e9cd7d20

--- AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/GitHub/Show-ConsoleStatus.ps1
@@ -9,23 +9,30 @@ function Show-ConsoleStatus {
 	_workOnConsoleWorkingDirectory {
 		_invokeGitCommand "remote --verbose update --prune" -Quiet
 
+		$currentBranch = _getCurrentBranch
+
+		Write-Host
 		Write-Host -ForegroundColor Cyan "Currently on branch:"
-		Write-Host -ForegroundColor Red "`t$(_getCurrentBranch)"
+		Write-Host -ForegroundColor Red "`t$currentBranch"
 
 		if (-not $OnlyThisBranch) {
 			if ($Detailed) {
-				Write-Host -ForegroundColor Cyan "`nGit Origin Configuration..."
+				Write-Host
+				Write-Host -ForegroundColor Cyan "Git Origin Configuration..."
 				_invokeGitCommand "remote --verbose show origin"
 			} 
 
-			Write-Host -ForegroundColor Cyan "`nRemote tracking branches..."
+			Write-Host
+			Write-Host -ForegroundColor Cyan "Remote tracking branches..."
 			_invokeGitCommand "branch --remotes"
 
-			Write-Host -ForegroundColor Cyan "`nLocal branches..."
+			Write-Host  
+			Write-Host -ForegroundColor Cyan "Local branches..."
 			_invokeGitCommand "branch --list"
 		}
 
-		Write-Host -ForegroundColor Cyan "`nBranch structure..."
+		Write-Host
+		Write-Host -ForegroundColor Cyan "Branch structure..."
 		$branchStructureCommand = "log --color --date-order --graph --oneline --decorate --no-walk"
 		if (-not $OnlyThisBranch) {
 			$branchStructureCommand += " --all"
@@ -35,7 +42,17 @@ function Show-ConsoleStatus {
 		}
 		_invokeGitCommand $branchStructureCommand
 
-		Write-Host -ForegroundColor Cyan "`nShow uncommited changes..."
+		Write-Host
+		Write-Host -NoNewLine -ForegroundColor Cyan "Show uncommited changes ("
+		$uncommitedChanges = _getNumberOfUncommitedChanges
+		Write-Host -NoNewLine -ForegroundColor (?: { $uncommitedChanges -ge 1 } { "Red" } { "Green" }) $uncommitedChanges
+		Write-Host -ForegroundColor Cyan ")..."
 		_invokeGitCommand "status --short --branch --untracked-files=all $(?: { $IncludeIgnored } { "--ignored" })"
+		Write-Host
+
+		if ($Detailed) {
+			Write-Host -ForegroundColor Cyan "Showing uncommited changes:"
+			_invokeGitCommand "diff $currentBranch"
+		}
     }
 }
