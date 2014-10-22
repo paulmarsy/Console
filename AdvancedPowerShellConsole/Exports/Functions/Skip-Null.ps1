06a6dcd829fac1b44a937aab6f5c24c1d2004162
Paul Marston <paul@marston.me>
2014-10-22 23:48:59 +0100

    fixing skip-null cmdlet
    Former-commit-id: b81bcc6400d12bae384579164cba06f244f9cd46
    Former-commit-id: cc65f9cf171e7427eccd544a2fd29ae7630dffaf
    Former-commit-id: 1dc3b52c90c4b57185c43145367bd9b8cdaf026b
    Former-commit-id: 57143e18f49ddcf0b75f32049ca995e03f74e63b
    Former-commit-id: 6d312e746d9c831c6a2316c86385d14c2096df2d
    Former-commit-id: 9575d7f57ec60bb886f121e326f0a332a98afb84
    Former-commit-id: 34c06ad2b5b18bf35ccd1256cdae3e9be43e6516
    Former-commit-id: 541fae064790db8cff41bbc44c45d601b1996f0f
    Former-commit-id: 139e84a98ccb7c18a68faecd4ed8744d475b089f

--- AdvancedPowerShellConsole/Exports/Functions/Skip-Null.ps1
+++ AdvancedPowerShellConsole/Exports/Functions/Skip-Null.ps1
@@ -1,16 +1,22 @@
-filter Skip-Null {
+function Skip-Null {
 	[CmdletBinding(DefaultParameterSetName="IsNull")]
 	param(
+		[Parameter(ValueFromPipeline = $true)]$InputObject,
 		[Parameter(ParameterSetName="IsNull")][switch]$IsNull,
 		[Parameter(ParameterSetName="OrEmpty")][switch]$OrEmpty,
 		[Parameter(ParameterSetName="OrWhiteSpace")][switch]$OrWhiteSpace
 	)
 	
-	$_ | ? {
-		switch ($PsCmdlet.ParameterSetName) {
-			"IsNull" { return ($null -ne $_) }
-			"OrEmpty" { return (-not ([string]::IsNullOrEmpty($_))) }
-			"OrWhiteSpace" { return (-not ([string]::IsNullOrWhiteSpace($_))) }
+	PROCESS {
+		$InputObject | foreach {
+			$current = $_
+			$shouldSkip = switch ($PsCmdlet.ParameterSetName) {
+				"IsNull" { $null -eq $current }
+				"OrEmpty" { [string]::IsNullOrEmpty($current) }
+				"OrWhiteSpace" { [string]::IsNullOrWhiteSpace($current) }
+			}
+			if ($shouldSkip) { continue }
+			else { return $current }
 		}
 	}
-} 
+}
