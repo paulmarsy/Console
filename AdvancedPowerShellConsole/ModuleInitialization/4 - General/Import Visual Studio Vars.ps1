1e1c1e0158f72c894284b82aff8075feb9809c22
Paul Marston <paul@marston.me>
2014-10-23 19:41:47 +0100

    added error handling around importing the visual studio tools environment
    Former-commit-id: 4a25a065ede8a467a39e7438cec58539418f7dad
    Former-commit-id: ae524be6f15ed4c94fc72ee817aaef292bb77780
    Former-commit-id: eac8486d166216fcdc4f359a9bd5c6cd10b1c23e
    Former-commit-id: 4fbb7234284fde077b18a8bd9f81fe4317855abf
    Former-commit-id: f1f1eb2cf29100534c8d15e1a84660690d75edd4
    Former-commit-id: 19e439a88fea4e9117c2a5491c311373924cbc06
    Former-commit-id: a6429a0fd4f07785268782d36b1a3d165de88c4f
    Former-commit-id: ff103724fb904c24621ea051a348377d4f8dd467
    Former-commit-id: c7feeba100c247c88ec36cb5ab5e58440dd57610

--- AdvancedPowerShellConsole/ModuleInitialization/4 - General/Import Visual Studio Vars.ps1
+++ AdvancedPowerShellConsole/ModuleInitialization/4 - General/Import Visual Studio Vars.ps1
@@ -1,5 +1,9 @@
-if (Test-Path Env:VS120COMNTOOLS) 
+if (Test-Path Env:VS120COMNTOOLS) {
 	Import-VisualStudioVars 2013
-	} elseif (Test-Path Env:VS120COMNTOOLS) {
-		
-	}
+} elseif (Test-Path Env:VS110COMNTOOLS) {
+	Import-VisualStudioVars 2012
+} elseif (Test-Path Env:VS100COMNTOOLS) {
+	Import-VisualStudioVars 2010
+} else {
+    Write-Warning "Unable to detect Visual Studio (2013, 2012 or 2010) to be able to import the Visual Studio Tools environment"
+}
