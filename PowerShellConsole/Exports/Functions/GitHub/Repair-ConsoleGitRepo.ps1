1c7f79e3116579ecdef92c33825e60b41351abac
Paul Marston <paul@marston.me>
2014-10-24 19:35:16 +0100

    Adding repair cmdlet for git repo
    Former-commit-id: 1139c60a1d1e7a6e28318779baada363e09d3288
    Former-commit-id: ad8d3adda6d54df906eb332fa90068086d1e052f
    Former-commit-id: 250865def3131bb24da8603386b604ef9575193c
    Former-commit-id: 8996172eae360dc36d168390d0480f7a8595a99e
    Former-commit-id: b7fceb32dc59e7ae05cd5d9be2f51fc6ae9e140f
    Former-commit-id: fcc3422204b51d7b3a6dd0aef3a7f9cf248a5216
    Former-commit-id: 581d604b4648d572c6484e8fd394440fb85c39a3
    Former-commit-id: 30681798c2172e5ba9c610f7a71ee1487f4f17cf
    Former-commit-id: aaee09158383ec7bc38248a4f85ea51dcb710186

---
+++ PowerShellConsole/Exports/Functions/GitHub/Repair-ConsoleGitRepo.ps1
@@ -0,0 +1,37 @@
+function Repair-ConsoleGitRepo {
+	_workOnConsoleWorkingDirectory {
+		Write-Host -ForegroundColor Green "Attempting to repair Console Git repo..."
+
+		try {
+			$gitProcesses = Get-Process -Name "git"
+
+			Write-Host -NoNewLine -ForegroundColor Green ("Killing {0} Git processes... " -f ($gitProcesses | Measure-Object -Line | Select-Object -ExpandPropertu Line))
+			$gitProcesses | Stop-Process -Force
+			Write-Host -ForegroundColor Green "Done"
+		}
+		catch {
+			Write-Host -ForegroundColor Yellow "No Git processes running"
+		}
+
+		$gitLockFile = ".\.git\index.lock"
+		if (Test-Path $gitLockFile) {
+			Write-Host -NoNewLine -ForegroundColor Green "Removing Git index lock file... "
+			Remove-Item -Path $gitLockFile -Force	
+			Write-Host -ForegroundColor Green "Done"
+		}
+
+		Write-Host -ForegroundColor Green "Git garbage collection..."
+		_invokeGitCommand "gc --prune --verbose --aggressive --auto"
+
+		Write-Host -ForegroundColor Green "Git pruning unreachable objects in the object database..."
+		_invokeGitCommand "prune --progress"
+
+		Write-Host -ForegroundColor Green "Git pruning unreachable objects in pack files..."
+		_invokeGitCommand "prune-packed"
+
+		Write-Host -ForegroundColor Green "Git integrity check..."
+		_invokeGitCommand "fsck --full --strict --progress --unreachable --dangling --cache"
+
+		Write-Host -ForegroundColor Green "Finished repairing Console Git repo"
+	}
+}
