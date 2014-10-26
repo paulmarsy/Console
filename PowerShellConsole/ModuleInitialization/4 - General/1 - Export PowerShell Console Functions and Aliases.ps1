404a4c459682075378eaacca00034187b232150b
Paul Marston <paul@marston.me>
2014-10-26 20:07:37 +0000

    Fixing an issue with exporting cmdlets and aliases from the module
    Former-commit-id: f5d4475b927cae8e1d2d319a5209d35d2fe8a459
    Former-commit-id: 752a8553186cab11102762b0888d319f61c6dadc
    Former-commit-id: 7491dce9c4a93dd5d4ad21b06dd34fcd7ba5f49a
    Former-commit-id: 680aa67950483d59f3bf812fff1c2b275cbcb9d2
    Former-commit-id: ad5f8521a53b4a860d39d9696dd0d9f2f9443495
    Former-commit-id: ad8f2446bc2e789f69a0ad56e9559129a7fa14d7
    Former-commit-id: 8971ee666e9ddf8a9b5efa846a7581b565ba52ea
    Former-commit-id: e64dbeb5ae13e20b9b79ee5611684d399293a3ce
    Former-commit-id: 6583cfe48983c908e4bd0dfec5ff087d963d0fac

--- PowerShellConsole/ModuleInitialization/4 - General/1 - Export PowerShell Console Functions and Aliases.ps1
+++ PowerShellConsole/ModuleInitialization/4 - General/1 - Export PowerShell Console Functions and Aliases.ps1
@@ -1,12 +1,22 @@
+$ProfileConfig.Temp.ModuleExports = @{
+	Functions = @()
+	Aliases = @()
+}
+
+$exportExclusionPattern = "_*"
+
+$referenceFunctions =  Get-ChildItem Function:
 $functions = Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Functions") -Filter *.ps1 -Recurse | Sort-Object -Property FullName
 $functions | % { . $_.FullName }
+$differenceFunctions =  Get-ChildItem Function:
+Compare-Object -ReferenceObject $referenceFunctions -DifferenceObject $differenceFunctions | Select-Object -ExpandProperty InputObject | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.Module.Name } | % {
+	$ProfileConfig.Temp.ModuleExports.Functions += $_.Name
+} 
 
+$referenceAliases = Get-ChildItem Alias:
 $aliases = Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Aliases") -Filter *.ps1 -Recurse | Sort-Object -Property FullName
 $aliases | % { . $_.FullName }
-
-$exportExclusionPattern = "_*.ps1"
-
-$ProfileConfig.Temp.ModuleExports = @{
-	Functions = ($functions | ? { $_ -notlike $exportExclusionPattern } | % BaseName)
-	Aliases = ($aliases | ? { $_ -notlike $exportExclusionPattern } | % BaseName)
+$differenceAliases = Get-ChildItem Alias:
+Compare-Object -ReferenceObject $referenceAliases -DifferenceObject $differenceAliases | Select-Object -ExpandProperty InputObject | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.Module.Name } | % {
+	$ProfileConfig.Temp.ModuleExports.Aliases += $_.Name
 }
