1b4e2b2d0dd6dadb17c0475c2e63c1ac1928349b
Paul Marston <paul@marston.me>
2014-10-27 21:33:00 +0000

    explicit cast to an array so even if theres only one branch it still works
    Former-commit-id: 7fd3fc18b230352b1f864ed2a8b782e81cdfb30a
    Former-commit-id: f9398515319552c9154c8e88c523c39948d2574e
    Former-commit-id: eef2254d839c06d9c653d1855d4eb55134a51e96
    Former-commit-id: ec63410090338352e9989d41928bcdffbf9cbdeb
    Former-commit-id: ba1e1d4c2c5fea271eb401a436e84c714e358aaf
    Former-commit-id: db5e9dfb18bcc0f03c0a205fd87f77f3a01560bd
    Former-commit-id: 5952cd883d4e6cb726bf5c590eef4221cb6ebf1a
    Former-commit-id: 48d4035fd01ae9a208964879072b210fffda4bd9
    Former-commit-id: 1ab31afc3380c1a0302152f362df78c7006076c7

--- PowerShellConsole/Exports/Functions/GitHub/_Internal/_getRemoteBranches.ps1
+++ PowerShellConsole/Exports/Functions/GitHub/_Internal/_getRemoteBranches.ps1
@@ -1,6 +1,6 @@
 function _getRemoteBranches {
     _workOnConsoleWorkingDirectory {
-        $remoteBranchNames =  @() + (& git branch -r | % Remove 0 2 | ? { -not $_.StartsWith("origin/HEAD") })
+        [array]$remoteBranchNames = & git branch -r | % Remove 0 2 | ? { -not $_.StartsWith("origin/HEAD") }
         if ($LASTEXITCODE -ne 0) { throw "Git command returned exit code: $LASTEXITCODE" }
         return $remoteBranchNames
 	} -ReturnValue
