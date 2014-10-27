32564d042e01c32733959812c2513952204de4cc
Paul Marston <paul@marston.me>
2014-10-27 22:07:29 +0000

    Casting the returned object to an array
    Former-commit-id: 071b85d96889931b4b190c5f6b94e3ca06b05cc8
    Former-commit-id: b4c2740370c058f418984d072beb1ad086663068
    Former-commit-id: 33b3e7d38be8e8cbf68dc2ee376c680198f36b07
    Former-commit-id: 5efcfa43532e07c357ca0207251a944c89dcd87f
    Former-commit-id: e6c600fbb9be4393caaf1215eeac8d3f40b627e3
    Former-commit-id: 72f4fc0a7d581df1dd716b64806ab72567634022
    Former-commit-id: 575dcf9aedd7907670003c2eb68c2859244f2a9c
    Former-commit-id: 6a62675f4e6eb677b19a4f7ca15ad2015c4f7e07
    Former-commit-id: d186cc52b018fc3ab74eb91049bdae1be4993dc4

--- PowerShellConsole/Exports/Functions/GitHub/_Internal/_getLocalBranches.ps1
+++ PowerShellConsole/Exports/Functions/GitHub/_Internal/_getLocalBranches.ps1
@@ -1,7 +1,7 @@
 function _getLocalBranches {
-    _workOnConsoleWorkingDirectory {
+    [array](_workOnConsoleWorkingDirectory {
         [array]$localBranchNames = & git branch -l | % Remove 0 2
         if ($LASTEXITCODE -ne 0) { throw "Git command returned exit code: $LASTEXITCODE" }
         return $localBranchNames
-	} -ReturnValue
+	} -ReturnValue)
 }
--- PowerShellConsole/Exports/Functions/GitHub/_Internal/_getRemoteBranches.ps1
+++ PowerShellConsole/Exports/Functions/GitHub/_Internal/_getRemoteBranches.ps1
@@ -1,7 +1,7 @@
 function _getRemoteBranches {
-    _workOnConsoleWorkingDirectory {
+    [array](_workOnConsoleWorkingDirectory {
         [array]$remoteBranchNames = & git branch -r | % Remove 0 2 | ? { -not $_.StartsWith("origin/HEAD") }
         if ($LASTEXITCODE -ne 0) { throw "Git command returned exit code: $LASTEXITCODE" }
         return $remoteBranchNames
-	} -ReturnValue
+	} -ReturnValue)
 }
