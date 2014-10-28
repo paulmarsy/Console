cdcc639b294e4da0665b2837ef4ec16e3e1355a6
Paul Marston <paul@marston.me>
2014-10-28 23:56:58 +0000

    Refactoring custom prompt and adding awareness of PSDebugContext from parent scope
    Former-commit-id: b9c6556a8886cc97ea2169ff33bfa7f23a3f6b3e
    Former-commit-id: d32ed46d2aeb007d2603f958737c7e4316d87a00
    Former-commit-id: b0739be2ab7b8957002f418974449fc30d88b973
    Former-commit-id: a8c7e8a7563314ce66fa2237a9ac7e80f334ae98
    Former-commit-id: ff4ecc24d56a9f8e920f1758542877a2fac6003d
    Former-commit-id: 441d6fb318cbf72135d7036291b2a423a23f6dba
    Former-commit-id: ddf7ad816399e6c04c7c353a3368a088b7a4753e
    Former-commit-id: fd60420f036007de48af09e5618ab13006d9bde5
    Former-commit-id: 6ee4f7454bfdb61e68883564b54b644bee7fbf2e

--- PowerShellConsole/ModuleInitialization/4 - General/Set Custom Prompt.ps1
+++ PowerShellConsole/ModuleInitialization/4 - General/Set Custom Prompt.ps1
@@ -1,25 +1,44 @@
+function _getSecurityContext {
+    if (-not ($ProfileConfig.Temp.ContainsKey("SecurityContext"))) {
+        $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal $windowsIdentity
+        if ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { $ProfileConfig.Temp.SecurityContext = "Red" }
+        else { $ProfileConfig.Temp.SecurityContext = "Green" }
+    }
+    return $ProfileConfig.Temp.SecurityContext
+}
+
+function _writePrompt {
+    param ($Object)
+
+    Write-Host -ForegroundColor (_getSecurityContext) -NoNewLine -Object $Object
+}
+
 Set-Item -Path Function:\prompt -Value {
+    [CmdletBinding()]
+    param ()
+
     $realLASTEXITCODE = $LASTEXITCODE
 
-    $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal $windowsIdentity
-    if ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { $securityColour = "Red" }
-    else { $securityColour = "Green" }
+    if ($PSCmdlet.GetVariableValue("PSDebugContext")) {
+       _writePrompt "[DBG] "
+    }
 
     if ((Split-Path $pwd -NoQualifier) -eq "\") { $path = Split-Path $pwd -Qualifier }
     else { $path = Split-Path $pwd -Parent | Get-ChildItem -Filter (Split-Path $pwd -Leaf) -Force | Select-Object -ExpandProperty Name }
 
     if ($path) {
-        Write-Host -ForegroundColor $securityColour -NoNewLine "$path"
+        _writePrompt $path
         Write-VcsStatus
     } else {
-        Write-Host -ForegroundColor $securityColour -NoNewLine "<Invalid Path>"
+        _writePrompt "<Invalid Path>"
     }
+
     if ($NestedPromptLevel -ne 0) {
-        Write-Host -ForegroundColor $securityColour -NoNewLine "($NestedPromptLevel)"
+        _writePrompt " ($NestedPromptLevel)"
     }
-    Write-Host -ForegroundColor $securityColour -NoNewLine "$"
 
     $global:LASTEXITCODE = $realLASTEXITCODE
-    return " "
+    
+    return "$ "
 }
