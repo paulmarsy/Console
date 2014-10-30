dcdc19f7dde5a679d27ebaeda75761c44543d0df
Paul Marston <paulm@asos.com>
2014-10-30 09:16:00 +0000

    Tweaking github synchronisation
    Former-commit-id: a85bd6f8e5bcd284e611c49c848b4cc348221535
    Former-commit-id: 9077c441797890948c6813e0478b7ff7ccbc9243
    Former-commit-id: 2078f54afe2096c382cc0e1109c48dc8839606e3
    Former-commit-id: 4fbe6f379a2980a4d241079d498ffd881c097046
    Former-commit-id: b2928a5beb173ca91d596a451d8466185d61ebd8
    Former-commit-id: 143ac9408d491eb0dfc6ad678d93179ffb3a4de3
    Former-commit-id: 4a011672efaf0d2f22e9f36109ed54b294012e66
    Former-commit-id: b0fafd6b9d1a516dd8c502443863334cfae4e1e4
    Former-commit-id: 62bdff16fbf7cb46ce4604043079194a7f3433fd

--- PowerShellConsole/ModuleInitialization/4 - General/Synchronize with GitHub.ps1
+++ PowerShellConsole/ModuleInitialization/4 - General/Synchronize with GitHub.ps1
@@ -1,9 +1,1 @@
-if (Test-NetworkStatus) {
-	if (Assert-ConsoleIsInSync -Quiet) {
-		Sync-ConsoleWithGitHub -DontPushToGitHub -UpdateConsole Auto
-	} else {
-		Sync-ConsoleWithGitHub -DontPushToGitHub -DontPullFromGitHub
-	}
-} else {
-	Write-Host -ForegroundColor Red "Unable to synchronise with GitHub due to a lack of network connectivity"
-}
+Sync-ConsoleWithGitHub -DontPushToGitHub -UpdateConsole Auto -Quiet
