33152cb93f264315187e569433853a256c4cd527
Your Name <email@example.com>
2014-10-31 10:45:46 +0000

    Only syncing submodules if explicitley specified
    Former-commit-id: d1978d6b7139b7d9740a43cb804a4ca4d86ad8e1
    Former-commit-id: 83d331ce1c08c3f1ab4e85abcb9a94af9c2a099b
    Former-commit-id: 28de30545faaaa7319035c2606c9a660129cdb9f
    Former-commit-id: 9c907f11417eade8d7253f0adb7ffd65f688feff
    Former-commit-id: 6c59585776518e7ec8f3ab880c7b556d0a335b70
    Former-commit-id: da72c8ea03256c16df76c4960b1dc3a4b3ca6c19
    Former-commit-id: bcd338d5b2900bca605a0c43ac8bb315188ceb95
    Former-commit-id: e36852407975dbf69344211f6d46603190132a62
    Former-commit-id: e9045c060c465df5d6f4f6e50cedb03b72267530

--- PowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitRemotes.ps1
+++ PowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitRemotes.ps1
@@ -1,9 +1,10 @@
 function _updateGitRemotes {
     param(
-        [switch]$Quiet
+        [switch]$Quiet,
+        [switch]$UpdateSubModules
     )
     _workOnConsoleWorkingDirectory {
         _invokeGitCommand "remote --verbose update --prune" -Quiet:$Quiet 
-        _invokeGitCommand "submodule update --init --recursive --remote --rebase" -Quiet:$Quiet
+        _invokeGitCommand "submodule update --init --recursive $(?:{ $UpdateSubModules } { "--remote --rebase" })" -Quiet:$Quiet
 	}
 }
