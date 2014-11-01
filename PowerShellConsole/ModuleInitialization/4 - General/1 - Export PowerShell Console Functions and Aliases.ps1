a6193b3b690aeb21aa35d79440b15ced1627569c
Paul Marston <paul@marston.me>
2014-11-01 12:54:20 +0000

    Improving detecting of imported functions and aliases, so they are detected even if they already exist in another module
    Former-commit-id: a82af710aae06615db0dd93170d906fbba5c4cd4
    Former-commit-id: 639a15b0992670ff71cc433fa93c53ed0be3fff8
    Former-commit-id: 4e827c6a8ff5a6a4b617748dff9ab43f9c4c00c1
    Former-commit-id: 23b975789c4917518e5f3a7d238eb93b5b36e588
    Former-commit-id: 16c2e58ccab8618858e5baa4b2d94fcf0a7e0a26
    Former-commit-id: 80adec401fa7e33a2f9361ee64d3fff632a32f08
    Former-commit-id: d760f868f5ff5df345999b2235e68f1a840531eb
    Former-commit-id: fe94ad7f803d18a99d0a21b28a4cd93695596fdd
    Former-commit-id: 3a51ecfa7e699fd856b5fb8febbb151413fdd2c3

--- PowerShellConsole/ModuleInitialization/4 - General/1 - Export PowerShell Console Functions and Aliases.ps1
+++ PowerShellConsole/ModuleInitialization/4 - General/1 - Export PowerShell Console Functions and Aliases.ps1
@@ -5,18 +5,16 @@ $ProfileConfig.Temp.ModuleExports = @{
 
 $exportExclusionPattern = "_*"
 
-$referenceFunctions =  Get-ChildItem Function:
-$functions = Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Functions") -Filter *.ps1 -Recurse | Sort-Object -Property FullName
-$functions | % { . $_.FullName }
+$referenceFunctions =  Get-ChildItem Function: 
+Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Functions") -Filter *.ps1 -Recurse | Sort-Object -Property FullName | % { . $_.FullName }
 $differenceFunctions =  Get-ChildItem Function:
-Compare-Object -ReferenceObject $referenceFunctions -DifferenceObject $differenceFunctions | Select-Object -ExpandProperty InputObject | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.Module.Name } | % {
+Compare-Object -ReferenceObject $referenceFunctions -DifferenceObject $differenceFunctions -Property @("Name", "ModuleName") | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.ModuleName } | % {
 	$ProfileConfig.Temp.ModuleExports.Functions += $_.Name
 } 
 
 $referenceAliases = Get-ChildItem Alias:
-$aliases = Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Aliases") -Filter *.ps1 -Recurse | Sort-Object -Property FullName
-$aliases | % { . $_.FullName }
+Get-ChildItem (Join-Path $PSScriptRoot "..\..\Exports\Aliases") -Filter *.ps1 -Recurse | Sort-Object -Property FullName | % { . $_.FullName }
 $differenceAliases = Get-ChildItem Alias:
-Compare-Object -ReferenceObject $referenceAliases -DifferenceObject $differenceAliases | Select-Object -ExpandProperty InputObject | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.Module.Name } | % {
+Compare-Object -ReferenceObject $referenceAliases -DifferenceObject $differenceAliases -Property @("Name", "ReferencedCommand", "ModuleName") | ? { $_.Name -notlike $exportExclusionPattern -and $ExecutionContext.SessionState.Module.Name -eq $_.ModuleName } | % {
 	$ProfileConfig.Temp.ModuleExports.Aliases += $_.Name
 }
