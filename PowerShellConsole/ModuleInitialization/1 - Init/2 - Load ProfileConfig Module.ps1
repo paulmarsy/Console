d69367f6f9dc1b6e458989468b9cc79b6a0a7463
Paul Marston <paul@marston.me>
2014-11-06 01:13:33 +0000

    Removing module before reimporting if reloading the console
    Former-commit-id: 68a30018589afe064d222db0ea1c80cc2bf4d4d0
    Former-commit-id: 2736c7b6ec663f32fd93319d4ba2ad3a31ee52ac
    Former-commit-id: a97e3e5147d8e870a8ebfba1ba93a6d1b98b3071
    Former-commit-id: 322a661f4f4c797c63cbb48b041986a3114c715f
    Former-commit-id: 9143fef9874b01d582b49fe6c5e3876bf5870fc5
    Former-commit-id: ddb83a6a894c89862105ba96c505c723bed48e57
    Former-commit-id: df0a504d1961e614a03878e222696d323515b1ab
    Former-commit-id: 9b9ba87566074ea39ed7ba6b1706a9c6fce515f3
    Former-commit-id: ee3f81971b84cfe30996c14ca105e9ed34f02c29

--- PowerShellConsole/ModuleInitialization/1 - Init/2 - Load ProfileConfig Module.ps1
+++ PowerShellConsole/ModuleInitialization/1 - Init/2 - Load ProfileConfig Module.ps1
@@ -1,1 +1,4 @@
-Import-Module (Join-Path $ExecutionContext.SessionState.Module.ModuleBase "ProfileConfig") -ArgumentList $InstallPath -Global
+if (Get-Module ProfileConfig) {
+	Remove-Module ProfileConfig
+}
+Import-Module (Join-Path $ExecutionContext.SessionState.Module.ModuleBase "ProfileConfig") -ArgumentList $InstallPath -Global -Force
