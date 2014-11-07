f94a2c23329bfcff3c06ba564894308c0b9801ec
Paul Marston <paul@marston.me>
2014-11-07 00:57:27 +0000

    Fixing a bug where powershell has no parent process
    Former-commit-id: 99695352866def58de14a31b32abaee5747942d2
    Former-commit-id: 10e666ea1339c4f61f12e05a40aae9e2f57c41df
    Former-commit-id: 5684ac8d29ca561b0e0207946f57c08e2aa10244
    Former-commit-id: c32c79345d7a6a0d84fa585481909e51ecb5b01b
    Former-commit-id: 861c7246eb7cda7be4ebbf1d4f9a588253202e16
    Former-commit-id: 6c440ff2736bd2ffbed9dc12e25dc9a25a45e719
    Former-commit-id: 21460c9a6068efd0fa8ccbf3647239dbc181c7c3
    Former-commit-id: 561c7158d69f9c448d20e26d3755d044b11bf0bc
    Former-commit-id: 1240c1f0cf2bd44f576f6dd32d6bce2eced7607f

--- Install/Scripts/PowerShell Console User Folders/Init Functions/Start-PowerShellConsole.ps1
+++ Install/Scripts/PowerShell Console User Folders/Init Functions/Start-PowerShellConsole.ps1
@@ -3,9 +3,9 @@ function Start-PowerShellConsole {
 		[switch]$Force
 	)
 
-	if (Get-Process -Id (Get-WmiObject Win32_Process -Filter "ProcessId='$PID'").ParentProcessId | `
-	% Name | `
-	? { $_ -notlike "ConEmuC*" -and -not $Force }) {
+	$parentProcessId = (Get-WmiObject Win32_Process -Filter "ProcessId='$PID'").ParentProcessId
+	$parentProcess = Get-Process -Id $parentProcessId -ErrorAction Ignore
+	if (($null -eq $parentProcess -or ($parentProcess | % Name) -notlike "ConEmuC*") -and -not $Force) {
 		Write-Host -ForegroundColor Red "Not starting PowerShell Console as we are not using ConEmu. Use the -Force switch to override."
 		return
 	}
