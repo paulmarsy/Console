64a4f554a3494383eb0deea08ff1a4cbbbb5a0cb
Paul Marston <paul@marston.me>
2014-11-09 21:43:46 +0000

    Referencing new location of profileconfig module
    Former-commit-id: 325d52ce6ea0a11eee8c238128dcb337b86e679b
    Former-commit-id: 7e7a38182b7f23a1b382a3dba5fd5284f01cde2b
    Former-commit-id: 67c38c2191c0eda53f2a430d1b1c1746a3bf9e31
    Former-commit-id: 2b76b616d506f11d2f0d02888ba81328599fc509
    Former-commit-id: ff0269fdd833558f1970147fdfee90b15c6acfdc
    Former-commit-id: f5b0ca8cbd8962074631eaf0e9d34c1d8e021587
    Former-commit-id: 17fb6cd45d59b0baac440b9996262c54a2af5d25
    Former-commit-id: b6526474ceb3c51d1a1cbf6b76ad9faca689f792
    Former-commit-id: 040a02efc69c66f9227acd6dd03f687782ac9b3c

--- PowerShellConsole/ModuleInitialization/1 - Init/2 - Load ProfileConfig Module.ps1
+++ PowerShellConsole/ModuleInitialization/1 - Init/2 - Load ProfileConfig Module.ps1
@@ -1,4 +1,4 @@
 if (Get-Module ProfileConfig) {
 	Remove-Module ProfileConfig
 }
-Import-Module (Join-Path $ExecutionContext.SessionState.Module.ModuleBase "ProfileConfig") -ArgumentList $InstallPath -Global -Force
+Import-Module (Join-Path $ConsoleRoot "ProfileConfig") -Global -Force
