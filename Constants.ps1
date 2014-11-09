690543581298ca427b0a7f67d3da3774abf7822e
Paul Marston <paul@marston.me>
2014-11-09 21:40:54 +0000

    Moving constants to root
    Former-commit-id: 5ae1084b31bc52a388271a03f68af68980c42540
    Former-commit-id: 501026dc6d7c032e2a1bac514a90215dbabdb7a2
    Former-commit-id: b90b969ab23cbff1ad72e81af3e9af54e98facde
    Former-commit-id: 9af43468617d47766db2d8cf2fcd542f37b98cf5
    Former-commit-id: 84090b959fb71de359557be112cbffe1820873b9
    Former-commit-id: 7438c2021adcb9a8958d2041971d0c82f5f690bb
    Former-commit-id: ba68cf2c312153576e428acd044b158d88065c92
    Former-commit-id: 29111f75f034d59d2b0a1660cfdfd68962b87dc9
    Former-commit-id: d62a757ac65ba3b8e54d456095489a6b1109b174

---
+++ Constants.ps1
@@ -0,0 +1,55 @@
+$ConsoleRoot = Resolve-Path -Path $PSScriptRoot | % Path
+
+$UserFolder = Join-Path ([System.Environment]::GetFolderPath("MyDocuments")) "PowerShell Console"
+$AppSettingsFolder = Join-Path $UserFolder "App Settings"
+$UserScriptsFolder = Join-Path $UserFolder "User Scripts"
+
+$Constants = @{
+	InstallPath = $ConsoleRoot
+	HookFiles = @{
+		PowerShell = $PROFILE.CurrentUserAllHosts
+		CommandPrompt = (Join-Path $ConsoleRoot "CommandPromptConsole\Init.bat")
+	}
+	UserFolders = @{
+		Root = $UserFolder
+		AppSettingsFolder = $AppSettingsFolder
+		AppSettingsFunctionsFolder = (Join-Path $AppSettingsFolder "Functions")
+		UserScriptsFolder = $UserScriptsFolder
+		UserScriptsIncludeFile = (Join-Path $UserScriptsFolder "include.ps1")
+		UserScriptsAutoFolder = (Join-Path $UserScriptsFolder "Auto Import")
+		TempFolder = (Join-Path $UserFolder "Temp")
+	}
+	Version = @{
+		CurrentFile = (Join-Path $ConsoleRoot "Version.semver")
+		InstalledFile = (Join-Path $AppSettingsFolder "Version.semver")
+	}
+	Executables = @{
+		ConEmu = (Join-Path $ConsoleRoot "Libraries\ConEmu\ConEmu64.exe")
+		SublimeText = (Join-Path $ConsoleRoot "Libraries\Sublime Text\sublime_text.exe")
+		Hstart = (Join-Path $ConsoleRoot "Libraries\Binaries\Hstart\hstart64.exe")
+	}
+}
+
+$version = $Constants.Version
+$Constants.Version.Update = {
+	$version.CurrentVersion = Get-Content -Path $version.CurrentFile
+	if (Test-Path $version.InstalledFile) {
+		$version.InstalledVersion = Get-Content -Path $version.InstalledFile
+	} else {
+		$version.InstalledVersion = "Not Installed"
+	}
+	if ($version.InstalledVersion -eq $version.CurrentVersion) {
+		$version.CurrentVersionInstalled = $true
+	} else {
+		$version.CurrentVersionInstalled = $false
+	}
+}.GetNewClosure()
+$Constants.Version.Update.Invoke()
+
+if ([System.Environment]::Is64BitProcess) {
+	$Constants.ProcessArchitecture = "x64" 
+} else { 
+	$Constants.ProcessArchitecture = "x86" 
+}
+
+return $Constants
