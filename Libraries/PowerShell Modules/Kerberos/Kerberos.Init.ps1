4f0416d10552771cf0bb6bf4e77ab6ada4955b19
Paul Marston <paul@marston.me>
2014-11-09 21:38:53 +0000

    Combining external dependencies under a single libraries folder
    Former-commit-id: 37e25fef32e9c702831b8c9161145f8be4a0d738
    Former-commit-id: c4a5dfead104d3630716e83176f3ee17d7ad8bdc
    Former-commit-id: da0f74195cc5b1b9c6cc1b0a8a6788e2f33f240b
    Former-commit-id: 84a6dbdcb0bb3ce4e37035acfc168e6f588207a8
    Former-commit-id: 034ac9c9e2357ac0600a5eba44cfec158cacdf0e
    Former-commit-id: 731bbb769ab23f4b22884fe0b03f62ae8a88ffce
    Former-commit-id: 7dcef25895140b92244862c0a7bafc41b5f690ea
    Former-commit-id: 8fbfd50461217f9bee5d773eb174490c115fd017
    Former-commit-id: d92f25264bae51814d3c8135b21b313625b07b27

---
+++ Libraries/PowerShell Modules/Kerberos/Kerberos.Init.ps1
@@ -0,0 +1,22 @@
+﻿if ([IntPtr]::Size -eq 8)
+{
+	Add-Type -Path $PSScriptRoot\KerberosLibx64.dll
+}
+else
+{
+	Add-Type -Path $PSScriptRoot\KerberosLibx86.dll
+}
+
+#using Update-FormatData and not FormatsToProcess in the PSD1 as FormatsToProcess does not offer
+#putting format data in front of the default data. This is required to make the new formatter the default ones.
+Update-FormatData -PrependPath $PSScriptRoot\Kerberos.format.ps1xml
+
+if ([IntPtr]::Size -eq 8)
+{
+	Write-Host "64-Bit Types added" -ForegroundColor DarkGreen
+}
+else
+{
+	Write-Host "32-Bit Types added" -ForegroundColor DarkGreen
+}
+Write-Host "Kerberos Module loaded" -ForegroundColor DarkGreen
