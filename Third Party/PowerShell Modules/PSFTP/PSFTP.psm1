73f0482d2275385f95f41cf1b741656bb3ffc7d4
Paul Marston <paul@marston.me>
2014-11-09 21:39:42 +0000

    Removing the old folders now dependencies have been combined under libraries folder
    Former-commit-id: 61cb92970793d2bca4be39d510c96aca4462af6d
    Former-commit-id: 2b0e0b80f35d998c6a878dfec07569add75522da
    Former-commit-id: 753692db7c5edef99f79ac57cf3c2d38bfa9ff08
    Former-commit-id: 6cc341989588434b60f0d55f76e68fc5983ecabe
    Former-commit-id: 92870f3663a8a44cff7302a09a4a9d4ff0debb58
    Former-commit-id: c923cdbadd8a14fe1e15aabc726a97b8a5ecc6da
    Former-commit-id: 9642e603a83629b5280f8d719364e7ac2e3e8485
    Former-commit-id: a54f96fbfadabb4b6fe524e644c3f7b47511a826
    Former-commit-id: eae484dc9de18d161a7c727e3ae158fe4a4f424d

--- Third Party/PowerShell Modules/PSFTP/PSFTP.psm1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Path $PSScriptRoot\*.ps1 | Foreach-Object{ . $_.FullName }
-
-New-Alias Send-FTPItem Add-FTPItem
-New-Alias Receive-FTPItem Get-FTPItem
-New-Alias Move-FTPItem Rename-FTPItem
-
-Export-ModuleMember -Function * -Alias *
