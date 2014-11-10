512b077e3593c20168014928ff7184bc5c1f6530
Paul Marston <paul@marston.me>
2014-11-10 23:29:33 +0000

    Defaulting Format-List to display much more information
    Former-commit-id: f032f2234b394c7b7458d4570b1b6d16ecdf2632
    Former-commit-id: 88ea70e9980bbd84539da8e839607eb88cdab5bb
    Former-commit-id: 3cacb2bfdf4d35b2afa4e50509e8628bbac208c4
    Former-commit-id: 9e7beac8256c58d893c338431eb349f9d165e994
    Former-commit-id: 6a7fe0251311eb974cd90ff837d466cc98521e81
    Former-commit-id: 3bb95c2c6e8924973f9538ccfbde845bed190917
    Former-commit-id: ff1b1d8f0745feedd08de153300b47cef4788166
    Former-commit-id: e9d17af593c9af845d92ed4dbda9f12994ee1454
    Former-commit-id: f346f8d7b48784c1af8666f569867048eeb073e4

---
+++ PowerShellConsole/ModuleInitialization/4 - General/Set Format-List Default Parameter Values.ps1
@@ -0,0 +1,3 @@
+$Global:PSDefaultParameterValues.Add("Format-List:Property", "*")
+$Global:PSDefaultParameterValues.Add("Format-List:Expand", "Both")
+$Global:PSDefaultParameterValues.Add("Format-List:Force", $true)
