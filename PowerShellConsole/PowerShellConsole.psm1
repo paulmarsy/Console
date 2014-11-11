801a19e5c431cb7a55702fa960871b6db12cd6ab
Paul Marston <paul@marston.me>
2014-11-11 01:32:27 +0000

    Adding profiling to module initialization
    Former-commit-id: 81faae014c586c1b075f6a713db5e980ea455c71
    Former-commit-id: 10f2c83c342424baa3e414c1da5e7b69cf9bb4b0
    Former-commit-id: 75df2d47bd896579fa2cc3b771dda3926254cf5a
    Former-commit-id: 44a8a7979892fb2c471f61bda2d3d275fcd24ace
    Former-commit-id: 89d87ceaf45a9e7e91063f21e89fa31a132bc204
    Former-commit-id: 256ef804e88180be92cfee89b746d83bee7b5bd2
    Former-commit-id: 2d5f45ae98ee71a70e717511d3f1bf442cc1dae3
    Former-commit-id: 8dfcd0a41cae2da785ec67454f8966fd1f37e17a
    Former-commit-id: 2dd5d8596f7ffdf6a8ab6cd153828e086f545b15

--- PowerShellConsole/PowerShellConsole.psm1
+++ PowerShellConsole/PowerShellConsole.psm1
@@ -2,7 +2,35 @@ Set-StrictMode -Version Latest
 
 $ConsoleRoot = Resolve-Path -Path (Join-Path $PSScriptRoot "..\") | % Path
 
-Get-ChildItem (Join-Path $PSScriptRoot "ModuleInitialization") -Filter *.ps1 -Recurse | Sort-Object FullName | % { . $_.FullName }
+$profiler = @{
+	RawData = ({@()}.Invoke())
+}
+$moduleInitialization = Join-Path $PSScriptRoot "ModuleInitialization"
+Get-ChildItem -Path $moduleInitialization -Filter *.ps1 -Recurse | Sort-Object FullName | % {
+	$profiledItem = @{
+		FullName = $_.FullName
+		Start = ([System.DateTime]::Now.Ticks)
+	}
+	. "$($_.FullName)"
+	$profiledItem.Finish = [System.DateTime]::Now.Ticks
+	$profiler.RawData.Add((New-Object -TypeName PSObject -Property $profiledItem))
+}
+
+$profiler.Report = @{
+	Actions = ({@()}.Invoke())
+	TotalTime = ([System.TimeSpan]::FromTicks((($profiler.RawData | Select-Object -Last 1 | % Finish) - ($profiler.RawData | Select-Object -First 1 | % Start))))
+}
+$profiler.RawData | % {
+	$duration = [System.TimeSpan]::FromTicks($_.Finish - $_.Start)
+	$profiler.Report.Actions.Add((New-Object -TypeName PSObject -Property @{
+		Name = ($_.FullName.Substring($moduleInitialization.Length + 1))
+		Duration = $duration
+		Milliseconds = ($duration.TotalMilliseconds.ToString())
+	}))
+}
+$profiler.Report.HighestImpact = $profiler.Report.Actions | Sort-Object -Property Duration -Descending | Select-Object -First 1 | % { "$($_.Name) ($($_.Milliseconds) milliseconds)" }
+
+$ProfileConfig.Temp.ModuleInitializationProfiler = $profiler
 
 & (Join-Path $PSScriptRoot "Module Destructor.ps1")
 
