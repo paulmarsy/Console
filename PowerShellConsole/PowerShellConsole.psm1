5446212e360170b83893d1cb165748c593406a16
Paul Marston <paul@marston.me>
2014-11-11 00:25:46 +0000

    adding deconstructor
    Former-commit-id: 6614d4dcd6dd6214fcf114b4721491e9064b9703
    Former-commit-id: 1bd078588c65133c68713e4e4c1dfd2c4168f403
    Former-commit-id: 03eb3184c4b17a01b520971c8ae3d7bd9a295abd
    Former-commit-id: 8ddef9c6b8ee42f9178a3c2d23c13fabaa147946
    Former-commit-id: 5003e72f33b9583834f4d4ef1a4c6a80882a441d
    Former-commit-id: 54a9a41652b190723db7e1ffbdee58945eb36798
    Former-commit-id: 64d46596d22d5de7b757ccd33a82d1046b725cae
    Former-commit-id: 3cf0065619a92cf0ed310a8fa533860dae7d083b
    Former-commit-id: 3c38b3345800aa8c3791bd13763a72392cffe70d

--- PowerShellConsole/PowerShellConsole.psm1
+++ PowerShellConsole/PowerShellConsole.psm1
@@ -4,6 +4,8 @@ $ConsoleRoot = Resolve-Path -Path (Join-Path $PSScriptRoot "..\") | % Path
 
 Get-ChildItem (Join-Path $PSScriptRoot "ModuleInitialization") -Filter *.ps1 -Recurse | Sort-Object FullName | % { . $_.FullName }
 
+& (Join-Path $PSScriptRoot "Module Destructor.ps1")
+
 Export-ModuleMember -Function $ProfileConfig.Temp.ModuleExports.Functions -Alias $ProfileConfig.Temp.ModuleExports.Aliases
 
 Write-Host
