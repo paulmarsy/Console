89afe8133084cf205e32fa506324ecc4c65c8529
Paul Marston <paul@marston.me>
2014-11-14 21:00:35 +0000

    Always updating submodules
    Former-commit-id: 9163844eeab06a0aec6fb021b2da7b441bccd2c9
    Former-commit-id: ee0a022b70de23c5ae25a4d3cf9eb6d71dacf970
    Former-commit-id: 3e51de21ad770ff764242224c1ea6635c385ad67
    Former-commit-id: bfe6d72c9240945a76bdf7fe613cd75d922ea71e
    Former-commit-id: fb65b92f7159930a20d978a80e90cfa0ee4f1517
    Former-commit-id: 38f324260332574d9ebb50c4f122996db55dad12
    Former-commit-id: a51aeb74d3a3a203dceef287c9e2fdf8b8106bf4
    Former-commit-id: 60a8fe0dd5fbdacc60c69ca99100c833412ea084

--- PowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitRemotes.ps1
+++ PowerShellConsole/Exports/Functions/GitHub/_Internal/_updateGitRemotes.ps1
@@ -1,10 +1,9 @@
 function _updateGitRemotes {
     param(
-        [switch]$Quiet,
-        [switch]$UpdateSubModules
+        [switch]$Quiet
     )
     _workOnConsoleWorkingDirectory {
         _invokeGitCommand "remote --verbose update --prune" -Quiet:$Quiet 
-        _invokeGitCommand "submodule update --init --recursive $(?:{ $UpdateSubModules } { "--remote --rebase" })" -Quiet:$Quiet
+        _invokeGitCommand "submodule update --init --recursive --remote --rebase" -Quiet:$Quiet
 	}
 }
