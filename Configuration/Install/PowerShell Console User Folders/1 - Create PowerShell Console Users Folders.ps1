4bd85ad4f1a4c74f54c78df1148922a9f7fa919d
Paul Marston <paul@marston.me>
2014-11-15 02:19:41 +0000

    Customising user folder's appearance
    Former-commit-id: 55d93839ba49baf0c58ed25495aab191c639fce0
    Former-commit-id: 565df77b801686e13ab2dabedccca18a2fc4264f
    Former-commit-id: bdc1a73cd234ac50791bcc9a4fbbfd30a76aff7b
    Former-commit-id: 3352445c84fdbd44bed79ff242435d30f791d702
    Former-commit-id: 85acae2d86012c98b8e392bdcfac43911a6a8298
    Former-commit-id: ed55d3cdc053abef85f0deabfead11feb60fc054
    Former-commit-id: a09b25ee0b90b9cce9cc9b886fdb978c5e7828c0
    Former-commit-id: 20631586865742f334e31325f8eccb89f6679736

--- Configuration/Install/PowerShell Console User Folders/1 - Create PowerShell Console Users Folders.ps1
+++ Configuration/Install/PowerShell Console User Folders/1 - Create PowerShell Console Users Folders.ps1
@@ -1,5 +1,19 @@
-Invoke-InstallStep "Creating Users Folders" {
-	if (-not (Test-Path $PowerShellConsoleContstants.UserFolders.Root)) {
-		New-Item $PowerShellConsoleContstants.UserFolders.Root -Type Directory -Force | Out-Null
+Invoke-InstallStep "Creating Users Folder" {
+	if (Test-Path $PowerShellConsoleContstants.UserFolders.Root) {
+		$userFolder = Get-Item $PowerShellConsoleContstants.UserFolders.Root -Force
 	}
+	else {
+		$userFolder = New-Item $PowerShellConsoleContstants.UserFolders.Root -Type Directory -Force
+	}
+
+	$userFolder.Attributes = @([System.IO.FileAttributes]::ReadOnly,[System.IO.FileAttributes]::System)
+}
+
+Invoke-InstallStep "Customising Users Folder's appearance" {
+	$desktopIniFile = $PowerShellConsoleContstants.UserFolders.Root desktop.ini
+	Copy-Item -Path (Join-Path $PowerShellConsoleContstants.ConsoleRoot "Libraries\Icons\PowerShellUserFolder-desktop.ini") -Destination $desktopIniFile -Force
+
+	$desktopIniText = [System.IO.File]::ReadAllText($desktopIniFile)
+	$desktopIniText = $desktopIniText.Replace("##InstallPath##", $PowerShellConsoleContstants.InstallPath)
+	[System.IO.File]::WriteAllText($desktopIniFilee, $desktopIniText)
 }
