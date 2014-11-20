6c440366ce1d31806c866f598797a937a1bed712
Paul Marston <paul@marston.me>
2014-11-20 22:55:29 +0000

    Fixing missing $ on variable name
    Former-commit-id: 1405100a6acecfcc912638f7038393d80fc8c6a4
    Former-commit-id: 41c685bb84d8d94bb46fafb0b9d3eaeec757453e
    Former-commit-id: 8eb470295fb9961957e3fa0669efc503125ea660
    Former-commit-id: 91917256c0f4cb6e5d7091cdf824d8c71a5d0f9b
    Former-commit-id: f3943070aed3e5a7b0158c92469de2563a325371
    Former-commit-id: b56b306dc23da7497f473ba1f23e26c2882aa0d0
    Former-commit-id: 3db8a2aed758a08632c8b1067723e4c58729c60a
    Former-commit-id: 66492745183ebbbdc39c21efbf5073fcf3de6afe

--- PowerShellConsole/ModuleInitialization/6 - Final/1 - Display Welcome Banner.ps1
+++ PowerShellConsole/ModuleInitialization/6 - Final/1 - Display Welcome Banner.ps1
@@ -10,7 +10,7 @@ Write-Host -NoNewline -ForegroundColor Red $machineIpProperties.HostName
 if (-not [string]::IsNullOrWhiteSpace($machineIpProperties.DomainName)) {
     Write-Host -NoNewline -ForegroundColor Red ".$($machineIpProperties.DomainName)"
 }
-if (Test-NetworkStatus -and $null -ne networkConnectionProfile) {
+if (Test-NetworkStatus -and $null -ne $networkConnectionProfile) {
 	Write-Host -NoNewline -ForegroundColor Gray " ("
 	Write-Host -NoNewline -ForegroundColor DarkGreen $networkConnectionProfile.NetworkCategory
 	Write-Host -NoNewline -ForegroundColor Gray ")"
@@ -18,7 +18,7 @@ if (Test-NetworkStatus -and $null -ne networkConnectionProfile) {
 
 Write-Host
 
-if (Test-NetworkStatus -and $null -ne networkConnectionProfile) {
+if (Test-NetworkStatus -and $null -ne $networkConnectionProfile) {
 	Write-Host -NoNewline "Connected to network '"
 	Write-Host -NoNewline -ForegroundColor DarkGreen $networkConnectionProfile.Name
 	Write-Host -NoNewline "' over "
