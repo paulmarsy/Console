a000e6ff340597a65eecf029bf058db5e9808651
Paul Marston <paul@marston.me>
2014-11-24 19:14:44 +0000

    Fixing the setting of PSDefaultParameterValues when already added i.e. doing reload
    Former-commit-id: f91c1ac1d8a401ce3864ff5e542bffd83b4a187c
    Former-commit-id: c49ad6fcf9684e1780e87300fa6a06bf3e54f010
    Former-commit-id: bcff517f6da7c96e73c121524945660696698efa
    Former-commit-id: c7320c404ab361fb777a893a10e9c8c892ec994d
    Former-commit-id: 2b37073cfc950f85e3e8770b74d24eeea95fce13
    Former-commit-id: e3c2087d186481f7388fe5fdd9221f9f9e544677
    Former-commit-id: bc86ba99e453775cb858c01bd5e4b6b24a750446
    Former-commit-id: f4468c1a28679ab49938b41214f2d48d3ac8dabe

--- PowerShellConsole/ModuleInitialization/4 - General/Set Format-List Default Parameter Values.ps1
+++ PowerShellConsole/ModuleInitialization/4 - General/Set Format-List Default Parameter Values.ps1
@@ -1,6 +1,15 @@
 param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 3; Critical = $false}) }
 
-$Global:PSDefaultParameterValues.Add("Format-List:Property", "*")
-$Global:PSDefaultParameterValues.Add("Format-List:Expand", "Both")
-$Global:PSDefaultParameterValues.Add("Format-List:Force", $true)
+function Update-PSDefaultParameterValues {
+	param($Name, $Value)
+
+	if ($Global:PSDefaultParameterValues.ContainsKey($Name)) {
+		$Global:PSDefaultParameterValues.Remove($Name)
+	}
+	$Global:PSDefaultParameterValues.Add($Name, $Value)
+}
+
+Update-PSDefaultParameterValues "Format-List:Property" "*"
+Update-PSDefaultParameterValues "Format-List:Expand" "Both"
+Update-PSDefaultParameterValues "Format-List:Force" $true
