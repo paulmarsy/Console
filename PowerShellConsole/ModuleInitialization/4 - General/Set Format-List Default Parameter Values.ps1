7627a7b15c9e02a44b0383447f7cb37c93037870
Paul Marston <paul@marston.me>
2014-12-02 20:01:53 +0000

    Removing commented out definition now the code has been verified as working with powershell 4 and 5
    Former-commit-id: b3d01145728779b4e6c6e1facb73b03fcd199e7a
    Former-commit-id: b28031c44f69990a81c748ef02d7fec1c60e7cfd
    Former-commit-id: 2eb42a2939dd8f187afb05dc48c6e59482f1d071
    Former-commit-id: cf6b7278069fc79b2d96d270a0aa6519db66b0ac
    Former-commit-id: 0460d44a919ced1a41327ba00bac903a7f5a9dcb
    Former-commit-id: a950af150ff469081be6e7009783462a63ce68db
    Former-commit-id: 610ccbc3dafaf7ec15ab3af9d8c38ee6694ff6ce
    Former-commit-id: eecbd36a3c90f0c17a59002e75904750ad362725

---
+++ PowerShellConsole/ModuleInitialization/4 - General/Set Format-List Default Parameter Values.ps1
@@ -0,0 +1,15 @@
+param([switch]$GetModuleStepDetails)
+if ($GetModuleStepDetails) { return (@{RunLevel = 3; Critical = $false}) }
+
+function Update-PSDefaultParameterValues {
+	param($Name, $Value)
+
+	if ($Global:PSDefaultParameterValues.Contains($Name)) {
+		$Global:PSDefaultParameterValues.Remove($Name)
+	}
+	$Global:PSDefaultParameterValues.Add($Name, $Value)
+}
+
+Update-PSDefaultParameterValues "Format-List:Property" "*"
+Update-PSDefaultParameterValues "Format-List:Expand" "Both"
+Update-PSDefaultParameterValues "Format-List:Force" $true
