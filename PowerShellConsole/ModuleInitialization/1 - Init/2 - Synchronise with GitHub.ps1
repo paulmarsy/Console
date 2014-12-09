ecc742b75f172c8b0e65a82c27d273187114efb3
Paul Marston <paul@marston.me>
2014-12-09 23:12:08 +0000

    Tweaking syncer
    Former-commit-id: 10a0155474d5fc95032252a9886910866136a927
    Former-commit-id: 5e38729732e89a47355767f38c201e37358efb7e
    Former-commit-id: 70e2f1c9a58b2a6f93d1946a0508f442fc740bbf
    Former-commit-id: bfe208dfba663c8349602c00fc3df6f845e97190
    Former-commit-id: 324bd60c59e8d5bffc7a6a2ce18a688bf505e1a6
    Former-commit-id: 917522d70a8df18237741a8293d93ceb798e9321
    Former-commit-id: 7e7fd387d0fb418229362b4098fc4d0a1b0631ee
    Former-commit-id: 970c1f0565cedcce0ca9ba6fadd62ede7eb28218

--- PowerShellConsole/ModuleInitialization/1 - Init/2 - Synchronise with GitHub.ps1
+++ PowerShellConsole/ModuleInitialization/1 - Init/2 - Synchronise with GitHub.ps1
@@ -5,19 +5,7 @@ $consoleGitHubSyncer = $PowerShellConsoleConstants.Executables.ConsoleGitHubSync
 
 $consoleGitHubSyncerCheck = Start-Process -FilePath $consoleGitHubSyncer -ArgumentList "-Check $($PowerShellConsoleConstants.InstallPath)" -PassThru -NoNewWindow -Wait
 
-switch ($consoleGitHubSyncerCheck.ExitCode) {
-	0 {
-		Write-Host -ForegroundColor Green "Git repository is up to date with GitHub changes"
-	}
-	10 {
-		Write-Host -ForegroundColor Red  "Unable to synchronize while the Git working tree has uncommitted changes"
-	}
-	160 {
-		Write-Host -ForegroundColor Red  "Git exited unexpectedly"
-	}
-	1306 {
-		[System.Diagnostics.Process]::Start($PowerShellConsoleConstants.Executables.Hstart, "/DELAY=3 `"`"$consoleGitHubSyncer`" -Synchronize `"$($PowerShellConsoleConstants.InstallPath)`" `"$($PowerShellConsoleConstants.Executables.ConEmu)`" `"/cmd {PowerShell}`" `"")
-		[System.Environment]::Exit(0)
-	}
-	default { 	Write-Warning "ConsoleGitHubSyncer failed with error code $($consoleGitHubSyncerCheck.ExitCode)" }
+if ($consoleGitHubSyncerCheck.ExitCode -eq 1306) {
+	[System.Diagnostics.Process]::Start($PowerShellConsoleConstants.Executables.Hstart, "/DELAY=3 `"`"$consoleGitHubSyncer`" -Synchronize `"$($PowerShellConsoleConstants.InstallPath)`" `"$($PowerShellConsoleConstants.Executables.ConEmu)`" `"/cmd {PowerShell}`" `"")
+	[System.Environment]::Exit(0)
 }
