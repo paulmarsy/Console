d9bbae306db7aa24902317b6ee2d926d0100cf19
Paul Marston <paul@marston.me>
2014-12-20 01:47:45 +0000

    Fixing a bug when there are spaces in the path
    Former-commit-id: 8be68c518815a209d9a7dffd528ffff306ba5b96
    Former-commit-id: 52f270a6d84d5043a25e5646aae7eff2e1e92d1b
    Former-commit-id: 8478a8b96546d3f982c5745a2bfb335cc1dc9f8c
    Former-commit-id: 2e09496ccc46d733e0127a7c79163f45c7e51832
    Former-commit-id: 7d9e2a200061511b1406ddf1e29a840ed01ff9ff
    Former-commit-id: 00418dfbc71f124e0948660a2e55a24a1c830ed1
    Former-commit-id: d60316cf2226a65ceff4b57ed86bda79318fb919
    Former-commit-id: 59055a9ec5d6448599651a8e0b57ff7bc9644f3e

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Pscx.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Pscx.ps1
@@ -1,7 +1,7 @@
 param([switch]$GetModuleStepDetails)
-if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
+if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
 
 Import-Module Pscx -Global -ArgumentList @{
-    TextEditor = $PowerShellConsoleConstants.Executables.SublimeText
+    TextEditor = "`"$($PowerShellConsoleConstants.Executables.SublimeText)`""
     CD_EchoNewLocation = $false
 }
