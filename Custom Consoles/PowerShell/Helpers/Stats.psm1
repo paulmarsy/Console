4b98f79c2b2f61df8e2730f375e086bef44c00a8
Paul Marston <paul@marston.me>
2014-12-21 18:57:54 +0000

    Refactoring the stats module to be sorted and also track the underlying command from alias uses
    Former-commit-id: 22571799e30c564675955a916fcd53323ab80cae
    Former-commit-id: e754a6c5db6f66c8b403e365fc26904374e6df99
    Former-commit-id: 53490d875eec80aa2809db230c4954ba68cf9813
    Former-commit-id: f3c27d166822cd97bce5e7f50502238c96494951
    Former-commit-id: 9cb59225bd964afffb11ede8deed046d18589094
    Former-commit-id: 3a79d08a2c4258f2b7cb3b2a1610b84b7b8c4e22
    Former-commit-id: 5397ee4ba853ddbc30e964044bc161e662182546
    Former-commit-id: bde7b569df398584af0fe7903078bb8e16106d0b

--- Custom Consoles/PowerShell/Helpers/Stats.psm1
+++ Custom Consoles/PowerShell/Helpers/Stats.psm1
@@ -5,12 +5,15 @@ Set-StrictMode -Version Latest
 $ModuleStatsFilePath = Join-Path $AppSettingsFolder "ModuleStats.json"
 
 function Get-DefaultStats {
+	param($module)
+
 	$defaultStats = @{}
-	$module = Get-Module PowerShellConsole
+
 	if ($null -ne $module) {
 		$module.ExportedFunctions.Keys | % { $defaultStats.Add($_, 0) }
 		$module.ExportedAliases.Keys | % { $defaultStats.Add($_, 0) }
 	}
+	
 	return $defaultStats
 }
 
@@ -40,15 +43,22 @@ function Save-ModuleStats {
 }
 
 function Update-ModuleUsageStats {
-	$defaultStats =  Get-DefaultStats
-
+	$module = Get-Module CustomPowerShellConsole
+	$defaultStats =  Get-DefaultStats $module
+	
 	$currentStats = Load-ModuleStats $defaultStats
+
 	Microsoft.PowerShell.Core\Get-History |
 		% { [System.Management.Automation.PSParser]::Tokenize($_.CommandLine, [ref]$null) } | 
 		? Type -eq ([System.Management.Automation.PSTokenType]::Command) | 
 		% Content |
-		% {	if ($currentStats.ContainsKey($_)) { $currentStats[$_]++ } }
+		% {
+			if ($currentStats.ContainsKey($_)) { $currentStats[$_]++ }
+			else { return }
 
+			$aliasedCommand = $module | % ExportedAliases | % Values | ? Name -eq $_ | % Definition
+			if ($null -ne $aliasedCommand) { $currentStats[$aliasedCommand]++ }
+		}
 
 	Save-ModuleStats $currentStats
 
@@ -56,11 +66,12 @@ function Update-ModuleUsageStats {
 }
 
 function Show-ModuleUsageStats {
-	$defaultStats =  Get-DefaultStats
+	$module = Get-Module CustomPowerShellConsole
+	$defaultStats =  Get-DefaultStats $module
 
 	$currentStats = Load-ModuleStats $defaultStats
 
-	$currentStats.GetEnumerator() | Sort-Object -Property Value -Descending | Format-Table -AutoSize -Property @(@{Label = "Name"; Expression = { $_.Key }}, @{Label = "Usage Count"; Expression = { $_.Value }})
+	$currentStats.GetEnumerator() | Sort-Object -Property Value,Key -Descending | Format-Table -AutoSize -Property @(@{Label = "Name"; Expression = { $_.Key }}, @{Label = "Usage Count"; Expression = { $_.Value }})
 }
 
 function Initialize-Destructor {
