3c21d2ca090c70b923d402690893595265868974
Paul Marston <paul@marston.me>
2014-12-22 11:17:16 +0000

    Fixing tabbing issue
    Former-commit-id: 1c971d5a1592181021eb2dbb496deabdd6c7adf8
    Former-commit-id: c00f9940d425ae43678215fad7552cea7090cb21
    Former-commit-id: 429cf045fc91dbba80253b6f576911c30c93701c
    Former-commit-id: 263ee8448bc415d0efbff05288b6a3cf2f8f458e
    Former-commit-id: 6074cb0c46d7ae69aef3f2f8453d875a0a188ee0
    Former-commit-id: 04c9377877df225bc65bb19f1c6e4c0f52ce7bbf
    Former-commit-id: 1b2671d5e52fb5bfa4a6c9e94d2044d1b5185ced
    Former-commit-id: 03998b8ca37e2732b4714334f1567dcdcef5b8eb

--- Custom Consoles/PowerShell/Exports/Functions/Clear-PowerShellTempFolder.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Clear-PowerShellTempFolder.ps1
@@ -1,10 +1,10 @@
 function Clear-PowerShellTempFolder {
-		try{
-			Push-Location $ProfileConfig.General.UserFolder
-			Remove-Item $ProfileConfig.General.TempFolder -Force -Recurse
-			New-Item $ProfileConfig.General.TempFolder -Type Directory -Force | Out-Null
-		}
-		finally { 
-			Pop-Location
-		}
+	try{
+		Push-Location $ProfileConfig.General.UserFolder
+		Remove-Item $ProfileConfig.General.TempFolder -Force -Recurse
+		New-Item $ProfileConfig.General.TempFolder -Type Directory -Force | Out-Null
+	}
+	finally { 
+		Pop-Location
+	}
 }
