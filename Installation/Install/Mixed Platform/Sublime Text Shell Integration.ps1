33fd1060bfc20531b24df717a0f8ec2a39d42f93
Paul Marston <paul@marston.me>
2014-12-26 00:12:07 +0000

    Fixing a perf issue
    Former-commit-id: d5a17988c183afc94bc0073aa0cc69c00c98a080
    Former-commit-id: 5a284eb0342e009ad7e3810f0e347216ecfd1011
    Former-commit-id: d70622418670a4134b82d3ec13367fe33bb2fe3e
    Former-commit-id: e2b62a8137d7170ab27cafc45e0845db7750c7aa
    Former-commit-id: 405184018ecdbbc3880571cac6d29a7d45e73684
    Former-commit-id: f382e48cb6ab6f4bf52c785f4dd9e165d95c38f9
    Former-commit-id: 980b1908fd3fca5beee2f5f067eee222971eddf1
    Former-commit-id: 487deb277873bfd8f68d06efede89cf3112769b9

--- Installation/Install/Mixed Platform/Sublime Text Shell Integration.ps1
+++ Installation/Install/Mixed Platform/Sublime Text Shell Integration.ps1
@@ -6,19 +6,19 @@ Invoke-InstallStep "Configuring Sublime Text Shell Integration" {
 	$sublimeIcon = "$($sublimeTextExecutable),0"
 
 	New-Item "HKCU:\Software\Classes\*\shell\Sublime Text" -Force | Out-Null
-	New-ItemProperty "HKCU:\Software\Classes\*\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
+	New-ItemProperty -LiteralPath "HKCU:\Software\Classes\*\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
 	New-Item "HKCU:\Software\Classes\*\shell\Sublime Text\command" -Value """$sublimeTextExecutable"" ""%L""" -Type String -Force | Out-Null
 
 	New-Item "HKCU:\Software\Classes\Directory\Background\shell\Sublime Text" -Force | Out-Null
-	New-ItemProperty "HKCU:\Software\Classes\Directory\Background\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
+	New-ItemProperty -LiteralPath "HKCU:\Software\Classes\Directory\Background\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
 	New-Item "HKCU:\Software\Classes\Directory\Background\shell\Sublime Text\command" -Value """$sublimeTextExecutable"" ""%V""" -Type String -Force | Out-Null
 
 	New-Item "HKCU:\Software\Classes\Folder\shell\Sublime Text" -Force | Out-Null
-	New-ItemProperty "HKCU:\Software\Classes\Folder\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
+	New-ItemProperty -LiteralPath "HKCU:\Software\Classes\Folder\shell\Sublime Text" "Icon" -Value "$sublimeIcon" -Type String -Force | Out-Null
 	New-Item "HKCU:\Software\Classes\Folder\shell\Sublime Text\command" -Value """$sublimeTextExecutable"" ""%L""" -Type String -Force | Out-Null
 
 	New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Notepad.exe" -Force | Out-Null
-	New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Notepad.exe" "Debugger" -Value """$sublimeLauncher"" -z" -Type String -Force | Out-Null
+	New-ItemProperty -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Notepad.exe" "Debugger" -Value """$sublimeLauncher"" -z" -Type String -Force | Out-Null
 }
 
 Invoke-InstallStep "Setting up Sublime Text Start Shortcut" {
