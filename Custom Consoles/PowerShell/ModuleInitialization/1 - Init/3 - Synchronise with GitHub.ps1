1ab5d8271981dcee5e5269d29d9baf4cba4acfb4
Paul Marston <paul@marston.me>
2014-12-26 23:15:12 +0000

    Reordering and adding in the periodic check code
    Former-commit-id: 2628b957e34364feca99ae39572fafebf1f533b0
    Former-commit-id: 3daeaabacad312039e039a76bcdfa1650e5398b4
    Former-commit-id: 462067f4bac6259dd55b9db8ddaa402863b1a3e5
    Former-commit-id: fcd2b1c53b9c4dec48c46d25bdd5c12d094e0eae
    Former-commit-id: d0ad6709d2729e74cde46838f519d2a0e6bf9219
    Former-commit-id: 30704714d39816bdd3dc53d398dd5f4cb76fad38
    Former-commit-id: 2be66294bb72757cac284311ecbe24a0e216b3c2
    Former-commit-id: d8559bfc83de760f74ed33c87f276f3c5f0156fd

--- Custom Consoles/PowerShell/ModuleInitialization/1 - Init/3 - Synchronise with GitHub.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/1 - Init/3 - Synchronise with GitHub.ps1
@@ -1,11 +1,20 @@
 param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
 
+$lastSync = New-Object -Type System.DateTime -ArgumentList $ProfileConfig.Git.LastAutoSyncTickTime
+$nextSyncAfter = $lastSync.AddSeconds($ProfileConfig.Git.SyncIntervalInSeconds)
+if ($nextSyncAfter.Ticks -gt (Get-Date | Select-Object -ExpandProperty Ticks)) {
+	Write-Host -ForegroundColor Yellow "Not performing GitHub auto sync, next auto sync after $($nextSyncAfter.ToShortDateString()) at $($nextSyncAfter.ToShortTimeString())"
+	return
+}
+
 $consoleGitHubSyncer = $PowerShellConsoleConstants.Executables.ConsoleGitHubSyncer
 
 $consoleGitHubSyncerCheck = Start-Process -FilePath $consoleGitHubSyncer -ArgumentList "-Check $($PowerShellConsoleConstants.InstallPath)" -PassThru -NoNewWindow -Wait
 
 if ($consoleGitHubSyncerCheck.ExitCode -eq 1306) {
+	$ProfileConfig.Git.LastAutoSyncTickTime = [long](Get-Date | Select-Object -ExpandProperty Ticks)
+	Save-ProfileConfig -Quiet
 	try {
 		$mutex = [System.Threading.Mutex]::new($false, $PowerShellConsoleConstants.MutexGuid)
 		while(-not $mutex.WaitOne([TimeSpan]::FromSeconds(1).TotalMilliseconds, $false)) {
