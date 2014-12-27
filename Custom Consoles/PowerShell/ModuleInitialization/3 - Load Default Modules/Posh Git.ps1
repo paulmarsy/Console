3888d4cad46f4209aef9ba888f30a1573b35f125
Paul Marston <paul@marston.me>
2014-12-27 19:02:30 +0000

    Removing the starting of the ssh-agent as it isnt being used
    Former-commit-id: 87c9ebb16303912b409ea028fcd49672bac4e4f2
    Former-commit-id: 45113bbe649b0d89e460067c956c813c9c336e02
    Former-commit-id: 70b24e853b14eabde102181fd7ca7272d8a896a0
    Former-commit-id: 0550ba2e65c43c4070f9f267150c64476b96cf01
    Former-commit-id: dc306493d15b712dac1a0622ab7aa71b7f04f736
    Former-commit-id: 5664a4d12a350f412f980328cc09c00f63d210ea
    Former-commit-id: f802aaa48dbd226b54789087962c66901469de60
    Former-commit-id: 543c1a45df0d3b2a2717153bcc93c0cefdb6246a

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
@@ -4,14 +4,8 @@ if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
 Export-Module posh-git
 $GitPromptSettings.EnableWindowTitle = "Windows PowerShell - Git - "
 Enable-GitColors
-Start-SshAgent -Quiet
-
-Register-EngineEvent -SourceIdentifier ([System.Management.Automation.PsEngineEvent]::Exiting) -Action {
-	Stop-SshAgent
-} | Out-Null
 
 $ExecutionContext.SessionState.Module.OnRemove = {
-	Stop-SshAgent
 	if (Test-Path Variable:Global:VcsPromptStatuses) {
     	Remove-Variable -Name "VcsPromptStatuses" -Scope Global
     }
