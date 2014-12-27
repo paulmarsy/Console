bdf2153a5a57637da4eb892e4894facfff623fea
Paul Marston <paul@marston.me>
2014-12-27 22:10:20 +0000

    Removing an unnecessary line
    Former-commit-id: a5923feb4558b9906b9b98d7a0c4c64b95f41a55
    Former-commit-id: d5834e5048916af5d21080898dfbf3ba9d3094a8
    Former-commit-id: 4d924a59beb21fc1bd47e0407f8ab75f4b9057a3
    Former-commit-id: ee347c0fd1c60ac7bf3b2dc4d75917866f9ef558
    Former-commit-id: 45763d7bafa24f850ba881396057217afeac8bed
    Former-commit-id: a3d7c77e6984ada8cf61d25a9ee25674242130fb
    Former-commit-id: aff4139486a77bc9cff4fb448f2d91bd2ff2dc30
    Former-commit-id: 981207886cfa30f377c422c3356b0311c92ab25c

--- Custom Consoles/PowerShell/Helpers/Stats.psm1
+++ Custom Consoles/PowerShell/Helpers/Stats.psm1
@@ -82,8 +82,6 @@ function Initialize-Destructor {
 	$psEngineExitEventJob = Register-EngineEvent -SourceIdentifier ([System.Management.Automation.PsEngineEvent]::Exiting) -Action $destructor
 
 	$ExecutionContext.SessionState.Module.OnRemove = {
-	    $psEngineExitEventJob | Stop-Job -PassThru | Remove-Job
-
 	    $destructor.Invoke()
 	}.GetNewClosure()
 }
