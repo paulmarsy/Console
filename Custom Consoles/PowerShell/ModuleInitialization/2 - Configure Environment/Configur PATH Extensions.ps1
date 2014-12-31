294354039a61a7576d1a6997faf8bd265e61c0b8
Paul Marston <paul@marston.me>
2014-12-31 17:54:54 +0000

    Tweaking how path extensions are detected
    Former-commit-id: 0a601b9060df1974e95d49ed1889d553a4952f24
    Former-commit-id: 4be8b2bd23d6e64524f3ac03b74e9a643248f3d3
    Former-commit-id: 8272dcbb1ebcfa0a87274aef4d2419394f216661
    Former-commit-id: 29851d08189fc829062020b44692ab6f09e7af13
    Former-commit-id: 67808dbed1f2e766bae7cc6d15565f99ca113f2a
    Former-commit-id: ea24d97332ba2990d74dbe5be2917ef2fab7e94b
    Former-commit-id: 624893513dfb730887c2d35d58a53f9364252e05
    Former-commit-id: bc7f7c7d7de3ae03e6b7eab39462c6fd92227c07

--- Custom Consoles/PowerShell/ModuleInitialization/2 - Configure Environment/Configur PATH Extensions.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/2 - Configure Environment/Configur PATH Extensions.ps1
@@ -1,10 +1,12 @@
 param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 3; Critical = $false}) }
 
+$pathExtensionsDir = Join-Path $ProfileConfig.Module.InstallPath "Libraries\PATH Extensions"
+
 $Path = @(
 	$Env:PATH
-	(Join-Path $ProfileConfig.Module.InstallPath "Libraries\PATH Extensions")
-	(Get-ChildItem -Path (Join-Path $ProfileConfig.Module.InstallPath "Libraries\PATH Extensions") -Directory | Select-Object -ExpandProperty FullName) 
+	$pathExtensionsDir
+	(Get-ChildItem -Path $pathExtensionsDir -Directory | % FullName) 
 ) -Join ";"
 
 [System.Environment]::SetEnvironmentVariable("PATH", $Path, [System.EnvironmentVariableTarget]::Process)
