3930f8e6e05e2902d91d218f01e356602986538a
Paul Marston <paul@marston.me>
2015-01-01 13:02:10 +0000

    Only opening UAC version of the command prompt if not already promoted
    Former-commit-id: 6a3f8fa721afad55697bdc4115a7996c6b798f27
    Former-commit-id: 303857c712da1670b218e03b67827f5fc98b5a91
    Former-commit-id: 623c53abb8e71166d285ee1db201045d92807d33
    Former-commit-id: 5c10f8a536f62819eba33f40c6c492a889bfd94e
    Former-commit-id: 90c89421a611d36be1f1eefee58db552b6e26006
    Former-commit-id: 84562efcbcf9e668f2f898cce634a2f6b45a7b02
    Former-commit-id: eee9a9f05e5e2026c1d1be12ea52ee20f45bd13e
    Former-commit-id: d7abb4660577e6ddb9f92d0b836018c5854afb31

--- Custom Consoles/PowerShell/Exports/Functions/PowerShell Utilities/Open-Superuser.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/PowerShell Utilities/Open-Superuser.ps1
@@ -1,4 +1,9 @@
 function Open-Superuser
 {
-	Restart-PowerShellConsole -Su
+	$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+	$windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal $windowsIdentity
+	if ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+		throw "Already running with Administrator access"
+	}
+	else { Restart-PowerShellConsole -Su }
 }
