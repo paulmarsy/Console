9f534486499d58ed0b88be9ea25126d44227b51c
Paul Marston <paul@marston.me>
2015-01-01 13:32:20 +0000

    Removing moved files
    Former-commit-id: 0afb27becbfed10530ee2bd3028ba205576a3b17
    Former-commit-id: b3de2f892b67af0a4d261cf4464bad8433a4531a
    Former-commit-id: 6e7d656d1ad3afbeff28bdee1016e7843f622f7c
    Former-commit-id: 73e91d2627c502a0dd709cdd259f0b62d7bcc8f3
    Former-commit-id: 32867aab9777710f9a85ee7e2e333212566eea1b
    Former-commit-id: 8cebe77e82f0efae96dac0c31e1d9fa66ba6c455
    Former-commit-id: f83bed8ba363aa7d2ad90f7201e1609fc78424b2
    Former-commit-id: 4d5035ee42163ca06d2895a448b1b9b58bf6dc19

--- Custom Consoles/PowerShell/Exports/Functions/GitHub/_Internal/_getCurrentBranch.ps1
+++
@@ -1,7 +0,0 @@
-function _getCurrentBranch {
-	_workOnConsoleWorkingDirectory {
-    	$output = & git rev-parse --abbrev-ref HEAD
-    	if ($LASTEXITCODE -ne 0) { throw "Git command returned exit code: $LASTEXITCODE" }
-    	return $output
-	} -ReturnValue
-}
