1fa693d80280c91a828b53aa9991e39cd5544e86
Paul Marston <paul@marston.me>
2015-01-02 19:51:13 +0000

    Tweaking the configuring of git to increase startup time by moving most actions to install time
    Former-commit-id: dbf897127c8aea654a97c4e275ae6dc4075a34b5
    Former-commit-id: a5b0224c82f33200edaa6db29e073687c8d6591a
    Former-commit-id: 12591acab27ec0ea0bc2cbb5a06d51147c5e94f2
    Former-commit-id: 8e865e999b97981fa4bd129f6d9f1611b5da5621
    Former-commit-id: 45e06e18bfe5c6f3074c46b88b979466381858e2
    Former-commit-id: ef0f8b8db22c5f592c0038cdc99e87611f5306c1
    Former-commit-id: bdd9562f970652c86a2ff83550e21b6104f79371
    Former-commit-id: 26af34a74225cf843249e1164dcc8580007622d3

---
+++ Custom Consoles/PowerShell/ModuleInitialization/2 - Configure Environment/Configure Git.ps1
@@ -0,0 +1,30 @@
+param([switch]$GetModuleStepDetails)
+if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
+
+$Path = @(
+	(Join-Path $PowerShellConsoleConstants.GitInstallPath "bin")
+	(Join-Path $PowerShellConsoleConstants.GitInstallPath "mingw\bin")
+	(Join-Path $PowerShellConsoleConstants.GitInstallPath "cmd")
+	$Env:PATH
+) -Join ';'
+
+[System.Environment]::SetEnvironmentVariable("PATH", $Path, [System.EnvironmentVariableTarget]::Process)
+
+$gitConfigFileLocation = Join-Path $ProfileConfig.Module.AppSettingsFolder "Gitconfig.config"
+$gitConfigLockFileLocation = [System.IO.Path]::ChangeExtension($gitConfigFileLocation, "config.lock")
+if (Test-Path $gitConfigLockFileLocation) {
+	Remove-Item -Path $gitConfigLockFileLocation -Force
+}
+
+function SetGitConfig {
+	param(
+		$Name,
+        $Value
+    )
+
+    & git.exe config --file "$gitConfigFileLocation" $Name $Value
+}
+
+# Set the user details
+SetGitConfig "user.name" $ProfileConfig.Git.Name
+SetGitConfig "user.email" $ProfileConfig.Git.Email
