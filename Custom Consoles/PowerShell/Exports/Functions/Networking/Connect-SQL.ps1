3df5a0b6cdff9f3f7529457deb3e5b734b80c05f
Paul Marston <paul@marston.me>
2015-01-03 23:52:03 +0000

    Fixing a bug (test-path was the wrong way around), also making it so you can open ssms without specifying a server to connect to
    Former-commit-id: b172b5349b78371bd0f4ec882ea46d5ed0d111db
    Former-commit-id: 06cc4b12d84789e38d7fb7bdc2b55951bd674406
    Former-commit-id: 90e74e8177b9fdfb5cb643d7614e7339c654353d
    Former-commit-id: 56864575ffc0d3e646892a14bae4a71812b98983
    Former-commit-id: ecd6a82163f6b6f3427523a327dcc1caee85c3aa
    Former-commit-id: 42359e2d616a0fb79fbf6cdf6f49bd1c0416f618
    Former-commit-id: e50f10560dd34fe434e7bcbb8f0f799f1e4a94db
    Former-commit-id: 8c8a54d36ab6458b6a383ff88bcd2ecc356f5a01

--- Custom Consoles/PowerShell/Exports/Functions/Networking/Connect-SQL.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Networking/Connect-SQL.ps1
@@ -1,15 +1,14 @@
 function Connect-SQL {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true,Position=0)]$SqlServer,
+        [Parameter(Position=0)]$SqlServer,
         [Parameter(Position=1)]$DefaultDatabase,
         $Username = $null,
         $Password = $null
     )
 
-
     $sqlServerClientRegistryKey = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SQL Server\120\Tools\ClientSetup"
-    if (-not (Test-Path $sqlServerClientRegistryKey)) {
+    if (Test-Path $sqlServerClientRegistryKey) {
         $sqlToolsPath = Get-ItemProperty -Path $sqlServerClientRegistryKey | % "SqlToolsPath"
     }
 
@@ -22,13 +21,16 @@ function Connect-SQL {
         throw "Unable to find SQL Server Management Tools, has it been installed?"
     } 
 
-    $arguments = @("-nosplash", "-S $SqlServer")
-    if ($DefaultDatabase) { $arguments += @("-d $DefaultDatabase") }
-    if ($null -ne $Username -and $null -ne $Password) { 
-        $arguments += @("-U $Username")
-        $arguments += @("-P $Password")
-    } else {
-        $arguments += @("-E")
+    $arguments = @("-nosplash")
+    if ($SqlServer) {
+        $arguments += @("-S $SqlServer") 
+        if ($DefaultDatabase) { $arguments += @("-d $DefaultDatabase") }
+        if ($null -ne $Username -and $null -ne $Password) { 
+            $arguments += @("-U $Username")
+            $arguments += @("-P $Password")
+        } else {
+            $arguments += @("-E")
+        }
     }
 
     Start-Process -FilePath $ssmsPath -ArgumentList $arguments
