ce9d54c05a7fd5eb21e016d38b005f6e93ffda9c
Paul Marston <paul@marston.me>
2015-01-03 15:41:54 +0000

    Fixing a bug if the folder already exists but isnt a symlink
    Former-commit-id: 13eebdf1d58e6c1ff46550f6d6feefddfa2102b9
    Former-commit-id: 6548bb73a74ab6eb3adf43e53395013689e15e81
    Former-commit-id: 1ebdee9bd7c87911bd133507d667e03fa8721a21
    Former-commit-id: c708eecdfa97a2dd49a453e26cf74e6af5c4f9fe
    Former-commit-id: d9f3d8bde5cc41db7b2a33e1ba1ecde4e62ddb0e
    Former-commit-id: f3b9be47d8d5fdf4a3687f84ef9beb5198e79102
    Former-commit-id: d353b8b0e62a017adce478d0447cb6a98baf7549
    Former-commit-id: c4b501a7aede7680ce73eb09a03487d8db587193

--- Installation/Install/Mixed Platform/SmartGit Updates Directory.ps1
+++ Installation/Install/Mixed Platform/SmartGit Updates Directory.ps1
@@ -6,18 +6,29 @@ Invoke-InstallStep "Redirecting SmartGit updates directory" {
 	}
 
 	@("updates", "updater") | % {
-		$originalPath = Join-Path $smartGitCustomSettingsPath $_
-		if (Test-Path $originalPath) {
-			& cmd.exe /Q /C	rmdir "$originalPath" *>$null
-			if ($LASTEXITCODE -ne 0) {
-				throw "Unable to remove existing symbolic link to SmartGit $_ directory (exit code $LASTEXITCODE)"
-			}
-		}
-
 		$redirectedPath = Join-Path $Env:TEMP "SmartGit-$($_)"
 		if (-not (Test-Path $redirectedPath)) {
 			New-Item $redirectedPath -Type Directory -Force | Out-Null
 		}
+
+		$originalPath = Join-Path $smartGitCustomSettingsPath $_
+		if (Test-Path $originalPath) {
+			if ([bool]((Get-Item -Path $originalPath | % Attributes) -band [IO.FileAttributes]::ReparsePoint)) {
+				& cmd.exe /Q /C	rmdir "$originalPath" *>$null
+				if ($LASTEXITCODE -ne 0) {
+					throw "Unable to remove existing symbolic link to SmartGit $_ directory (exit code $LASTEXITCODE)"
+				}
+			} else {
+				Get-ChildItem -Path $originalPath -Force | % { Move-Item -Path $_.FullName -Destination $redirectedPath -Force }
+
+				$remainingFiles = Get-ChildItem -Path $originalPath -Force | Measure-Object -Line | Select-Object -ExpandProperty Lines
+				if ($remainingFiles -ge 1) {
+					throw "$remainingFiles files still exist in the original SmartGit folder, please manually move them"
+				}
+
+				Remove-Item -Path $originalPath -Recurse -Force
+		}
+
 		& cmd.exe /Q /C mklink /D "$originalPath" "$redirectedPath" *>$null
 		if ($LASTEXITCODE -ne 0) {
 			throw "Unable to create symbolic link to custom SmartGit $_ directory (exit code $LASTEXITCODE)"
