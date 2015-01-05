eebf58a4449061702e52e099f0c773f75be891fb
Paul Marston <paulm@asos.com>
2015-01-05 20:15:52 +0000

    Fixing a bug when importing the tfs powertools module as the name and filepath to the dll need to be specified
    Former-commit-id: 0182bdaa9819cb18fc96e1a5317e67df2dd0ddab
    Former-commit-id: 8b94f6501e6aed41bb55c8b47602d44f809a91b9
    Former-commit-id: a111c69778c3c87f6327d20627a876f5984392ba
    Former-commit-id: 410717f83119386b46c55082aac24f47419fc129
    Former-commit-id: 922581934a0da62795d238547d3be90216a730d2
    Former-commit-id: 081c576f66895887a21ddd73e5432bfdce1258e0
    Former-commit-id: 648ed910957ab1e61bc90144fa6cf5cd835ba471
    Former-commit-id: 7cbe1633dd26661ab5177edc7cf0fff3776bbf6a

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Microsoft Team Foundation Server 2013 Power Tools.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Microsoft Team Foundation Server 2013 Power Tools.ps1
@@ -3,7 +3,7 @@ if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
 
 if (Test-Path Env:TFSPowerToolDir) {
 	$tfsPowerTools = Join-Path $env:TFSPowerToolDir "Microsoft.TeamFoundation.PowerTools.PowerShell.dll"
-	Export-Module $tfsPowerTools
+	Export-Module -ModuleName "Microsoft.TeamFoundation.PowerTools.PowerShell" -ModuleFile $tfsPowerTools
 } else {
 	Write-Warning "Unable to import the soft Team Foundation Server 2013 Power Tools module"
 }
