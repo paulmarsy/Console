330d3c07e8aff6b63a608b3af432d3aac095d3a2
Paul Marston <paul@marston.me>
2015-01-09 00:45:05 +0000

    Forcing creation so reloads don't error
    Former-commit-id: c326f001795a345bab086567f4abc0c479dc5c37
    Former-commit-id: 64c071c72ad943ea378ce833409b2ed6db22c395
    Former-commit-id: 8a108659a8efb3e26d4f60827ea6b77c3267cef1
    Former-commit-id: 6a108725a000446126e8aca016ff97dd2f4c7ff2
    Former-commit-id: ddf38514aa8f5cba395fd37e2bea91bf3300df67
    Former-commit-id: 0fab028cf0526a966298dd6c5d71ac9b39b2d747
    Former-commit-id: 0445bfd1d7519a14a8949694da262ae5b6f364fc
    Former-commit-id: bbb7b2b266444b5865796a93696bf7649f838507

--- Custom Consoles/PowerShell/Helpers/Module Pre-Init.ps1
+++ Custom Consoles/PowerShell/Helpers/Module Pre-Init.ps1
@@ -4,4 +4,4 @@ $ModuleInitLevel = & (Join-Path $PSScriptRoot "Module Init Level.ps1")
 
 . (Join-Path $PSScriptRoot "Export-Module.ps1")
 
-New-Variable -Name OnIdleScriptBlockCollection -Scope Global -Value ({@()}.Invoke())
+New-Variable -Name OnIdleScriptBlockCollection -Scope Global -Value ({@()}.Invoke()) -Force
