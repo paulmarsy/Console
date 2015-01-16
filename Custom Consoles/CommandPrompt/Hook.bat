e7f786260cceef1e928b3cc1378af4f8efbd7322
Paul Marston <paul@marston.me>
2015-01-16 22:52:00 +0000

    Only displaying a message if opened from explorer, else the cmd hook should'nt do anything to alter the natural output
    Former-commit-id: cad47d06ca2745643bef29e2834a3c51c178cd56
    Former-commit-id: 1fd96a639d9b407f2a52095bedc6e94e48512b43
    Former-commit-id: 7f3fed7f8b6e3de63af29e8f2671b45a9bf6eb76
    Former-commit-id: 12e75a085a0c354c0d824206a107abf05c290039
    Former-commit-id: 1659f92d2a8e4247ba98ecc42f87cd5b91da100a
    Former-commit-id: ae60a57f483f2a25ac39967cdc13c5ed9af86dbb
    Former-commit-id: caeeefdd28b9f8cf9ea94f86279811028b490b5e
    Former-commit-id: 0b1c33a5d509cda69a4f79c7c66b157c86672888

--- Custom Consoles/CommandPrompt/Hook.bat
+++ Custom Consoles/CommandPrompt/Hook.bat
@@ -4,14 +4,18 @@ SETLOCAL EnableDelayedExpansion
 
 SET InitializeFile="%CustomConsolesInstallPath%Custom Consoles\CommandPrompt\Initialize.bat"
 
-"%CustomConsolesInstallPath%\Libraries\Custom Helper Apps\ExtensibleEnvironmentTester\ExtensibleEnvironmentTester.exe"
+"%CustomConsolesInstallPath%Libraries\Custom Helper Apps\ExtensibleEnvironmentTester\ExtensibleEnvironmentTester.exe"
 IF %ERRORLEVEL% == 0 (
 	CALL %InitializeFile%
 	ENDLOCAL && SET PROMPT=%PROMPT%
 ) ELSE (
- 	ECHO Not starting Command Prompt Console as we are not using ConEmu. To override:
-	ECHO CALL %InitializeFile%
-	ENDLOCAL
+	IF %ERRORLEVEL% == 10 (
+ 		ECHO Not starting Command Prompt Console as we are not using ConEmu. To override:
+		ECHO CALL %InitializeFile%
+		ENDLOCAL
+	) ELSE (
+		ENDLOCAL
+	)
 )
 
 EXIT /B 0
