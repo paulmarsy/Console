131a1003a2d3e5088a186bc67c0c626f32ca3771
Paul Marston <paul@marston.me>
2015-01-16 19:38:51 +0000

    Fixing start-build
    Former-commit-id: a35ec5d56df2bbdbb123ec82a9f63a565c8d7888
    Former-commit-id: bec3f47ca67821a5358aa680020ba0144037d4bc
    Former-commit-id: 95f7f41b3d848f836025f67da27b2797be3815df
    Former-commit-id: 1e1d7d834205e64f800acfe01a719399da9ac474
    Former-commit-id: 6deb5bdc08cc233a4c53d1d8367c632ccd70fadf
    Former-commit-id: 7e8f88409cfe5be6e35a17008cbcc183368e09ab
    Former-commit-id: 73ae303d9112a4c3f9d824bae6478a14b7ead21e
    Former-commit-id: 2c5a4cf21102ab5608a68a98118b80a4f5d6b68c

--- Custom Consoles/PowerShell/Exports/Functions/TFS/Start-Build.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/TFS/Start-Build.ps1
@@ -9,7 +9,7 @@ function Start-Build {
 		[Parameter(ParameterSetName="Default")]
 		[Parameter(ParameterSetName="Interactive")]
 		[Parameter(ParameterSetName="NonInteractive")]
-		$ProcessParameters,
+		[Hashtable]$ProcessParameters,
 		[Parameter(ParameterSetName="Interactive", Mandatory = $true)][switch]$Wait,
 		[Parameter(ParameterSetName="Interactive")][switch]$ContinueOnError,
 		[Parameter(ParameterSetName="NonInteractive")][switch]$PassThru
@@ -18,19 +18,23 @@ function Start-Build {
 	PROCESS {
 		$BuildDefinition | foreach {
 			$currentBuildDefinition = $_
+			$buildServer = $_.BuildServer
 
-			$buildRequest = $currentBuildDefinition.BuildServer.CreateBuildRequest($currentBuildDefinition)
+			$buildRequest = $buildServer.CreateBuildRequest($currentBuildDefinition)
 
-			if (-not ([string]::IsNullOrWhiteSpace($ProcessParameters))) {
-				$workflowProcessProperties = [Microsoft.TeamFoundation.Build.Workflow.WorkflowHelpers]::DeserializeWorkflow($currentBuildDefinition.Process.Parameters).Properties
+			if ($null -ne $ProcessParameters) {
+				$workflowProcessProperties = [Microsoft.TeamFoundation.Build.Workflow.WorkflowHelpers]::DeserializeWorkflow($currentBuildDefinition.Process.Parameters) | % Properties | % Name
 				$currentProcessParameters = [Microsoft.TeamFoundation.Build.Workflow.WorkflowHelpers]::DeserializeProcessParameters($buildRequest.ProcessParameters)
-				$ProcessParameters.GetEnumerator() | ?| % {
-
+				$ProcessParameters.GetEnumerator() | ? { $workflowProcessProperties -contains $_.Key } | % {
+					if ($currentProcessParameters.ContainsKey($_.Key)) {
+						$currentProcessParameters.Remove($_.Key)
 					}
+					$currentProcessParameters.Add($_.Key, $_.Value)
+				}
 				$buildRequest.ProcessParameters = [Microsoft.TeamFoundation.Build.Workflow.WorkflowHelpers]::SerializeProcessParameters($currentProcessParameters)
 			}
 
-			$queuedBuild = $currentBuildDefinition.BuildServer.QueueBuild($buildRequest)
+			$queuedBuild = $buildServer.QueueBuild($buildRequest)
 
 			if ($PsCmdlet.ParameterSetName -eq "Interactive") {
 				Write-Host -NoNewLine "Starting build '$($currentBuildDefinition.Name)'... "
