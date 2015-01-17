f365b2bde0a6beb0f00812c9930cbb184e1a3ee8
Paul Marston <paul@marston.me>
2015-01-17 22:27:48 +0000

    Exiting the console if it is not up to date locally
    Former-commit-id: 80130535b208403096a82fabf87a5e0a08b58f4d
    Former-commit-id: fc689951bcf926b5231f0525bc45122322075aa4
    Former-commit-id: 7f48cf36af2493b64f59e82e20df864301aac99c
    Former-commit-id: 5ef2cf89a67a1cd71d73b95eb915fb8c78204684
    Former-commit-id: e766dc621b19fe3a8d03fde947cbce2a66f62985
    Former-commit-id: 1eb96d4b406a7e0fd5bd3dc0ade32cd757194e5c
    Former-commit-id: 910237d97aa9e5ebf700bc75bc3106c867aba4a7
    Former-commit-id: 47c8446c831be63a191d2b6e4158e3fbde20d803

--- Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Update-RemoteConsoleGitRepository.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Update-RemoteConsoleGitRepository.ps1
@@ -1,5 +1,9 @@
 function Update-RemoteConsoleGitRepository {
 	$consoleGitHubSyncer = Join-Path $InstallPath "Libraries\Custom Helper Apps\ConsoleGitHubSyncer\ConsoleGitHubSyncer.exe"
 
-	Start-Process -FilePath $consoleGitHubSyncer -ArgumentList "-UpdateRemote `"$($InstallPath.TrimEnd('\'))`" " -NoNewWindow -Wait
+	$consoleGitHubSyncerCheck = Start-Process -FilePath $consoleGitHubSyncer -ArgumentList "-UpdateRemote `"$($InstallPath.TrimEnd('\'))`" " -NoNewWindow -Wait -PassThru
+
+	if ($consoleGitHubSyncerCheck.ExitCode -eq 1306) {
+		[System.Environment]::Exit(0)
+	}
 }
