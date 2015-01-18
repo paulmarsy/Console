6e146d81a81f0cd6de2e53dfed77dde292126b26
Paul Marston <paul@marston.me>
2015-01-18 22:23:31 +0000

    Updating the install step to use the constants as it can now be used by the installer
    Former-commit-id: 51706f138b777f417b0bbe09a906db4c4d18e45d
    Former-commit-id: b5aaedff8113c1c7f919e5013106dbf21204f361
    Former-commit-id: b7c8e8cf40a6656e65a50abb0bb027996a948004
    Former-commit-id: c4c09ef2e5d1df9e19c6b9a45c85a03fc3b129e5
    Former-commit-id: b1c5b07012a1bcdeadccae19e33c5386ab1cfea9
    Former-commit-id: a24cd590633469c99e0a290245dd09569409cec1
    Former-commit-id: 9df01527b90f982dc19c620b1ad654a9b1e9c13e
    Former-commit-id: bcfb7451756ef3a857e14fba02f372d2f2a40eeb

--- Custom Consoles/PowerShell/ModuleInitialization/2 - Configure Environment/Configure PATH Extensions.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/2 - Configure Environment/Configure PATH Extensions.ps1
@@ -1,12 +1,12 @@
 param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 3; Critical = $false}) }
 
-$pathExtensionsDir = Join-Path $ProfileConfig.Module.InstallPath "Libraries\PATH Extensions"
+$pathExtensionsDir = Join-Path $PowerShellConsoleConstants.InstallPath "Libraries\PATH Extensions"
 
 $Path = @(
 	$Env:PATH
 	$pathExtensionsDir
-	(Get-ChildItem -Path $pathExtensionsDir -Directory | % FullName) 
+	(Get-ChildItem -Path $pathExtensionsDir -Directory | % FullName)
 ) -Join ";"
 
 [System.Environment]::SetEnvironmentVariable("PATH", $Path, [System.EnvironmentVariableTarget]::Process)
