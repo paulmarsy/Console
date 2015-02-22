e82cd8a339ae7ff29c7c966858613405049e1b0a
Paul Marston <paul@marston.me>
2015-02-22 20:02:18 +0000

    removing lines which make the module slow to load
    Former-commit-id: dfb0c422a350c03c15b9a43c45874d0fc7ea23a9
    Former-commit-id: dbc475817268d6ba6de2a881eaa1865f1d1ae7b1
    Former-commit-id: 91b447b581953b29c135193866ac0f28dbade1d9
    Former-commit-id: d7cdf4b71ed3c9bd18eea9b76a1c11d484f23c52
    Former-commit-id: 07b05eb0f3264c5d57e336acf848250c044d4534
    Former-commit-id: 91291a90ee667afaf187638016e1058415ab4d5b
    Former-commit-id: 5469d1eadabe70f715d9ec048677b5140ffae5e5
    Former-commit-id: 2285054379b02e956f2e28c1cc22250709d87a27

--- Custom Consoles/PowerShell/ModuleInitialization/5 - Final/1 - Display Welcome Banner.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/5 - Final/1 - Display Welcome Banner.ps1
@@ -2,7 +2,6 @@ param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
 
 $machineIpProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()
-$networkConnectionProfile = Get-NetConnectionProfile | Sort-Object -Property InterfaceIndex | Select-Object -First 1
 
 Write-Host
 Write-Host -NoNewline -ForegroundColor DarkGreen ([Environment]::UserDomainName)
@@ -12,21 +11,6 @@ Write-Host -NoNewline -ForegroundColor Gray " on "
 Write-Host -NoNewline -ForegroundColor Red $machineIpProperties.HostName
 if (-not [string]::IsNullOrWhiteSpace($machineIpProperties.DomainName)) {
     Write-Host -NoNewline -ForegroundColor Red ".$($machineIpProperties.DomainName)"
-}
-
-if (Test-NetworkStatus -and $null -ne $networkConnectionProfile) {
-    Write-Host -NoNewline -ForegroundColor Gray " ("
-    Write-Host -NoNewline -ForegroundColor DarkGreen $networkConnectionProfile.NetworkCategory
-    Write-Host -NoNewline -ForegroundColor Gray ")"
-    Write-Host
-	Write-Host -NoNewline "Connected to network '"
-	Write-Host -NoNewline -ForegroundColor DarkGreen $networkConnectionProfile.Name
-	Write-Host -NoNewline "' over "
-	Write-Host -ForegroundColor Yellow $networkConnectionProfile.InterfaceAlias
-	Write-Host "IPv4 Connectivity: $($networkConnectionProfile.IPv4Connectivity)"
-	Write-Host "IPv6 Connectivity: $($networkConnectionProfile.IPv6Connectivity)"
-} else {
-	Write-Host -ForegroundColor Red "No network connectivity detected"
 }
 
 Write-Host
