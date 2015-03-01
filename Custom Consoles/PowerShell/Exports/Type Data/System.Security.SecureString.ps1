e7c580f41b1a03cca853452b559214da34b39b98
Paul Marston <paul@marston.me>
2015-03-01 23:05:03 +0000

    adding securestring type accelerator
    Former-commit-id: 6bb2defb702d50e3abcbc6b1b47e2a8934a956cf
    Former-commit-id: 6de432e15b906d2b9746879929ad054ea5c423f3
    Former-commit-id: b2f1b54dad52bcece573f195921174e1af04e3ac
    Former-commit-id: 29b2fabebe395f23fcff4b05651725712a8f156c
    Former-commit-id: 2d72ba6e4870684dfd435e66daf6aad63d2b4577
    Former-commit-id: 766d16ec55eec9ea00e68688db36655417daf80f
    Former-commit-id: 89fc3f966669f72eec1518fb331ff217de2f428b
    Former-commit-id: 3430950e0c2634ec48b47a70903ebe91f95ddb60

--- Custom Consoles/PowerShell/Exports/Type Data/System.Security.SecureString.ps1
+++ Custom Consoles/PowerShell/Exports/Type Data/System.Security.SecureString.ps1
@@ -1,14 +1,26 @@
-@{
-	MemberType = "ScriptMethod"
-	MemberName = "Peek"
-	Value = {
-				try {
-					$binarySecureString = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($this)
-					return [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($binarySecureString)
-				} finally {
-					if ($null -ne $binarySecureString) {
-						[System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($binarySecureString)
+@(
+	@{
+		MemberType = "ScriptMethod"
+		MemberName = "Peek"
+		Value = {
+					try {
+						$binarySecureString = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($this)
+						return [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($binarySecureString)
+					} finally {
+						if ($null -ne $binarySecureString) {
+							[System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($binarySecureString)
+						}
 					}
 				}
-			}
-}
+	},	@{
+		MemberType = "ScriptMethod"
+		MemberName = "SetReadOnly"
+		Value = {
+					param($String)
+					
+					$this.Clear()
+					$String.ToCharArray() | % { $this.AppendChar($_) }
+					$this.MakeReadOnly()
+				}
+	}
+)
