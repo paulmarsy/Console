a06a07b2260ce4a247c2a1f5a73ee380d393e016
Your Name <email@example.com>
2015-03-12 12:08:32 +0000

    fixing load dependencies when the cosnole has already been installed
    Former-commit-id: a13087a137a21ca5d54e6f8c1c95be17874722a1
    Former-commit-id: 528343bed621d18d6efca3f966d86125e9ba591b
    Former-commit-id: 628649920516b674fb4d5a18a045592558e16507
    Former-commit-id: 41dfd1196a41dd46fe479281e094e78ecd900f36
    Former-commit-id: 4eb892a422bfd655c55071ff3655feb0d69c2158
    Former-commit-id: 6c8ea870959a127e32fcb702d33c8b3be234e416
    Former-commit-id: 8885ef066b984cfef5665d9746450ed6fc1ab36e
    Former-commit-id: fcfaa5c22ea6ff6d8d01904ef8ec8d603e8264fc

--- Installation/Load Dependencies.ps1
+++ Installation/Load Dependencies.ps1
@@ -1,6 +1,6 @@
 New-Variable -Scope Global -Name MessageScope -Value 0
 New-Variable -Scope Global -Name InsideInstallStep -Value $false
 
-. (Join-Path $PSScriptRoot "..\Config\Core-Functionality.ps1")
+. (Join-Path $PSScriptRoot "..\Config\Core-Functionality.ps1") -InstallPath (Join-Path $PSScriptRoot "..\")
 
 Get-ChildItem (Join-Path $PSScriptRoot "Helpers") -File -Filter *.ps1 | % { . $_.FullName }
