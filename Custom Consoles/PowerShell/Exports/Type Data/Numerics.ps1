8b5b8ae060405eeceaadf5b4e81fd741591e40cc
Paul Marston <paul@marston.me>
2015-03-21 23:28:56 +0000

    adding to hex accellerator to numerics
    Former-commit-id: 43eae22afd4eb7888f4c3c0cf171a10cad2959eb
    Former-commit-id: d878a319084578a68f364a4bc07877e61e5526bd
    Former-commit-id: bf5cdf5b54332a22b983a76cb37dc814ce7d9918
    Former-commit-id: 0e97b9587900e93c62995c56507af351de9ef1ec
    Former-commit-id: 666d67b0a189cb26fd44a789ed8dc290bc920ea8
    Former-commit-id: 89ce736bfb3742b67ef8750fe033765c7d19eee7
    Former-commit-id: 303b363bd875e78e5008f74b0b0719393e73b2dd
    Former-commit-id: ca3ec2d58e49a73167a965bd54bf2751c2a52232

---
+++ Custom Consoles/PowerShell/Exports/Type Data/Numerics.ps1
@@ -0,0 +1,10 @@
+@(
+	@{
+        TypeName = @('System.SByte', 'System.Int16', 'System.Int32', 'System.Int64', 'System.Byte', 'System.UInt16', 'System.UInt32', 'System.UInt64')
+		MemberType = "ScriptMethod"
+		MemberName = "ToHex"
+		Value = {
+                    '0x{0:X}' -f $this
+				}
+	}
+)
--- Custom Consoles/PowerShell/ModuleInitialization/4 - General/Exports/Type Data.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/4 - General/Exports/Type Data.ps1
@@ -2,14 +2,28 @@ param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
 
 Get-ChildItem -Path (Join-Path $ProfileConfig.ConsolePaths.PowerShell "Exports\Type Data") -File -Filter "*.ps1" | % {
-	$typeName = $_.BaseName
+	$baseName = $_.BaseName
 	$typeExtensionData = & "$($_.FullName)"
 
 	if ($typeExtensionData -isnot [array]) {
 		$typeExtensionData = @($typeExtensionData)
 	}
 
-	$typeExtensionData | % { Update-TypeData -TypeName $typeName -Force @_ }
+	$typeExtensionData | % {
+		if ($_.ContainsKey("TypeName")) {
+			if ($_.TypeName -isnot [array]) {
+				$typeNames = @($_.TypeName)
+			} else {
+				$typeNames = $_.TypeName
+			}
+			$_.Remove("TypeName")
+		} else {
+			$typeNames = @($baseName)
+		}
+		
+		$typeData = $_
+		$typeNames | % { Update-TypeData -TypeName $_ -Force @typeData }
+	}
 }
 
 Get-ChildItem -Path (Join-Path $ProfileConfig.ConsolePaths.PowerShell "Exports\Type Data") -File -Filter "*.types.ps1xml" | % {
