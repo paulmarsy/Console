b6cd6264410177e328744cf1f9b4bd72f4fbb6ec
Paul Marston <paul@marston.me>
2015-04-17 01:45:23 +0100

    adding java to path
    Former-commit-id: ebef439bd8be5b8bad79a23eee03da0bbec478bf
    Former-commit-id: b5bd274b36b8e063c8f3b55e1f8ec9eceea6b9a3
    Former-commit-id: aec641ef0e576159e8a0d2c1907f5e2d892ff251
    Former-commit-id: dce97246297dd360c3ee349351cba69c2f8f1d5d
    Former-commit-id: 9049bb1eafac6434d051f8b9fa85df98cd372532
    Former-commit-id: 69614eded7b6e9d17b65e2299702e7d0f82e3f3e
    Former-commit-id: 426a1be28ee7410b7ba2a6919198a249462c4e80
    Former-commit-id: faacb2fb8ca4548f29b2349c81dcd2c8b218a79d

---
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Java.ps1
@@ -0,0 +1,21 @@
+param([switch]$GetModuleStepDetails)
+if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
+
+$javaRegKey = "HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment"
+if (-not (Test-Path $javaRegKey)) {
+	return
+}
+
+$currentVersionRegKey = Join-Path $javaRegKey (Get-ItemProperty -Path $javaRegKey -Name "CurrentVersion" | Select-Object -First 1 -ExpandProperty CurrentVersion)
+$javaHomePath = Get-ItemProperty -Path $currentVersionRegKey -Name "JavaHome" | Select-Object -First 1 -ExpandProperty JavaHome
+
+if (-not (Test-Path $javaHomePath)) {
+	return
+}
+
+if ($Env:PATH.Contains($javaHomePath)) {
+	return
+}
+
+$updatedPath = "{0};{1}" -f $Env:PATH, $javaHomePath
+[System.Environment]::SetEnvironmentVariable("PATH", $updatedPath, [System.EnvironmentVariableTarget]::Process)
