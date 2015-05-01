21ecedeb0ccc5caa184e354b27522782e32cc710
Your Name <email@example.com>
2015-05-01 16:06:04 +0100

    updating reset update timestamp to show more information

--- Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Reset-PowerShellConsoleGitUpdateTimeStamp.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Reset-PowerShellConsoleGitUpdateTimeStamp.ps1
@@ -1,4 +1,8 @@
 function Reset-PowerShellConsoleGitUpdateTimeStamp {
     $ProfileConfig.Git.LastAutoSyncTickTime = 0
     Save-ProfileConfig
+    
+    $lastSync = New-Object -Type System.DateTime -ArgumentList $ProfileConfig.Git.LastAutoSyncTickTime
+    $nextSyncAfter = $lastSync.AddSeconds($ProfileConfig.Git.SyncIntervalInSeconds)
+    Write-Host "Last sync time set to: $lastSync, next sync after $nextSyncAfter"
 }
