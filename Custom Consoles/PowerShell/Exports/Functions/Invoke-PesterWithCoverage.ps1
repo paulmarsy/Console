a4c6dc78bd3b381d4865c3b9877d86b2a0b54297
Paul Marston <paul@marston.me>
2015-05-22 12:47:50 +0100

    adding pester alias and improving Invoke-PesterWithCoverage
    Former-commit-id: 4d1dc52d6679c3412d9e4f5d7042dcd6ac3c1cf3

--- Custom Consoles/PowerShell/Exports/Functions/Invoke-PesterWithCoverage.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Invoke-PesterWithCoverage.ps1
@@ -3,11 +3,5 @@ function Invoke-PesterWithCoverage {
         $Path = $PWD
     )
     
-    try {
-        Push-Location $Path
-        Invoke-Pester -Strict -CodeCoverage (Get-ChildItem -File -Recurse -Include "*.ps1" -Exclude "*.Tests.ps1")
-    }
-    finally {
-        Pop-Location
-    }
+    Invoke-Pester -Path $Path -CodeCoverage (Get-ChildItem -Path $Path -File -Recurse -Include "*.ps1" -Exclude "*.Tests.ps1" | % FullName)
 }
