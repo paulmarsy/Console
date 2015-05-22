2c2c17d8e6f94e4cd63cfc20b2cfd2d96dc25e63
Paul Marston <paul@marston.me>
2015-05-22 10:28:43 +0100

    Attempting to swithch to the new Git for Windows fork, also updating other pre-req versions
    Former-commit-id: 9d4acbc8682f2d447b14d75ff770220144161d79

--- Installation/Prerequisites/InstallPrerequisites.ps1
+++ Installation/Prerequisites/InstallPrerequisites.ps1
@@ -7,12 +7,12 @@ try {
     
     if (# Microsoft Azure PowerShell  https://github.com/Azure/azure-powershell
         (Invoke-Installer -Name "Microsoft Azure PowerShell" -Uri "https://github.com/Azure/azure-powershell/releases/download/0.9.1-May2015/azure-powershell.0.9.1.msi" -Optional -Type msi) -and `
-        # Git for Windows             http://git-scm.com/download/win
-        (Invoke-Installer -Name "Git for Windows" -Uri "https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20150319/Git-1.9.5-preview20150319.exe" -Type exe -ArgumentList ($innoInstallerArguments + @('/COMPONENTS=""', '/TASKS=""'))) -and `
+        # Git for Windows             https://github.com/git-for-windows/git
+        (Invoke-Installer -Name "Git for Windows" -Uri "https://github.com/git-for-windows/git/releases/download/v2.4.1.windows.1/Git-2.4.1.1-dev-preview-64-bit.exe" -Type exe -ArgumentList ($innoInstallerArguments + @('/COMPONENTS=""', '/TASKS=""'))) -and `
         # GitHub for Windows          https://windows.github.com/
         (Invoke-Installer -Name "GitHub for Windows" -Uri "https://github-windows.s3.amazonaws.com/GitHubSetup.exe" -Type exe -ArgumentList @("/Force")) -and `
         # SmartGit                    http://www.syntevo.com/smartgit/
-        (Invoke-Installer -Name "SmartGit" -Uri "http://www.syntevo.com/downloads/smartgit/smartgit-win32-setup-jre-6_5_7.zip" -Optional -Type exe -Zipped -ArgumentList ($innoInstallerArguments + @('/MERGETASKS="!desktopicon"'))) -and `
+        (Invoke-Installer -Name "SmartGit" -Uri "http://www.syntevo.com/downloads/smartgit/smartgit-win32-setup-jre-6_5_8.zip" -Optional -Type exe -Zipped -ArgumentList ($innoInstallerArguments + @('/MERGETASKS="!desktopicon"'))) -and `
         # WinPcap                     http://www.winpcap.org/
         (Invoke-Installer -Name "WinPcap" -Uri "https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe" -Optional -Type exe -ArgumentList @("/S", "/desktopicon=no", "/quicklaunchicon=no"))) {
             
