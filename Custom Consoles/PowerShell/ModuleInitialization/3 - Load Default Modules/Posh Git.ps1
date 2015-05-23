f34eb64ba147fd6f04801ed2b6ca0c8ba76044bd
Paul Marston <paul@marston.me>
2015-05-23 17:36:28 +0100

    Removing unneeded workarounds now posh-git has been fixed
    Former-commit-id: 86b76ab3f06effc17bb0558449d8820016f2d72a

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
@@ -2,11 +2,4 @@ param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
 
 Export-Module posh-git
-$GitPromptSettings.EnableWindowTitle = "Windows PowerShell - Git - "
-Enable-GitColors
-
-(Get-ModulePrivateData -Name OnRemove).ScriptBlocks += {
-	if (Test-Path Variable:Global:VcsPromptStatuses) {
-    	Remove-Variable -Name "VcsPromptStatuses" -Scope Global
-    }
-}
+$GitPromptSettings.EnableWindowTitle = $null
