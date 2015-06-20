3dd4b27e406560545e1097cb6cade128e42b8ccb
Paul Marston <paul@marston.me>
2015-06-20 16:06:10 +0100

    Tweaking exit-tab and adding quit alias
    Former-commit-id: 89c55e5bd9558e967bc2da18f4d325a08bbdc3d3

--- Custom Consoles/PowerShell/Exports/Functions/Exit-Tab.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Exit-Tab.ps1
@@ -1,3 +1,3 @@
 function Exit-Tab {
-    Start-Process -FilePath $PowerShellConsoleConstants.Executables.ConEmuC -ArgumentList "/GuiMacro Close(7)" -Wait -NoNewWindow
+    Start-Process -FilePath $PowerShellConsoleConstants.Executables.ConEmuC -ArgumentList "-GuiMacro Close(7)" -Wait -NoNewWindow
 }
