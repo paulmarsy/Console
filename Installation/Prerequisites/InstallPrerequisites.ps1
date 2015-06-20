1ae73961534d6d236485decd9ff6b6d7f3462925
Paul Marston <paul@marston.me>
2015-06-20 18:57:44 +0100

    Updating prereq versions
    Former-commit-id: a508a1733ddd00a4eebaa60aec1fd6b281022a5b

--- Installation/Prerequisites/InstallPrerequisites.ps1
+++ Installation/Prerequisites/InstallPrerequisites.ps1
@@ -6,13 +6,13 @@ try {
     $innoInstallerArguments = @("/SILENT", "/NORESTART", "/CLOSEAPPLICATIONS", "/RESTARTAPPLICATIONS")
     
     if (# Microsoft Azure PowerShell  https://github.com/Azure/azure-powershell
-        (Invoke-Installer -Name "Microsoft Azure PowerShell" -Uri "https://github.com/Azure/azure-powershell/releases/download/0.9.1-May2015/azure-powershell.0.9.1.msi" -Optional -Type msi) -and `
+        (Invoke-Installer -Name "Microsoft Azure PowerShell" -Uri "https://github.com/Azure/azure-powershell/releases/download/0.9.3-June2015/azure-powershell.0.9.3.msi" -Optional -Type msi) -and `
         # Git for Windows             https://github.com/git-for-windows/git
-        (Invoke-Installer -Name "Git for Windows" -Uri "https://github.com/git-for-windows/git/releases/download/v2.4.1.windows.1/Git-2.4.1.1-dev-preview-64-bit.exe" -Type exe -ArgumentList ($innoInstallerArguments + @('/COMPONENTS=""', '/TASKS=""'))) -and `
+        (Invoke-Installer -Name "Git for Windows" -Uri "https://github.com/git-for-windows/git/releases/download/v2.4.4.windows.2/Git-2.4.4.2-3rd-release-candidate-64-bit.exe" -Type exe -ArgumentList ($innoInstallerArguments + @('/COMPONENTS=""', '/TASKS=""'))) -and `
         # GitHub for Windows          https://windows.github.com/
         (Invoke-Installer -Name "GitHub for Windows" -Uri "https://github-windows.s3.amazonaws.com/GitHubSetup.exe" -Type exe -ArgumentList @("/Force")) -and `
         # SmartGit                    http://www.syntevo.com/smartgit/
-        (Invoke-Installer -Name "SmartGit" -Uri "http://www.syntevo.com/downloads/smartgit/smartgit-win32-setup-jre-6_5_8.zip" -Optional -Type exe -Zipped -ArgumentList ($innoInstallerArguments + @('/MERGETASKS="!desktopicon"'))) -and `
+        (Invoke-Installer -Name "SmartGit" -Uri "http://www.syntevo.com/downloads/smartgit/smartgit-win32-setup-jre-6_5_9.zip" -Optional -Type exe -Zipped -ArgumentList ($innoInstallerArguments + @('/MERGETASKS="!desktopicon"'))) -and `
         # WinPcap                     http://www.winpcap.org/
         (Invoke-Installer -Name "WinPcap" -Uri "https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe" -Optional -Type exe -ArgumentList @("/S", "/desktopicon=no", "/quicklaunchicon=no"))) {
             
