362e74482fd4d7696ad029e26c16475d67f462b5
Paul Marston <paul@marston.me>
2015-06-21 22:38:36 +0100

    adding Get-NetStat cmdlet
    Former-commit-id: 19775e3318751038adc0c60ca09cd93e8c3e9ea3

---
+++ Custom Consoles/PowerShell/Exports/Functions/Networking/Get-NetStat.ps1
@@ -0,0 +1,26 @@
+function Get-NetStat
+{
+    & NETSTAT.EXE -aof | Select-Object -Skip 4 | % {
+        $entry = ($_.Trim()) -split "\s+"
+        $local = $entry[1] -split ":"
+        $remote = $entry[2] -split ":"
+        if ($entry.Length -eq 5) {
+            $state = $entry[3]
+            $ownerPid = $entry[4]
+        } else {
+            $state = [string]::Empty
+            $ownerPid = $entry[3]
+        }
+        
+		New-Object -TypeName PSObject -Property @{
+			Protocol = $entry[0]
+			LocalAddressIP = ([string]::Join(":", ($local | Select-Object -SkipLast 1)))
+			LocalAddressPort = ($local | Select-Object -Last 1)
+			ForeignAddressIP = ([string]::Join(":", ($remote | Select-Object -SkipLast 1)))
+			ForeignAddressPort = ($remote | Select-Object -Last 1)
+			State = $state
+            OwnerPid = $ownerPid
+            OwnerProcess = (Get-Process -Id $ownerPid | % Name)
+		}
+	}
+}
