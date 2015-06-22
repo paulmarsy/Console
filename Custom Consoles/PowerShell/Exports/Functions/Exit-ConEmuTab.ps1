00a7733d528948fad8e1e4c086e65eac8022e4b4
Paul Marston <paul@marston.me>
2015-06-22 18:03:46 +0100

    Changing Exit-Tab to Exit-ConEmuTab
    Former-commit-id: 8e11bb87824c4ab5429d19e006b1dc162a690382

--- Custom Consoles/PowerShell/Exports/Functions/Exit-ConEmuTab.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Exit-ConEmuTab.ps1
@@ -1,4 +1,5 @@
-function Exit-Tab {
+function Exit-ConEmuTab {
     $tempOutputFile = Get-TempFile -Path ConsoleTempDirectory
     Start-Process -FilePath $PowerShellConsoleConstants.Executables.ConEmuC -ArgumentList "-GuiMacro Close(7) -GuiMacro Shell(`"open`",`"cmd.exe`",`"/C DEL /Q `"$tempOutputFile`"`")" -Wait -NoNewWindow -RedirectStandardOutput $tempOutputFile
+    [System.Environment]::Exit(0)
 }
