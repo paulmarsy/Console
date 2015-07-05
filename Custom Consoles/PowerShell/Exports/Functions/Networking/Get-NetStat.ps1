563bd0084f492ea99ad6af3c61e2537eeaae24f5
Paul Marston <paul@marston.me>
2015-07-05 01:39:28 +0100

    Tweaking & improving get-netstat
    Former-commit-id: 8bd36641996e6d7f09ee2b19412c6a1a1353aa46

--- Custom Consoles/PowerShell/Exports/Functions/Networking/Get-NetStat.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Networking/Get-NetStat.ps1
@@ -3,10 +3,41 @@ function Get-NetStat
     param(
         [switch]$ResolveAddresses
     )
-    & NETSTAT.EXE -ao (?: { $ResolveAddresses } { "-f" } { "-n" })| Select-Object -Skip 4 | % {
+    
+    $DnsCache = @{
+        "127.0.0.1" = $env:COMPUTERNAME
+        "0.0.0.0" = $env:COMPUTERNAME
+        "*" = $env:COMPUTERNAME
+    }
+    $ProcessCache = @{}
+    
+    & NETSTAT.EXE -aon | Select-Object -Skip 4 | % {
         $entry = ($_.Trim()) -split "\s+"
         $local = $entry[1] -split ":"
+        $localAddress = [string]::Join(":", ($local | Select-Object -SkipLast 1))
         $remote = $entry[2] -split ":"
+        $remoteAddress = [string]::Join(":", ($remote | Select-Object -SkipLast 1))
+        if ($ResolveAddresses) {
+            if (-not $DnsCache.Contains($localAddress)) {
+                try {
+                    $DnsCache[$localAddress] = [System.Net.DNS]::GetHostByAddress($localAddress).HostName
+                }
+                catch [System.Net.Sockets.SocketException] {
+                    $DnsCache[$localAddress] = $localAddress
+                }
+            }
+            $localAddress = $DnsCache[$localAddress]
+            
+            if (-not $DnsCache.Contains($remoteAddress)) {
+                try {
+                    $DnsCache[$remoteAddress] = [System.Net.DNS]::GetHostByAddress($remoteAddress).HostName
+                }
+                catch [System.Net.Sockets.SocketException] {
+                    $DnsCache[$remoteAddress] = $remoteAddress
+                }
+            }
+            $remoteAddress = $DnsCache[$remoteAddress]
+        }
         if ($entry.Length -eq 5) {
             $state = $entry[3]
             $ownerPid = $entry[4]
@@ -15,15 +46,19 @@ function Get-NetStat
             $ownerPid = $entry[3]
         }
         
+        if (-not $ProcessCache.Contains($ownerPid)) {
+            $ProcessCache[$ownerPid] = Get-Process -Id $ownerPid | % Name
+        }
+        
 		$outputObject = New-Object -TypeName PSObject -Property @{
 			Protocol = $entry[0]
-			LocalAddressIP = ([string]::Join(":", ($local | Select-Object -SkipLast 1)))
+			LocalAddressIP = $LocalAddress
 			LocalAddressPort = ($local | Select-Object -Last 1)
-			ForeignAddressIP = ([string]::Join(":", ($remote | Select-Object -SkipLast 1)))
+			ForeignAddressIP = $remoteAddress
 			ForeignAddressPort = ($remote | Select-Object -Last 1)
 			State = $state
             OwnerPid = $ownerPid
-            OwnerProcess = (Get-Process -Id $ownerPid | % Name)
+            OwnerProcess = $ProcessCache[$ownerPid]
 		}
         
         $outputObject.PSTypeNames.Insert(0, 'Netstat')
