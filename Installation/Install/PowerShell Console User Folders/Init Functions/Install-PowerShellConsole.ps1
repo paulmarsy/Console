404bab8723eb1b5a2ff5f4b7915c3de15bc984d2
Paul Marston <paul@marston.me>
2015-07-05 01:39:51 +0100

    Switching conemuc commands to run in the same tab
    Former-commit-id: 01d7cb4e07d189ff796806415fb11dd2e6bbdbab

--- Installation/Install/PowerShell Console User Folders/Init Functions/Install-PowerShellConsole.ps1
+++ Installation/Install/PowerShell Console User Folders/Init Functions/Install-PowerShellConsole.ps1
@@ -1,5 +1,5 @@
 function Install-PowerShellConsole {
 	Start-Process -FilePath "##HStartPath##" -ArgumentList "/ELEVATE """"##InternalInstallFile##"" -StartAfterInstall $(if ($AutomatedReinstall) { "-AutomatedReinstall" }) """
-	Start-Process -FilePath "##ConEmuCExecutablePath##" -ArgumentList "-GuiMacro Close(7)" -Wait -WindowStyle Hidden
+	Start-Process -FilePath "##ConEmuCExecutablePath##" -ArgumentList "-GuiMacro Close(7)" -Wait -NoNewWindow
 	Get-Host | % SetShouldExit 0
 }
--- Installation/Install/PowerShell Console User Folders/Init Functions/Uninstall-PowerShellConsole.ps1
+++ Installation/Install/PowerShell Console User Folders/Init Functions/Uninstall-PowerShellConsole.ps1
@@ -1,5 +1,5 @@
 function Uninstall-PowerShellConsole {
 	Start-Process -FilePath "##HStartPath##" -ArgumentList "/ELEVATE """"##InternalUninstallFile##"""""
-	Start-Process -FilePath "##ConEmuCExecutablePath##" -ArgumentList "-GuiMacro Close(7)" -Wait -WindowStyle Hidden
+	Start-Process -FilePath "##ConEmuCExecutablePath##" -ArgumentList "-GuiMacro Close(7)" -Wait -NoNewWindow
 	Get-Host | % SetShouldExit 0
 }
