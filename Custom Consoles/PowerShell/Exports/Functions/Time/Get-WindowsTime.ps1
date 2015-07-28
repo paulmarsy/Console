70214b4069dff81332d3e602812ffc241fb2698b
Paul Marston <paul@marston.me>
2015-07-28 23:18:41 +0100

    Adding functionality to Get-WindowsTime
    Former-commit-id: 98efea17ccfcf6b94f0ed36d2b20550139e848ea

--- Custom Consoles/PowerShell/Exports/Functions/Time/Get-WindowsTime.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Time/Get-WindowsTime.ps1
@@ -1,3 +1,7 @@
 function Get-WindowsTime {
-    
+    Write-Host -ForegroundColor Green "`tStatus:"
+    Start-Process -FilePath "w32tm.exe" -ArgumentList "/query /status /verbose" -NoNewWindow -Wait
+    Write-Host
+    Write-Host -ForegroundColor Green "`tPeers:"
+    Start-Process -FilePath "w32tm.exe" -ArgumentList "/query /peers /verbose" -NoNewWindow -Wait
 }
