04316de0e544a07e4be19a6523444c9adddd3b52
Paul Marston <paul@marston.me>
2015-08-01 15:49:42 +0100

    Updating cmdlet to use Update-PowerShellConsoleVersion
    Former-commit-id: c14ccf89d782a0b36ef470e9ca04979d11ea64c3

--- Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Save-LocalPowerShellConsoleChanges.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Customisation Helpers/Save-LocalPowerShellConsoleChanges.ps1
@@ -1,43 +1,42 @@
 function Save-LocalPowerShellConsoleChanges {
+	[CmdletBinding(DefaultParameterSetName="Patch")]
 	param(
-		[Parameter(ValueFromRemainingArguments=$true)]$CommitMessage
+		[Parameter(ParameterSetName="Major", Mandatory=$false)][switch]$Major,
+        [Parameter(ParameterSetName="Minor", Mandatory=$false)][switch]$Minor,
+        [Parameter(ParameterSetName="Patch", Mandatory=$false)][switch]$Patch,
+		
+		[Parameter(ValueFromRemainingArguments=$true)][ValidateNotNullOrEmpty()][string]$CommitMessage
     )
 
-	while([string]::IsNullOrWhiteSpace($CommitMessage)) {
-		Write-Host -ForegroundColor Red "ERROR: Commit message cannot be empty"
-		$CommitMessage = Read-Host -Prompt "Commit message"
-	}
-	
 	Optimize-PowerShellConsoleFiles
 
-	$script:changesCommited = $false
-	function commitChanges {
-		param($Repo)
-		if ((_getNumberOfUncommitedChanges) -eq 0) { return }
 
-		Write-Host -ForegroundColor Cyan "Commiting changes to $Repo"
-		_invokeGitCommand "add -A" | Out-Null
-		_invokeGitCommand "commit -a -m `"$CommitMessage`""
-		$script:changesCommited = $true
+	$script:changesCommited = $false
+	filter commitChanges {
+		try {
+			Push-Location $_
+			
+			if ((_getNumberOfUncommitedChanges) -eq 0) { return }
+
+			Write-Host -ForegroundColor Cyan "Commiting changes to $(Split-Path $_ -Leaf)..."
+			_invokeGitCommand "add -A" | Out-Null
+			_invokeGitCommand "commit -a -m `"$CommitMessage`""
+			$script:changesCommited = $true
+		}
+		finally {
+			Pop-Location
+		}
 	}
 
-	_workOnConsoleWorkingDirectory {
-		_getSubmodulePaths | % {
-			try {
-				Push-Location $_
-				commitChanges $_
-			}
-			finally {
-				Pop-Location
-			}
-		}
+	_getSubmodulePaths | commitChanges
 
-		commitChanges "Main Repo"
+	Update-PowerShellConsoleVersion -Major:$Major -Minor:$Minor -Patch:$Patch 
+	
+	$ProfileConfig.Module.InstallPath | commitChanges "Main Console Repo..."
 
-		if ($script:changesCommited) {
-    		Write-Host -ForegroundColor Green "Changes commited successfully!"
-		} else {
-			Write-Host -ForegroundColor Red "No changes commited"
-		}
-    }
+	if ($script:changesCommited) {
+		Write-Host -ForegroundColor Green "Changes commited successfully!"
+	} else {
+		Write-Host -ForegroundColor Red "No changes commited"
+	}
 }
