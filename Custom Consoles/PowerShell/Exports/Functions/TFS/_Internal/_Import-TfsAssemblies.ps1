7176995fb94e33fe4ee8b188ef4d88023b38081f
Paul Marston <paul@marston.me>
2015-08-06 01:08:51 +0100

    Updating assemblies to vs2015
    Former-commit-id: 8fdd12f67ebc09f6fb617d2d938c35690a2bb5ef

--- Custom Consoles/PowerShell/Exports/Functions/TFS/_Internal/_Import-TfsAssemblies.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/TFS/_Internal/_Import-TfsAssemblies.ps1
@@ -2,10 +2,9 @@ function _Import-TfsAssemblies {
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')] 
 	param()
 	
-	Add-Type -AssemblyName "Microsoft.TeamFoundation.Common, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-	Add-Type -AssemblyName "Microsoft.TeamFoundation.Client, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-	Add-Type -AssemblyName "Microsoft.TeamFoundation.Build.Client, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-	Add-Type -AssemblyName "Microsoft.TeamFoundation.VersionControl.Client, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-
-	Add-Type -Path (Resolve-Path (Join-Path $Env:VS120COMNTOOLS "..\IDE\PrivateAssemblies\Microsoft.TeamFoundation.Build.Workflow.dll"))
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Common.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Build.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.VersionControl.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $Env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Build.Workflow.dll")
 }
