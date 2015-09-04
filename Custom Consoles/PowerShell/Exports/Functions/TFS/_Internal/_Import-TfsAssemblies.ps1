bd13653b6e076d19cea8c629bf3591f649483c5b
Paul Marston <paul@marston.me>
2015-09-04 13:49:43 +0100

    fixing the import of VS2015 TFS assemblies to a common location as the extension folder name is randomised per installed
    Former-commit-id: 7bc0b8bf0e27ac7d5942b66fb6c6eb39dd9b2f26

--- Custom Consoles/PowerShell/Exports/Functions/TFS/_Internal/_Import-TfsAssemblies.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/TFS/_Internal/_Import-TfsAssemblies.ps1
@@ -2,9 +2,9 @@ function _Import-TfsAssemblies {
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')] 
 	param()
 	
-	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Common.dll")
-	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Client.dll")
-	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Build.Client.dll")
-	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.VersionControl.Client.dll")
-	Add-Type -Path (Join-Path -Resolve -Path $Env:VS140COMNTOOLS -ChildPath "..\IDE\Extensions\kx5cqnkm.l3s\Microsoft.TeamFoundation.Build.Workflow.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.Common.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.Build.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $env:VS140COMNTOOLS -ChildPath "..\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.VersionControl.Client.dll")
+	Add-Type -Path (Join-Path -Resolve -Path $Env:VS140COMNTOOLS -ChildPath "..\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.Build.Workflow.dll")
 }
