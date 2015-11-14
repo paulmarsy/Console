e06deb24f2e5b40c8458972658203513f5cf9220
Your Name <email@example.com>
2015-11-14 16:54:34 +0000

    Adding Reverse method to system.array
    Former-commit-id: de9e81df2e655184cf5d8f4d2bfb85d8356cf9a9

--- Custom Consoles/PowerShell/Exports/Type Data/System.Array.ps1
+++ Custom Consoles/PowerShell/Exports/Type Data/System.Array.ps1
@@ -1,13 +1,45 @@
-@{
-	MemberType = "ScriptMethod"
-	MemberName = "Unique"
-	Value = {
-				$uniqueArray = {@()}.Invoke()
-				$this | % {
-					if ($uniqueArray.Contains($_)) { return }
-					else { $uniqueArray.Add($_) }
+@(
+	@{
+		MemberType = "ScriptMethod"
+		MemberName = "Unique"
+		Value = {
+					$uniqueArray = {@()}.Invoke()
+					$this | % {
+						if ($uniqueArray.Contains($_)) { return }
+						else { $uniqueArray.Add($_) }
+					}
+	
+					return ($uniqueArray -as $this.GetType())
 				}
-
-				return ($uniqueArray -as $this.GetType())
+	}, @{
+		MemberType = "ScriptMethod"
+		MemberName = "Slice"		
+		Value = {
+			param(
+        		[Parameter(Mandatory=$true)][ValidateRange(1,($this.Lenghth))]$Count
+    		)
+			
+			 $slice = @()
+   			 foreach ($i in $this) {
+        		if ($slice.Count -eq $Count) {
+           			Write-Output $slice
+					$slice = @()
+				}
+				
+				$slice += $i
 			}
+			Write-Output $slice
+        }
+        $slice.Add($item) > $null
+	}, @{
+		MemberType = "ScriptMethod"
+		MemberName = "Reverse"		
+		Value = {
+    		if ($this.Length -eq 1) {
+        		@($this[-1..-$this.Length])
+    		} else {
+        		@($this)
+    		}
+		}
+	}
 }
