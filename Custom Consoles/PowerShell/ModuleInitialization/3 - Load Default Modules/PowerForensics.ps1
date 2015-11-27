d5cb555e5ccc48c1c8fa3eb8b0c6da2ebd7a44b7
Paul Marston <paul@marston.me>
2015-11-27 10:13:16 +0000

    Fixing module path
    Former-commit-id: 1fec2f8667758b2a76f4302ce391900d3a53af13

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/PowerForensics.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/PowerForensics.ps1
@@ -1,4 +1,4 @@
 param([switch]$GetModuleStepDetails)
 if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
 
-Export-Module PowerForensics
+Export-Module PowerForensics (Join-Path $ProfileConfig.Module.InstallPath "Libraries\PowerShell Modules\PowerForensics\PowerForensics\Module\PowerForensics")
