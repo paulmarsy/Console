875ed36f1fd6344aaf53569ac6352ef8a90bc581
Paul Marston <paul@marston.me>
2016-03-08 10:30:55 +0000

    adding support for vs 2015

--- Custom Consoles/PowerShell/ModuleInitialization/4 - General/Import Visual Studio Vars.ps1
+++ Custom Consoles/PowerShell/ModuleInitialization/4 - General/Import Visual Studio Vars.ps1
@@ -3,15 +3,14 @@ if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
 
 switch ($ProfileConfig.TFS.Version)
 {
+    "2015" {
+		if (Test-Path Env:VS140COMNTOOLS) {
+			Import-VisualStudioVars -VisualStudioVersion 140 -Architecture amd64
+		}
+	}
 	"2013" {
 		if (Test-Path Env:VS120COMNTOOLS) {
 			Import-VisualStudioVars -VisualStudioVersion 2013 -Architecture amd64
 		}
 	}
-	"2010" {
-		if (Test-Path Env:VS100COMNTOOLS) {
-			Import-VisualStudioVars -VisualStudioVersion 2010 -Architecture amd64
-		}
-	}
 }
-
