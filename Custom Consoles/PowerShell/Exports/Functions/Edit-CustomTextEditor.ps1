86dd6fc576770f0de0dbf316dae8910a413ec062
Paul Marston <paul@marston.me>
2016-06-26 01:54:42 +0100

    fixing output

--- Custom Consoles/PowerShell/Exports/Functions/Edit-CustomTextEditor.ps1
+++ Custom Consoles/PowerShell/Exports/Functions/Edit-CustomTextEditor.ps1
@@ -1,19 +1,23 @@
 function Edit-CustomTextEditor {
     [CmdletBinding()]
     param(
-        $Path = $PWD,
+        [string]$Path = $PWD.ProviderPath,
         [switch]$CreateFile
     )
     
     if ([System.IO.Path]::IsPathRooted($Path)) {
         $Path = Get-AbsolutePath -Path $Path
     } else {
-        $Path = Get-AbsolutePath -Path (Join-Path -Path $PWD -ChildPath $Path)
+        $Path = Get-AbsolutePath -Path (Join-Path -Path $PWD.ProviderPath -ChildPath $Path)
     }
     
     if ($CreateFile -and -not (Test-Path -Path $Path -PathType Leaf)) {
         Set-File -Path $Path
-    }
+    }    
 
-    Start-Process -FilePath $PowerShellConsoleConstants.Executables.VisualStudioCode -ArgumentList "`"$Path`""
+    # Hack to supress atom's console output as redirecting output streams and other methods don't seem to work
+    Start-Job -ScriptBlock {
+        param($VisualStudioCode, $Path)
+        Start-Process -FilePath $VisualStudioCode -ArgumentList "`"$Path`""
+    } -ArgumentList @($PowerShellConsoleConstants.Executables.VisualStudioCode, $Path) | Wait-Job | Remove-Job
 }
