4ad9a99dd3bb0b4a7a0b4ae63d0f582074e5137f
Paul Marston <paul@marston.me>
2016-07-23 18:50:13 +0100

    y

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/HistoryPx.ps1
+++
@@ -1,6 +0,0 @@
-param([switch]$GetModuleStepDetails)
-if ($GetModuleStepDetails) { return (@{RunLevel = 1; Critical = $false}) }
-
-Export-Module HistoryPx
-
-Set-ExtendedHistoryConfiguration -MaximumEntryCount $MaximumHistoryCount
--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/PowerForensics.ps1
+++
@@ -1,4 +0,0 @@
-param([switch]$GetModuleStepDetails)
-if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
-
-Export-Module PowerForensics (Join-Path $ProfileConfig.Module.InstallPath "Libraries\PowerShell Modules\PowerForensics\PowerForensics\Module\PowerForensics")
