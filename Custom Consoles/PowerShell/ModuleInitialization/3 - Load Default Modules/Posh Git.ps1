526ac26d93c3d2b09235b2f574fedf8707706fcb
Paul Marston <paul@marston.me>
2016-07-24 16:06:02 +0100

    w

--- Custom Consoles/PowerShell/ModuleInitialization/3 - Load Default Modules/Posh Git.ps1
+++
@@ -1,5 +0,0 @@
-param([switch]$GetModuleStepDetails)
-if ($GetModuleStepDetails) { return (@{RunLevel = 2; Critical = $false}) }
-
-Export-Module posh-git
-$GitPromptSettings.EnableWindowTitle = $null
